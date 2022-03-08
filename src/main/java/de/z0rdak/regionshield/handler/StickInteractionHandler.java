1b90d45f0a68cc783b4042cf8f5420d7f2862881
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-03-08 22:46:23 +0100

    implement marker functionality

--- src/main/java/de/z0rdak/regionshield/handler/StickInteractionHandler.java
+++ src/main/java/de/z0rdak/regionshield/handler/StickInteractionHandler.java
@@ -1,16 +1,23 @@
 package de.z0rdak.regionshield.handler;
 
 import de.z0rdak.regionshield.RegionShield;
+import de.z0rdak.regionshield.core.area.AreaType;
+import de.z0rdak.regionshield.core.region.AbstractMarkableRegion;
+import de.z0rdak.regionshield.core.stick.MarkerStick;
+import de.z0rdak.regionshield.util.RegionUtil;
 import de.z0rdak.regionshield.util.StickType;
+import de.z0rdak.regionshield.util.StickUtil;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.AnvilRepairEvent;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,62 +27,108 @@ import static de.z0rdak.regionshield.util.StickUtil.*;
 @Mod.EventBusSubscriber(modid = RegionShield.MODID)
 public class StickInteractionHandler {
 
+    private StickInteractionHandler() {
+    }
+
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
         if (!event.getWorld().isClientSide) {
             PlayerEntity player = event.getPlayer();
-            ItemStack usedItem = player.getItemInHand(player.getUsedItemHand());
-            if (isStick(usedItem)) {
-                StickType stickType = getStickType(usedItem);
+            ItemStack involvedItemStack = event.getItemStack();
+            if (!involvedItemStack.equals(ItemStack.EMPTY) && isStick(involvedItemStack)) {
+                StickType stickType = getStickType(involvedItemStack);
+
                 boolean isShiftPressed = player.isShiftKeyDown();
                 BlockRayTraceResult blockRayTraceResult = event.getHitVec();
                 BlockPos pos = blockRayTraceResult.getBlockPos();
                 RayTraceResult.Type traceResultType = blockRayTraceResult.getType();
 
                 switch (stickType) {
+                    case MARKER:
+                        onMarkBlock(involvedItemStack, event);
+                        break;
                     case REGION_STICK:
-
                         break;
                     case FLAG_STICK:
-
-                        break;
-                    case MARKER:
-                        // add marking type for nbt (area type)
-                        handleRegionMarking(event);
                         break;
                     default:
                         break;
                 }
             }
         }
-
-
         // TODO: check block and handle stick action accordingly
-
         // TODO: rendering and charge use needs to be implemented in stickitem mixin
     }
 
-    private static void handleRegionMarking(PlayerInteractEvent.RightClickBlock event) {
-
+    public static void onMarkBlock(ItemStack involvedItem, PlayerInteractEvent.RightClickBlock event) {
+        MarkerStick marker = new MarkerStick(involvedItem.getTag().getCompound(STICK));
+        AreaType areaType = marker.getAreaType();
+        if (areaType == AreaType.UNKNOWN) {
+            RegionShield.LOGGER.warn("Unknown area type on marking - should really not happening");
+            return;
+        }
+        if (event.getPlayer().isShiftKeyDown()) {
+            marker.setTeleportPos(event.getPos());
+            involvedItem.getTag().put(STICK, marker.serializeNBT());
+            return;
+        }
+        // add block to NBT list
+        marker.addMarkedBlock(event.getPos());
+        // check whether marked blocks form a valid marked area
+        marker.checkValidArea();
+        involvedItem.getTag().put(STICK, marker.serializeNBT());
+        setStickName(involvedItem, StickType.MARKER);
     }
-
 
     @SubscribeEvent
-    public static void onRightClickAir(PlayerInteractEvent.RightClickEmpty event) {
-        // TODO: retrieve stick used
+    public static void onCycleMode(PlayerInteractEvent.RightClickItem event) {
+        if (!event.getWorld().isClientSide) {
+            ItemStack involvedItemStack = event.getItemStack();
+            // is some valid mod stick
+            if (!involvedItemStack.equals(ItemStack.EMPTY) && involvedItemStack.hasTag()
+                    && involvedItemStack.getTag() != null && involvedItemStack.getTag().contains(STICK)) {
 
-        // TODO: cycle modes according to sticks
+                RayTraceResult blockLookingAt = event.getPlayer().pick(20.0d, 0.0f, false);
+                boolean targetIsAir = false;
+                if (blockLookingAt.getType() == RayTraceResult.Type.BLOCK) {
+                    BlockPos blockpos = ((BlockRayTraceResult) blockLookingAt).getBlockPos();
+                    BlockState blockstate = event.getWorld().getBlockState(blockpos);
+                    targetIsAir = blockstate.getBlock().equals(Blocks.AIR);
+                }
+                if (blockLookingAt.getType() == RayTraceResult.Type.MISS) {
+                    targetIsAir = true;
+                }
+
+                if (event.getPlayer().isShiftKeyDown() && targetIsAir) {
+                    StickType stickType = getStickType(involvedItemStack);
+                    CompoundNBT nbt = involvedItemStack.getTag();
+                    switch (stickType) {
+                        case REGION_STICK:
+
+                            break;
+
+                        case FLAG_STICK:
+
+                            break;
+
+                        case MARKER:
+                            MarkerStick marker = new MarkerStick(nbt.getCompound(STICK));
+                            AreaType areaType = marker.getAreaType();
+                            AreaType nextType = AreaType.values()[(areaType.ordinal() + 1) % AreaType.values().length];
+                            // change area nbt, reset marked blocks, set valid to false
+                            marker.cycleArea(nextType == AreaType.UNKNOWN ? AreaType.CUBOID : nextType);
+                            // update stick name
+                            involvedItemStack.getTag().put(STICK, marker.serializeNBT());
+                            StickUtil.setStickName(involvedItemStack, StickType.MARKER);
+                            break;
+                        case UNKNOWN:
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
     }
-
-    @SubscribeEvent
-    public static void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event) {
-
-    }
-
-    public static void onAttackPlayer(AttackEntityEvent event) {
-        // TODO: handle add players with region stick
-    }
-
 
     /**
      * Handles creation of mod sticks by renaming them in an anvil
@@ -87,25 +140,66 @@ public class StickInteractionHandler {
             ItemStack outputItem = event.getItemResult();
             ItemStack inputItem = event.getItemInput();
             ItemStack ingredientInput = event.getIngredientInput();
+            boolean hasStickTag = outputItem.hasTag() && outputItem.getTag() != null && outputItem.getTag().contains(STICK);
+            if (hasStickTag) {
+                onCreateRegion(event);
+            }
             boolean isInputAndOutputStick = ItemStack.isSame(outputItem, Items.STICK.getDefaultInstance())
                     && ItemStack.isSame(inputItem, Items.STICK.getDefaultInstance());
             if (isInputAndOutputStick && ingredientInput.isEmpty()) {
-                String stickName = outputItem.getDisplayName().getString();
-                stickName = stickName.substring(1, stickName.length() - 1);
-                StickType type = StickType.of(stickName);
-                if (type != StickType.UNKNOWN) {
-                    inputItem.setCount(outputItem.getCount() - 1);
-                    player.addItem(inputItem);
-                    // TODO: Send network packet to force inventory sync
-                    player.giveExperienceLevels(1);
-                    outputItem.setCount(1);
-                    updateStickTag(outputItem, type);
-                    setStickNameAndTooltip(outputItem, type);
-                    applyEnchantmentGlint(outputItem);
-                }
+                onCreateStick(event);
             }
         }
     }
 
+    private static void onCreateStick(AnvilRepairEvent event) {
+        PlayerEntity player = event.getPlayer();
+        ItemStack outputItem = event.getItemResult();
+        ItemStack inputItem = event.getItemInput();
+        StickType type = StickType.of(outputItem.getHoverName().getString());
+        if (type != StickType.UNKNOWN) {
+            // split stack and only create one stick, also refund xp
+            inputItem.setCount(outputItem.getCount() - 1);
+            player.addItem(inputItem);
+            // TODO: Send network packet to force inventory sync
+            player.giveExperienceLevels(1);
+            outputItem.setCount(1);
+            // init NBT
+            initStickTag(outputItem, type, event.getPlayer().getCommandSenderWorld().dimension());
+            setStickName(outputItem, type);
+            setStickToolTip(outputItem, type);
+            applyEnchantmentGlint(outputItem);
+        }
+    }
+
+    private static void onCreateRegion(AnvilRepairEvent event) {
+        ItemStack outputItem = event.getItemResult();
+        PlayerEntity player = event.getPlayer();
+        CompoundNBT stickNBT = outputItem.getTag().getCompound(STICK);
+        StickType type = StickType.of(stickNBT.getString(STICK_TYPE));
+        switch (type) {
+            case MARKER:
+                String regionName = outputItem.getHoverName().getString();
+                MarkerStick marker = new MarkerStick(stickNBT);
+                if (marker.isValidArea()) {
+                    AbstractMarkableRegion region = RegionUtil.regionFrom(marker, regionName);
+                    // TODO: save region
+                    RegionShield.LOGGER.info(region.getName());
+                    marker.resetArea();
+                    outputItem.getTag().put(STICK, marker.serializeNBT());
+                    StickUtil.setStickName(outputItem, type);
+                } else {
+                    player.sendMessage(new TranslationTextComponent("Could not create region"), player.getUUID());
+                }
+                break;
+            case FLAG_STICK:
+                break;
+            case REGION_STICK:
+                break;
+            case UNKNOWN:
+            default:
+                break;
+        }
+    }
 
 }
--- src/main/java/de/z0rdak/regionshield/util/StickUtil.java
+++ src/main/java/de/z0rdak/regionshield/util/StickUtil.java
@@ -1,35 +1,158 @@
 package de.z0rdak.regionshield.util;
 
+import de.z0rdak.regionshield.core.stick.FlagStick;
+import de.z0rdak.regionshield.core.stick.MarkerStick;
+import de.z0rdak.regionshield.core.stick.RegionStick;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.StringNBT;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.text.IFormattableTextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
 public final class StickUtil {
 
     private StickUtil() {
     }
 
     public static final String STICK_TYPE = "stick_type";
+    public static final String STICK = "stick";
 
-    public enum StickType {
+    public static void applyEnchantmentGlint(ItemStack item) {
+        CompoundNBT dummy = new CompoundNBT();
+        dummy.putString("id", "");
+        dummy.putInt("lvl", 1);
+        ListNBT enchantmentList = new ListNBT();
+        enchantmentList.add(dummy);
+        item.addTagElement("Enchantments", enchantmentList);
+    }
 
-        MARKER("RegionMarker"),
-        FLAG_STICK("FlagStick"),
-        REGION_STICK("RegionStick");
-
-        public final String stickName;
-
-        StickType(String name) {
-            this.stickName = name;
+    /**
+     * Set init (default) nbt value for sticks
+     *
+     * @param stick stick item
+     * @param type  stick type to create
+     * @param dim   dimension tag to set for sticks
+     */
+    public static void initStickTag(ItemStack stick, StickType type, RegistryKey<World> dim) {
+        CompoundNBT itemTag = new CompoundNBT();
+        if (stick.hasTag()) {
+            itemTag = stick.getTag();
         }
-
-        public static StickType toStickType(String name) {
-            switch (name) {
-                case "RegionMarker":
-                    return StickType.MARKER;
-                case "FlagStick":
-                    return StickType.FLAG_STICK;
-                case "RegionStick":
-                    return StickType.REGION_STICK;
+        if (itemTag != null) {
+            switch (type) {
+                case MARKER:
+                    itemTag.put(STICK, new MarkerStick(dim).serializeNBT());
+                    break;
+                case FLAG_STICK:
+                    itemTag.put(STICK, new FlagStick().serializeNBT());
+                    break;
+                case REGION_STICK:
+                    itemTag.put(STICK, new RegionStick().serializeNBT());
+                    break;
                 default:
-                    return null;
+                    break;
             }
         }
     }
+
+    public static boolean isStick(ItemStack itemStack) {
+        return itemStack.getItem().getDescriptionId().equals(Items.STICK.getDescriptionId());
+    }
+
+    // TODO: fixme
+    public static StickType getStickType(ItemStack stick) {
+        if (stick.getTag() != null && stick.hasTag()) {
+            if (stick.getTag().contains(STICK)) {
+                return StickType.of(stick.getTag().getCompound(STICK).getString(STICK_TYPE));
+            }
+        } else {
+            return StickType.UNKNOWN;
+        }
+        return StickType.UNKNOWN;
+    }
+
+    public static void setStickName(ItemStack stick, StickType type) {
+        String displayName = "";
+        switch (type) {
+            case REGION_STICK:
+                displayName = TextFormatting.GREEN + type.stickName;
+                break;
+            case FLAG_STICK:
+                displayName = TextFormatting.AQUA + type.stickName;
+                break;
+            case MARKER:
+                MarkerStick marker = new MarkerStick(stick.getTag().getCompound(STICK));
+                String validFlag = marker.isValidArea() ? (TextFormatting.GREEN + "*" + TextFormatting.GOLD) : "";
+                displayName = TextFormatting.GOLD + type.stickName + " (" + marker.getAreaType().areaType + "" + validFlag + ")";
+                break;
+            default:
+                break;
+        }
+        stick.setHoverName(new StringTextComponent(displayName));
+    }
+
+    public static void setStickToolTip(ItemStack stick, StickType type) {
+        switch (type) {
+            case REGION_STICK:
+                setToolTip(stick, getRegionStickToolTip());
+                break;
+            case FLAG_STICK:
+                setToolTip(stick, getFlagStickToolTip());
+                break;
+            case MARKER:
+                setToolTip(stick, getMarkerToolTip());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void setToolTip(ItemStack stack, ListNBT loreNbt) {
+        stack.getOrCreateTagElement("display").put("Lore", loreNbt);
+    }
+
+    // TODO
+    private static ListNBT getMarkerToolTip() {
+        ListNBT lore = new ListNBT();
+        StringNBT simple1 = buildLoreTextLine(new TranslationTextComponent("help.tooltip.stick.marker.simple.1"), "#ff0020");
+        StringNBT simple2 = buildLoreTextLine(new TranslationTextComponent("help.tooltip.stick.marker.simple.2"), "#ff0010");
+        lore.add(simple1);
+        lore.add(simple2);
+        return lore;
+    }
+
+    // TODO
+    private static ListNBT getFlagStickToolTip() {
+        ListNBT lore = new ListNBT();
+        StringNBT simple1 = buildLoreTextLine(new TranslationTextComponent("help.tooltip.stick.flag-stick.simple.1"), "#ff0020");
+        StringNBT simple2 = buildLoreTextLine(new TranslationTextComponent("help.tooltip.stick.flag-stick.simple.2"), "#ff0010");
+        lore.add(simple1);
+        lore.add(simple2);
+        return lore;
+    }
+
+    // TODO
+    private static ListNBT getRegionStickToolTip() {
+        ListNBT lore = new ListNBT();
+        StringNBT simple1 = buildLoreTextLine(new TranslationTextComponent("help.tooltip.stick.region-stick.simple.1"), "#ff0020");
+        StringNBT simple2 = buildLoreTextLine(new TranslationTextComponent("help.tooltip.stick.region-stick.simple.2"), "#ff0010");
+        lore.add(simple1);
+        lore.add(simple2);
+        return lore;
+    }
+
+    private static StringNBT buildLoreTextLine(String text, String hexColor) {
+        return StringNBT.valueOf("{\"text\":\"" + text + "\", \"color\":\"" + hexColor + "\"}");
+    }
+
+    private static StringNBT buildLoreTextLine(IFormattableTextComponent text, String hexColor) {
+        return buildLoreTextLine(text.getString(), hexColor);
+    }
+
 }
