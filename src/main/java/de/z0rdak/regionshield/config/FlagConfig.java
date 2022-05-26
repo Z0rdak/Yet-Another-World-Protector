5fa6621087b0b807cc9fd76d28b3f40c087810eb
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-05-26 21:19:30 +0200

    add config to include entities for break and place flags

---
+++ src/main/java/de/z0rdak/regionshield/config/FlagConfig.java
@@ -0,0 +1,33 @@
+package de.z0rdak.regionshield.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FlagConfig {
+
+    public static final ForgeConfigSpec CONFIG_SPEC;
+
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>>  BREAK_FLAG_ENTITIES;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>>  BREAK_FLAG_ENTITY_TAGS;
+
+    static {
+        final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+        BUILDER.push("RegionShield mod flag configuration").build();
+
+        BREAK_FLAG_ENTITIES = BUILDER
+                .comment("Entities included/protected by the break block flag. Includes entities like armor stands and pictures by default")
+                .defineListAllowEmpty(Arrays.asList("break_flag_entities"), () -> Arrays.asList("minecraft:armor_stand", "minecraft:picture"), s -> true);
+
+        BREAK_FLAG_ENTITY_TAGS = BUILDER
+                .comment("Entity tags included/protected by the break block flag.")
+                .defineListAllowEmpty(Arrays.asList("break_flag_entity_tags"), () -> Arrays.asList(), s -> true);
+
+        BUILDER.pop();
+        CONFIG_SPEC = BUILDER.build();
+
+    }
+}
