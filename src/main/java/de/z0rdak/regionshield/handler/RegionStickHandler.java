c51067586a26a42060f4c4a1a4e359377b675f72
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-05-26 21:40:21 +0200

    implement region stick mode cycling

--- src/main/java/de/z0rdak/regionshield/handler/RegionStickHandler.java
+++ src/main/java/de/z0rdak/regionshield/handler/RegionStickHandler.java
@@ -1,12 +1,22 @@
 package de.z0rdak.regionshield.handler;
 
+import de.z0rdak.regionshield.core.stick.RegionStick;
+import de.z0rdak.regionshield.util.StickType;
+import de.z0rdak.regionshield.util.StickUtil;
 import net.minecraft.item.ItemStack;
-import org.apache.commons.lang3.NotImplementedException;
+import net.minecraft.nbt.CompoundNBT;
+
+import static de.z0rdak.regionshield.util.StickUtil.STICK;
 
 public class RegionStickHandler {
 
-
     public static void onCycleRegionStick(ItemStack regionStickItem) {
-        throw new NotImplementedException("onCycleRegionStick");
+        CompoundNBT nbt = regionStickItem.getTag();
+        RegionStick regionStick = new RegionStick(nbt.getCompound(STICK));
+        // cycle mode
+        regionStick.cycleMode();
+        // update stick name
+        regionStickItem.getTag().put(STICK, regionStick.serializeNBT());
+        StickUtil.setStickName(regionStickItem, StickType.REGION_STICK);
     }
 }
