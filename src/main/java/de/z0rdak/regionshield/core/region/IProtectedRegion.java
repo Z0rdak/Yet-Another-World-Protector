616201ee5eec865a4135b7405240d421f5829d9d
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-05-27 21:36:10 +0200

    cleanup interface - now uses IFlag

--- src/main/java/de/z0rdak/regionshield/core/region/IProtectedRegion.java
+++ src/main/java/de/z0rdak/regionshield/core/region/IProtectedRegion.java
@@ -8,7 +8,6 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.util.INBTSerializable;
 
 import java.util.Collection;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -16,8 +15,9 @@ import java.util.UUID;
  * This region has most likely a set of flags to prevent certain action in
  * this region and a set of player, which are allowed to bypass these
  * restrictions.
- * Additionally a flag can determine whether actions are whitelisted
- * or blacklisted and a flag which determines whether the regions is
+ * <p>
+ * Additionally, a flag can determine whether actions are whitelisted
+ * or blacklisted and a flag which determines whether the regions are
  * active or not.
  * <p>
  * Classes which implement this interface must also provide a way
@@ -29,17 +29,13 @@ public interface IProtectedRegion extends INBTSerializable<CompoundNBT> {
 
     boolean addFlag(IFlag flag);
 
-    boolean addFlag(String flag, boolean allowance);
+    boolean removeFlag(IFlag flag);
 
-    boolean removeFlag(String flag);
-
-    boolean containsFlag(String flag);
+    boolean containsFlag(IFlag flag);
 
     boolean containsFlag(RegionFlag flag);
 
-    Set<String> getFlags();
-
-    Collection<IFlag> getRegionFlags();
+    Collection<IFlag> getFlags();
 
     boolean addMember(PlayerEntity player);
 
@@ -56,11 +52,4 @@ public interface IProtectedRegion extends INBTSerializable<CompoundNBT> {
     PlayerContainer getMembers();
 
     boolean permits(PlayerEntity player);
-
-    boolean forbids(PlayerEntity player);
-
-    boolean denies(String flag);
-
-    boolean denies(RegionFlag flag);
-
 }
