b7be891b64d90d8d0e275c0643167e6ffc42eccc
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-08-19 19:54:51 +0200

    draft for region creation

--- src/main/java/de/z0rdak/regionshield/util/RegionUtil.java
+++ src/main/java/de/z0rdak/regionshield/util/RegionUtil.java
@@ -3,9 +3,12 @@ package de.z0rdak.regionshield.util;
 import de.z0rdak.regionshield.core.area.CuboidArea;
 import de.z0rdak.regionshield.core.region.AbstractMarkableRegion;
 import de.z0rdak.regionshield.core.region.CuboidRegion;
+import de.z0rdak.regionshield.core.region.SphereRegion;
 import de.z0rdak.regionshield.core.stick.MarkerStick;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -14,30 +17,40 @@ public final class RegionUtil {
     private RegionUtil() {
     }
 
-    // TODO: create region from marker stick info
     public static AbstractMarkableRegion regionFrom(PlayerEntity player, MarkerStick marker, String regionName) {
+        return regionFrom(player, marker, regionName, marker.getDimension());
+    }
+
+    public static AbstractMarkableRegion regionFrom(PlayerEntity player, MarkerStick marker, String regionName, RegistryKey<World> dim) {
         switch (marker.getAreaType()) {
             case CUBOID:
-                return cuboidRegionFrom(marker, regionName, player);
-            case CYLINDER:
+                return cuboidRegionFrom(marker, regionName, player, dim);
             case SPHERE:
-            case PRISM:
-            case POLYGON_3D:
+                return sphericalRegionFrom(marker, regionName, player, dim);
             case UNKNOWN:
             default:
                 return null;
         }
     }
 
+    private static SphereRegion sphericalRegionFrom(MarkerStick marker, String regionName, PlayerEntity player, RegistryKey<World> dim) {
+        return null;
+    }
+
     private static CuboidRegion cuboidRegionFrom(MarkerStick marker, String regionName, PlayerEntity player){
+        return cuboidRegionFrom(marker, regionName, player, marker.getDimension());
+    }
+
+    private static CuboidRegion cuboidRegionFrom(MarkerStick marker, String regionName, PlayerEntity player, RegistryKey<World> dim){
         List<BlockPos> blocks = marker.getMarkedBlocks();
+        // TODO: test this before hand? this would eliminate these double methods with and without dim
         if (blocks.size() != 2) {
             return null;
         }
         CuboidArea cuboidArea = new CuboidArea(blocks);
         if (marker.getTeleportPos() == null) {
-            return new CuboidRegion(regionName, cuboidArea, marker.getTeleportPos(), player, marker.getDimension());
+            return new CuboidRegion(regionName, cuboidArea, marker.getTeleportPos(), player, dim);
         }
-        return new CuboidRegion(regionName, cuboidArea, player, marker.getDimension());
+        return new CuboidRegion(regionName, cuboidArea, player, dim);
     }
 }
