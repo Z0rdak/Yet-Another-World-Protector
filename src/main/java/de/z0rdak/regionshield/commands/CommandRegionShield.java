633685096517bb81599439497994115d9fad5282
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-08-21 09:10:15 +0200

    add help info and link to online wiki

--- src/main/java/de/z0rdak/regionshield/commands/CommandRegionShield.java
+++ src/main/java/de/z0rdak/regionshield/commands/CommandRegionShield.java
@@ -3,16 +3,22 @@ package de.z0rdak.regionshield.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import de.z0rdak.regionshield.RegionShield;
 import de.z0rdak.regionshield.config.ServerConfigBuilder;
+import de.z0rdak.regionshield.util.CommandUtil;
 import de.z0rdak.regionshield.util.MessageUtil;
 import de.z0rdak.regionshield.util.PlayerUtils;
-import de.z0rdak.regionshield.util.RegionUtil;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.ClickEvent;
 
 import static de.z0rdak.regionshield.util.CommandUtil.helpLiteral;
+import static de.z0rdak.regionshield.util.MessageUtil.buildExecuteCmdComponent;
+import static de.z0rdak.regionshield.util.MessageUtil.buildHelpHeader;
 
 public class CommandRegionShield {
 
@@ -30,9 +36,9 @@ public class CommandRegionShield {
     private static LiteralArgumentBuilder<CommandSource> withSubCommands(LiteralArgumentBuilder<CommandSource> baseCommand) {
         return baseCommand
                 .requires(source -> source.hasPermission(ServerConfigBuilder.RS_CMD_OP_LEVEL.get()))
-                .executes(ctx -> giveHelp(ctx.getSource()))
+                .executes(ctx -> promptHelp(ctx.getSource()))
                 .then(helpLiteral
-                        .executes(ctx -> giveHelp(ctx.getSource())))
+                        .executes(ctx -> promptHelp(ctx.getSource())))
                 .then(DimensionCommands.DIMENSION_COMMAND)
                 //.then(RegionCommands.REGION_COMMAND)
                 //.then(RegionCommands.REGIONS_COMMAND)
@@ -53,26 +59,13 @@ public class CommandRegionShield {
         return false;
     }
 
-    private static int giveHelp(CommandSource source) {
-        try {
-            PlayerEntity player = source.getPlayerOrException();
-            // TODO: MessageUtil.promptBaseCommandHelp(player);
-        } catch (CommandSyntaxException e) {
-            e.printStackTrace();
-        }
-        return 0;
-
-    }
-
-
-    public static int promptBaseCommandHelp(CommandSource src) {
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpHeader("help.wp.header"));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpLink("help.wp.1", CommandConstants.EXPAND));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpLink("help.wp.4", CommandConstants.REGION));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpLink("help.wp.2", CommandConstants.FLAG));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpLink("help.wp.3", CommandConstants.PLAYER));
-        MessageUtil.sendCmdFeedback(src, new TranslationTextComponent("help.wp.5"));
+    private static int promptHelp(CommandSource src) {
+        MessageUtil.sendCmdFeedback(src, buildHelpHeader("cli.msg.help.header"));
+        MessageUtil.sendCmdFeedback(src, buildExecuteCmdComponent(" => ", CommandUtil.buildCommandStr(CommandConstants.DIMENSION.toString(), " "), TextFormatting.GREEN, "Manage dimensional regions", ClickEvent.Action.SUGGEST_COMMAND).append(new TranslationTextComponent("cli.msg.help.1")));
+        String wikiLink = "https://github.com/Z0rdak/RegionShield/wiki";
+        StringTextComponent wikiInfo = new StringTextComponent("The in-game help is under construction.\nVisit the online wiki for more guide on how to use the mod.\nOnline-Wiki: ");
+        MessageUtil.sendCmdFeedback(src, wikiInfo.append(buildExecuteCmdComponent(RegionShield.MODID_LONG + " online wiki", wikiLink,
+                TextFormatting.AQUA, "Open online wiki in your browser", ClickEvent.Action.OPEN_URL)));
         return 0;
     }
-
 }
