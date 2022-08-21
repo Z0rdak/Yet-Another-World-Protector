0171b84a7cad9d056b81da12a3dd445965e281d2
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-08-21 21:21:44 +0200

    update command interceptor

--- src/main/java/de/z0rdak/regionshield/handler/CommandInterceptor.java
+++ src/main/java/de/z0rdak/regionshield/handler/CommandInterceptor.java
@@ -1,6 +1,10 @@
 package de.z0rdak.regionshield.handler;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import de.z0rdak.regionshield.RegionShield;
 import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -12,9 +16,19 @@ public class CommandInterceptor {
         if (src.getEntity() == null) {
             // server console or command block
             // TODO: Check both and add config for command block
+            RegionShield.LOGGER.debug(src.getDisplayName().toString());
+            RegionShield.LOGGER.debug(src.getTextName());
         } else {
-            // most likely player executing command
-            // TODO: handle permissions
+            try {
+                // most likely player executing command
+                // TODO: handle permissions
+                if (src.getEntity() instanceof PlayerEntity){
+                    ServerPlayerEntity player = src.getPlayerOrException();
+                }
+            } catch (CommandSyntaxException e) {
+                // TODO: Logging
+            }
+
         }
     }
 }
