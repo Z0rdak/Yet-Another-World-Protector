fada56d0d6446df259194fd5a4c65f93fc4ad221
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-08-27 22:32:12 +0200

    organize flags

--- src/main/java/de/z0rdak/yawp/core/flag/RegionFlag.java
+++ src/main/java/de/z0rdak/yawp/core/flag/RegionFlag.java
@@ -13,104 +13,87 @@ import java.util.stream.Collectors;
 public enum RegionFlag {
 
     BREAK_BLOCKS(new ConditionFlag("break_blocks", false)),
-    PLACE_BLOCKS(new ConditionFlag("place_blocks", false));
-    /*
-    ENTITY_PLACE("entity-place"), // TODO: needs testing
+    SCOOP_FLUIDS(new ConditionFlag("scoop_fluids", false)),
+    BREAK_ENTITIES(new ConditionFlag("break_entities", false)),
+    PLACE_BLOCKS(new ConditionFlag("place_blocks", false)),
+    PLACE_FLUIDS(new ConditionFlag("place_fluids", false)),
+    ENTITY_PLACE(new ConditionFlag("entity-place", false)),
+    EXPLOSION_ENTITY(new ConditionFlag("explosions-entities", false)),
+    EXPLOSION_BLOCK(new ConditionFlag("explosions-blocks", false)),
+    EXPLOSION_CREEPER_BLOCK(new ConditionFlag("creeper-explosion-entities", false)),
+    EXPLOSION_CREEPER_ENTITY(new ConditionFlag("creeper-explosion-blocks", false)),
+    EXPLOSION_OTHER_BLOCKS(new ConditionFlag("other-explosion-entities", false)),
+    EXPLOSION_OTHER_ENTITY(new ConditionFlag("other-explosion-blocks", false)),
+    IGNITE_EXPLOSIVES(new ConditionFlag("ignite-explosives", false)),
+    TOOL_SECONDARY_USE(new ConditionFlag("tools-secondary", false)),
+    AXE_STRIP(new ConditionFlag("strip-wood", false)),
+    HOE_TILL(new ConditionFlag("till-farmland", false)),
+    SHOVEL_PATH(new ConditionFlag("shovel-path", false)),
+    TRAMPLE_FARMLAND(new ConditionFlag("trample-farmland", false)),
+    TRAMPLE_FARMLAND_PLAYER(new ConditionFlag("trample-farmland-player", false)),
+    TRAMPLE_FARMLAND_OTHER(new ConditionFlag("trample-farmland-other", false)),
+    DRAGON_BLOCK_PROT(new ConditionFlag("dragon-destruction", false)),
+    WITHER_BLOCK_PROT(new ConditionFlag("wither-destruction", false)),
+    ZOMBIE_DOOR_PROT(new ConditionFlag("zombie-destruction", false)),
+    LIGHTNING_PROT(new ConditionFlag("lightning", false)),
     //
-    EXPLOSION_ENTITY("explosions-entities"),
-    EXPLOSION_BLOCK("explosions-blocks"),
-    EXPLOSION_CREEPER_BLOCK("creeper-explosion-entities"),
-    EXPLOSION_CREEPER_ENTITY("creeper-explosion-blocks"),
-    EXPLOSION_OTHER_BLOCKS("other-explosion-entities"),
-    EXPLOSION_OTHER_ENTITY("other-explosion-blocks"),
-    IGNITE_EXPLOSIVES("ignite-explosives"),
-    //
-    TOOL_SECONDARY_USE("tools-secondary"),
-    AXE_STRIP("strip-wood"),
-    HOE_TILL("till-farmland"),
-    SHOVEL_PATH("shovel-path"),
-    //
-    TRAMPLE_FARMLAND("trample-farmland"),
-    TRAMPLE_FARMLAND_PLAYER("trample-farmland-player"),
-    TRAMPLE_FARMLAND_OTHER("trample-farmland-other"),
-    //
-    //PISTON_PUSH("piston-push"),
-    //PISTON_PULL("piston-pull"),
-    //
-    //FLUID_FLOWING("fluid-flowing"),
-    //WATER_FLOWING("water-flowing"),
-    //LAVA_FLOWING("lava-flowing"),
-    //
-    DRAGON_BLOCK_PROT("dragon-destruction"),
-    WITHER_BLOCK_PROT("wither-destruction"),
-    ZOMBIE_DOOR_PROT("zombie-destruction"),
-    LIGHTNING_PROT("lightning"),
-    //
-    ANIMAL_TAMING("animal-taming"),
-    ANIMAL_BREEDING("animal-breeding"),
-    ANIMAL_MOUNTING("animal-mounting"),
-    ANIMAL_UNMOUNTING("animal-unmounting"), // FIXME: Minecraft vanilla bug fixed in 21w03a
-    //
-    SPAWNING_MONSTERS("spawning-monsters"),
-    SPAWNING_GOLEM("spawning-irongolem"),
-    SPAWNING_ANIMAL("spawning-animal"),
-    SPAWNING_ALL("spawning-all"),
-    SPAWNING_XP("spawning-xp"),
-    //
-    USE("use"), // Buttons, Doors, Lever, etc
-    USE_BONEMEAL("use-bonemeal"),
-    CONTAINER_ACCESS("access-container"),
-    ENDER_CHEST_ACCESS("access-enderchest"),
-    USE_ENDERPEARL_FROM_REGION("enderpearl-from"),
-    USE_ENDERPEARL_TO_REGION("enderpearl-to"),
-    //
-    ENDERMAN_TELEPORT_TO_REGION("enderman-teleport-to"),
-    ENDERMAN_TELEPORT_FROM_REGION("enderman-teleport-from"),
-    SHULKER_TELEPORT_TO_REGION("shulker-teleport-to"),
-    SHULKER_TELEPORT_FROM_REGION("shulker-teleport-from"),
-    //
-    ITEM_DROP("item-drop"),
-    ITEM_PICKUP("item-pickup"),
+    ANIMAL_TAMING(new ConditionFlag("animal-taming", false)),
+    ANIMAL_BREEDING(new ConditionFlag("animal-breeding", false)),
+    ANIMAL_MOUNTING(new ConditionFlag("animal-mounting", false)),
+    // ANIMAL_UNMOUNTING(new ConditionFlag("animal-unmounting", false)), // FIXME: Minecraft vanilla bug fixed in 21w03a
+    SPAWNING_MONSTERS(new ConditionFlag("spawning-monsters", false)),
+    SPAWNING_GOLEM(new ConditionFlag("spawning-irongolem", false)),
+    SPAWNING_ANIMAL(new ConditionFlag("spawning-animal", false)),
+    SPAWNING_ALL(new ConditionFlag("spawning-all", false)),
+    SPAWNING_XP(new ConditionFlag("spawning-xp", false)),
+    USE(new ConditionFlag("use", false)), // Buttons, Doors, Lever, e, false), falsetc
+    USE_BONEMEAL(new ConditionFlag("use-bonemeal", false)),
+    CONTAINER_ACCESS(new ConditionFlag("access-container", false)),
+    ENDER_CHEST_ACCESS(new ConditionFlag("access-enderchest", false)),
+    USE_ENDERPEARL_FROM_REGION(new ConditionFlag("enderpearl-from", false)),
+    USE_ENDERPEARL_TO_REGION(new ConditionFlag("enderpearl-to", false)),
+    ENDERMAN_TELEPORT_TO_REGION(new ConditionFlag("enderman-teleport-to", false)),
+    ENDERMAN_TELEPORT_FROM_REGION(new ConditionFlag("enderman-teleport-from", false)),
+    SHULKER_TELEPORT_TO_REGION(new ConditionFlag("shulker-teleport-to", false)),
+    SHULKER_TELEPORT_FROM_REGION(new ConditionFlag("shulker-teleport-from", false)),
+    ITEM_DROP(new ConditionFlag("item-drop", false)),
     // unrelated: mobs pickup logic => MobEntity#livingTick
-    LOOT_DROP("loot-drop"),
-    XP_DROP_ALL("xp-drop-all"), // also includes blocks (furnace for example)
-    XP_DROP_MONSTER("xp-drop-monsters"), // only hostile mobs
-    XP_DROP_OTHER("xp-drop-other"), // non-hostile: animals, villagers,...
-    XP_PICKUP("xp-pickup"),
-    LEVEL_FREEZE("level-freeze"),
-    XP_FREEZE("xp-freeze"),
-    //
-    ATTACK_PLAYERS("attack-players"),
-    ATTACK_ANIMALS("attack-animals"),
-    ATTACK_MONSTERS("attack-monsters"),
-    ATTACK_VILLAGERS("attack-villagers"),
-    //
-    INVINCIBLE("invincible"),
-    //
-    FALL_DAMAGE("fall-damage"),
-    FALL_DAMAGE_VILLAGERS("fall-damage-villagers"),
-    FALL_DAMAGE_MONSTERS("fall-damage-monsters"),
-    FALL_DAMAGE_ANIMALS("fall-damage-animals"),
-    FALL_DAMAGE_PLAYERS("fall-damage-players"),
-    //
-    SEND_MESSAGE("send-chat"),
-    EXECUTE_COMMAND("exec-command"),
-    SET_SPAWN("set-spawn"),
-    // RESET_SPAWN("reset-spawn"), // not working
-    SLEEP("sleep"),
-    //
-    // ELYTRA_FLY("elytra-fly"),
-    // USE_FIREWORK("use-firework"),
-    //
-    USE_PORTAL("use-portal"),
-    USE_PORTAL_PLAYERS("use-portal-players"),
-    USE_PORTAL_ITEMS("use-portal-items"),
-    USE_PORTAL_ANIMALS("use-portal-animals"),
-    USE_PORTAL_MONSTERS("use-portal-monsters"),
-    USE_PORTAL_VILLAGERS("use-portal-villagers"),
-    USE_PORTAL_MINECARTS("use-portal-minecarts"),
-    SPAWN_PORTAL("spawn-portal");
-    */
+    ITEM_PICKUP(new ConditionFlag("item-pickup", false)),
+    LOOT_DROP(new ConditionFlag("loot-drop", false)),
+    XP_DROP_ALL(new ConditionFlag("xp-drop-all", false)), // also includes blocks (furnace for example)
+    XP_DROP_MONSTER(new ConditionFlag("xp-drop-monsters", false)), // only hostile mobs
+    XP_DROP_OTHER(new ConditionFlag("xp-drop-other", false)), // non-hostile: animals, villagers,...
+    XP_PICKUP(new ConditionFlag("xp-pickup", false)),
+    LEVEL_FREEZE(new ConditionFlag("level-freeze", false)),
+    XP_FREEZE(new ConditionFlag("xp-freeze", false)),
+    ATTACK_PLAYERS(new ConditionFlag("attack-players", false)),
+    ATTACK_ANIMALS(new ConditionFlag("attack-animals", false)),
+    ATTACK_MONSTERS(new ConditionFlag("attack-monsters", false)),
+    ATTACK_VILLAGERS(new ConditionFlag("attack-villagers", false)),
+    ATTACK_WANDERING_TRADER(new ConditionFlag("attack-wtrader", false)),
+    INVINCIBLE(new ConditionFlag("invincible", false)),
+    FALL_DAMAGE(new ConditionFlag("fall-damage", false)),
+    FALL_DAMAGE_VILLAGERS(new ConditionFlag("fall-damage-villagers", false)),
+    FALL_DAMAGE_MONSTERS(new ConditionFlag("fall-damage-monsters", false)),
+    FALL_DAMAGE_ANIMALS(new ConditionFlag("fall-damage-animals", false)),
+    FALL_DAMAGE_PLAYERS(new ConditionFlag("fall-damage-players", false)),
+    SEND_MESSAGE(new ConditionFlag("send-chat", false)),
+    EXECUTE_COMMAND(new ConditionFlag("exec-command", false)),
+    SET_SPAWN(new ConditionFlag("set-spawn", false)),
+    // RESET_SPAWN(new ConditionFlag("reset-spawn"), // not working
+    SLEEP(new ConditionFlag("sleep", false)),
+    USE_PORTAL(new ConditionFlag("use-portal", false)),
+    //TRAVEL_TO_DIM(new ConditionFlag("travel-to-dim", false)),
+    //TRAVEL_FROM_DIM(new ConditionFlag("travel-from-dim", false)),
+    USE_PORTAL_PLAYERS(new ConditionFlag("use-portal-players", false)),
+    USE_PORTAL_ITEMS(new ConditionFlag("use-portal-items", false)),
+    USE_PORTAL_ANIMALS(new ConditionFlag("use-portal-animals", false)),
+    USE_PORTAL_MONSTERS(new ConditionFlag("use-portal-monsters", false)),
+    USE_PORTAL_VILLAGERS(new ConditionFlag("use-portal-villagers", false)),
+    USE_PORTAL_MINECARTS(new ConditionFlag("use-portal-minecarts", false)),
+    SPAWN_PORTAL(new ConditionFlag("spawn-portal", false));
+    //PLAYER_SPAWN_PORTAL(new ConditionFlag("player-spawn-portal", false));
+
 
     public final String flagname;
     public final IFlag flag;
