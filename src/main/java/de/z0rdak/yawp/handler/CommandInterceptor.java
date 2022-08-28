9086dee5c9ee13dd126d94df6289a7847d95707c
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-08-28 15:50:30 +0200

    add dimCache null check

--- src/main/java/de/z0rdak/yawp/handler/CommandInterceptor.java
+++ src/main/java/de/z0rdak/yawp/handler/CommandInterceptor.java
@@ -3,17 +3,13 @@ package de.z0rdak.yawp.handler;
 import com.mojang.brigadier.context.CommandContextBuilder;
 import com.mojang.brigadier.context.ParsedArgument;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.tree.CommandNode;
 import de.z0rdak.yawp.YetAnotherWorldProtector;
 import de.z0rdak.yawp.commands.CommandConstants;
 import de.z0rdak.yawp.config.server.CommandPermissionConfig;
-import de.z0rdak.yawp.core.region.DimensionalRegion;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
-import de.z0rdak.yawp.util.CommandUtil;
 import de.z0rdak.yawp.util.MessageUtil;
 import net.minecraft.command.CommandSource;
-import net.minecraft.command.arguments.DimensionArgument;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.RegistryKey;
@@ -37,6 +33,7 @@ public class CommandInterceptor {
     /**
      * Handler for managing different command permissions.
      * Sketchy as hell. If CLI format changes, this breaks easily.
+     *
      * @param event
      */
     @SubscribeEvent
@@ -56,27 +53,31 @@ public class CommandInterceptor {
                     if (dimParsedArgument.getResult() instanceof ResourceLocation) {
                         ResourceLocation dimResLoc = ((ResourceLocation) dimParsedArgument.getResult());
                         RegistryKey<World> dim = RegistryKey.create(Registry.DIMENSION_REGISTRY, dimResLoc);
-                        DimensionRegionCache dimRegion = RegionDataManager.get().cacheFor(dim);
-                        if (src.getEntity() != null) {
-                            try {
-                                if (src.getEntity() instanceof PlayerEntity) {
-                                    ServerPlayerEntity player = src.getPlayerOrException();
-                                    boolean hasConfigPermission = CommandPermissionConfig.hasPermission(player);
-                                    if (!dimRegion.hasOwner(player) && !hasConfigPermission) {
-                                        YetAnotherWorldProtector.LOGGER.info("Player not allowed to manage dim");
-                                        MessageUtil.sendCmdFeedback(src, new StringTextComponent("You are not allowed to manage this dimensional region!"));
-                                        event.setCanceled(true);
+                        DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(dim);
+                        if (dimCache != null) {
+                            if (src.getEntity() != null) {
+                                try {
+                                    if (src.getEntity() instanceof PlayerEntity) {
+                                        ServerPlayerEntity player = src.getPlayerOrException();
+                                        boolean hasConfigPermission = CommandPermissionConfig.hasPlayerPermission(player);
+                                        if (!dimCache.hasOwner(player) && !hasConfigPermission) {
+                                            YetAnotherWorldProtector.LOGGER.info("Player not allowed to manage dim");
+                                            MessageUtil.sendCmdFeedback(src, new StringTextComponent("You are not allowed to manage this dimensional region!"));
+                                            event.setCanceled(true);
+                                        }
                                     }
+                                } catch (CommandSyntaxException e) {
+                                    YetAnotherWorldProtector.LOGGER.error(e);
                                 }
-                            } catch (CommandSyntaxException e) {
-                                YetAnotherWorldProtector.LOGGER.error(e);
+                            } else {
+                                if (!CommandPermissionConfig.hasPermission(src)) {
+                                    YetAnotherWorldProtector.LOGGER.info("' " + src.getTextName() + "' is not allowed to manage dim");
+                                    event.setCanceled(true);
+                                    MessageUtil.sendCmdFeedback(src, new StringTextComponent("You are not allowed to manage this dimensional region!"));
+                                }
                             }
                         } else {
-                            if (!CommandPermissionConfig.hasPermission(src)) {
-                                YetAnotherWorldProtector.LOGGER.info("' " + src.getTextName() + "' is not allowed to manage dim");
-                                event.setCanceled(true);
-                                MessageUtil.sendCmdFeedback(src, new StringTextComponent("You are not allowed to manage this dimensional region!"));
-                            }
+                            MessageUtil.sendCmdFeedback(src, new StringTextComponent("Dimension not found in region data"));
                         }
                     }
                 }
