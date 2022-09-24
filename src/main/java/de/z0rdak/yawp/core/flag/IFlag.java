aa578c6b350c1ae3009296016782aea60ac39e11
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-09-24 14:55:18 +0200

    add description for IFlag interface

--- src/main/java/de/z0rdak/yawp/core/flag/IFlag.java
+++ src/main/java/de/z0rdak/yawp/core/flag/IFlag.java
@@ -20,18 +20,9 @@ import net.minecraftforge.common.util.INBTSerializable;
  * Removes all blocks from blacklist
  * /rs flag remove <region> block-blacklist clear
  *
- *
- *
- *
  * NumberFlag
  * /rs flag add <region> max-level 30
  * /rs flag add <region>
- *
- *
- *
- *
- *
- *
  *
  * // Trigger
  * /rs trigger <region> on-leave clear-xp
@@ -40,19 +31,53 @@ import net.minecraftforge.common.util.INBTSerializable;
 public interface IFlag extends INBTSerializable<CompoundNBT> {
 
     /**
-     * Mod:Name -> ResourceLocation in future
-     * @return unique name for flag
+     * Get the unique identifier for the flag. <br>
+     * The valid flags are currently stored as an enum. <br>
+     * Mod:Name -> ResourceLocation in future.
+     * @see RegionFlag
+     * @return unique name for flag.
      */
     String getFlagIdentifier();
 
+    /**
+     * Returns the flag type of the flag.
+     * @see FlagType
+     * @return the flag type enum value of the flag.
+     */
     FlagType getFlagType();
 
+    /**
+     * Returns whether the flag is inverted for flag checks. <br>
+     * If true the flag is treated as a whitelist flag (flag action is allowed), <br>
+     * otherwise is treated like a blacklist (flag action is prohibited).
+     * @return true if the flag acts as a whitelist flag, false otherwise.
+     */
     boolean isInverted();
 
+    /**
+     * Set the inverted state of the flag.
+     * @param inverted true sets flag to act like a whitelist flag, false sets flag to be a blacklist flag.
+     */
     void setInverted(boolean inverted);
 
+    /**
+     * Returns whether the flag is active in the region. <br>
+     * Disabled flags are not considered for flag checks.
+     * @return true if flag is active, false otherwise.
+     */
     boolean isActive();
 
+    /**
+     * Set the active state of the flag.
+     * @param active true activates the flag, false disables the flag for checks.
+     */
     void setIsActive(boolean active);
 
+    /**
+     * FIXME: should work, but hacky workaround
+     * Returns whether the flag allows the flag action considering the optional arguments
+     * @param args optional arguments to be used in flag check
+     * @return whether the flag allows the flag action
+     */
+    boolean isAllowed(Object... args);
 }
