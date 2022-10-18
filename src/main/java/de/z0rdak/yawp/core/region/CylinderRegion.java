5509449678d076b76a75f0a762f0adf70174d009
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-10-18 16:24:25 +0200

    renaming

--- src/main/java/de/z0rdak/yawp/core/region/CylinderRegion.java
+++ src/main/java/de/z0rdak/yawp/core/region/CylinderRegion.java
@@ -1,6 +1,6 @@
 package de.z0rdak.yawp.core.region;
 
-import de.z0rdak.yawp.core.area.CylinderArea;
+import de.z0rdak.yawp.core.area.VerticalCylinderArea;
 import de.z0rdak.yawp.util.constants.RegionNBT;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
@@ -15,17 +15,17 @@ public final class CylinderRegion extends AbstractMarkableRegion {
         this.deserializeNBT(nbt);
     }
 
-    public CylinderRegion(String name, CylinderArea area, PlayerEntity owner, RegistryKey<World> dimension) {
+    public CylinderRegion(String name, VerticalCylinderArea area, PlayerEntity owner, RegistryKey<World> dimension) {
         this(name, area, new BlockPos(area.getCenter()), owner, dimension);
     }
 
-    public CylinderRegion(String name, CylinderArea area, BlockPos tpPos, PlayerEntity owner, RegistryKey<World> dimension) {
+    public CylinderRegion(String name, VerticalCylinderArea area, BlockPos tpPos, PlayerEntity owner, RegistryKey<World> dimension) {
         super(name, area, tpPos, owner, dimension);
     }
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
         super.deserializeNBT(nbt);
-        this.area = new CylinderArea(nbt.getCompound(RegionNBT.AREA));
+        this.area = new VerticalCylinderArea(nbt.getCompound(RegionNBT.AREA));
     }
 }
