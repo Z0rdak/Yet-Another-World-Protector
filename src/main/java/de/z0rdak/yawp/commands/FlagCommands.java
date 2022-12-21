5d1bcf86bce1a26b7b39ed0a57c41754a7218c80
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-12-21 07:38:18 +0100

    add cli implementations to set common states of flags

--- src/main/java/de/z0rdak/yawp/commands/FlagCommands.java
+++ src/main/java/de/z0rdak/yawp/commands/FlagCommands.java
@@ -1,11 +1,28 @@
 package de.z0rdak.yawp.commands;
 
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import de.z0rdak.yawp.config.server.CommandPermissionConfig;
+import de.z0rdak.yawp.commands.arguments.region.RegionArgumentType;
+import de.z0rdak.yawp.core.flag.BooleanFlag;
+import de.z0rdak.yawp.core.flag.FlagType;
+import de.z0rdak.yawp.core.flag.IFlag;
+import de.z0rdak.yawp.core.flag.RegionFlag;
+import de.z0rdak.yawp.core.region.IMarkableRegion;
+import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import de.z0rdak.yawp.util.MessageUtil;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.command.ISuggestionProvider;
+import net.minecraft.command.arguments.DimensionArgument;
+import net.minecraft.util.text.TranslationTextComponent;
 
-import static de.z0rdak.yawp.util.CommandUtil.literal;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static de.z0rdak.yawp.commands.CommandConstants.*;
+import static de.z0rdak.yawp.util.CommandUtil.*;
 
 /**
  * Management for flag values for List and Int Flags as well as default flag values
@@ -17,28 +34,49 @@ public final class FlagCommands {
     private FlagCommands(){}
 
     /**
-     * /wp flag <flag> <type> set <argument>
-     * /wp flag <flag> <type> add <argument>
-     * /wp flag <flag> <type> remove <argument>
      * /wp flag entity_spawning list add minecraft:zombie
-     * /wp flag entity_spawning list remove minecraft:zombie
+     * /wp flag entity_spawning list remove minecraft:zombie*
      *
-     * /wp flag <flag> mode whitelist|blacklist
-     * /wp flag <flag> activate <true|false>
-     * /wp flag <flag> remove-from <region>
-     * /wp flag <flag> add-to <region>
+     * /wp flag <dim> <region> add|remove|enable|disable|invert
+     *
+     * <type> int|bool|list
+     * /wp flag <dim> <region> <type> <flag> add|remove <entry>
+     * /wp flag <dim> <region> <type> <flag> set <value>
+     * /wp flag <dim> <region> <type> <flag> set interval|value <n>
+     * /wp flag <dim> <region> <flag> info
+     *
      * @return
      */
     private static LiteralArgumentBuilder<CommandSource> register() {
         return literal(CommandConstants.FLAG).executes(ctx -> promptHelp(ctx.getSource()));
     }
 
+    public static int setActiveState(CommandSource src, IMarkableRegion region, RegionFlag flag, boolean enable) {
+        IFlag regionFlag = region.getFlag(flag.name);
+        if (regionFlag != null) {
+            regionFlag.setIsActive(enable);
+            region.updateFlag(regionFlag);
+            RegionDataManager.save();
+            MessageUtil.sendCmdFeedback(src, new TranslationTextComponent("Set flag '%s' active state to '%s'", flag.name, enable));
+            return 0;
+        }
+        return 1;
+    }
 
-    public static int promptHelp(CommandSource src) {
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpHeader("help.flags.header"));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpSuggestionLink("help.flags.1", CommandConstants.FLAG, CommandConstants.ADD));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpSuggestionLink("help.flags.2", CommandConstants.FLAG, CommandConstants.REMOVE));
-        MessageUtil.sendCmdFeedback(src, MessageUtil.buildHelpSuggestionLink("help.flags.3", CommandConstants.FLAG, CommandConstants.LIST));
+    public static int setInvertState(CommandSource src, IMarkableRegion region, RegionFlag flag, boolean invert) {
+        IFlag regionFlag = region.getFlag(flag.name);
+        if (regionFlag != null) {
+            regionFlag.setInverted(invert);
+            region.updateFlag(regionFlag);
+            RegionDataManager.save();
+            MessageUtil.sendCmdFeedback(src, new TranslationTextComponent("Set flag '%s' inverted state to '%s'", flag.name, invert));
+            return 0;
+        }
+        return 1;
+    }
+
+    public static int promptBoolFlagInfo(CommandSource src, IMarkableRegion region, IFlag flag) {
+
         return 0;
     }
 
