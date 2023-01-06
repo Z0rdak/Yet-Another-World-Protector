8113f4d6728969b9ec29cd9e5ca0ba244e42814a
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-01-06 15:08:02 +0100

    implement commands to give and reset RegionMarker

--- src/main/java/de/z0rdak/yawp/commands/MarkerCommands.java
+++ src/main/java/de/z0rdak/yawp/commands/MarkerCommands.java
@@ -1,6 +1,5 @@
 package de.z0rdak.yawp.commands;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import de.z0rdak.yawp.YetAnotherWorldProtector;
@@ -19,20 +18,19 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
 
 import java.util.Objects;
 
 import static de.z0rdak.yawp.commands.CommandConstants.*;
-import static de.z0rdak.yawp.util.CommandUtil.*;
-import static de.z0rdak.yawp.util.MessageUtil.buildRegionInfoLink;
-import static de.z0rdak.yawp.util.MessageUtil.sendCmdFeedback;
+import static de.z0rdak.yawp.util.CommandUtil.getPlayerArgument;
+import static de.z0rdak.yawp.util.CommandUtil.literal;
+import static de.z0rdak.yawp.util.MessageUtil.*;
 import static de.z0rdak.yawp.util.StickUtil.STICK;
 import static de.z0rdak.yawp.util.StickUtil.getStickType;
 import static net.minecraft.util.text.TextFormatting.RED;
@@ -45,7 +43,7 @@ public final class MarkerCommands {
     }
 
     private static LiteralArgumentBuilder<CommandSource> register() {
-        return literal(FLAG)
+        return literal(MARKER)
                 .then(literal(GIVE)
                         .executes(ctx -> giveMarkerStick(ctx.getSource(), null))
                         .then(Commands.argument(PLAYER.toString(), EntityArgument.player())
@@ -56,11 +54,64 @@ public final class MarkerCommands {
                         .executes(ctx -> setParent(ctx.getSource(), getParentRegionArgument(ctx))))
                  */
                 .then(literal(RESET)
-                        .executes(ctx -> resetStick(ctx.getSource())));
-        /*
-        CREATE REGION
-         */
+                        .executes(ctx -> resetStick(ctx.getSource())))
+                /*
+                .then(literal(CREATE)
+                        .then(Commands.argument(NAME.toString(), StringArgumentType.word())
+                                .executes(ctx -> createRegion(ctx.getSource(), getRegionNameArgument(ctx), getDimCacheArgument(ctx), null)))
+                        .then(Commands.argument(PARENT.toString(), StringArgumentType.word())
+                                .executes(ctx -> createRegion(ctx.getSource(), getRegionNameArgument(ctx), getDimCacheArgument(ctx), getParentRegionArgument(ctx)))))
+                 */
+                ;
     }
+
+
+    private static int createRegion(CommandSource src, String regionName, DimensionRegionCache dimCache, IProtectedRegion parentRegion) {
+        int res = DimensionCommands.checkValidRegionName(regionName, dimCache);
+        if (res == -1) {
+            sendCmdFeedback(src, new TranslationTextComponent("cli.msg.dim.info.region.create.name.invalid", regionName));
+            return res;
+        }
+        if (res == 1) {
+            sendCmdFeedback(src, new TranslationTextComponent("cli.msg.dim.info.region.create.name.exists", dimCache.getDimensionalRegion().getName(), regionName));
+            return res;
+        }
+        // TODO: Check if player is allowed to create region - here or in command interceptor?
+
+        try {
+            PlayerEntity player = src.getPlayerOrException();
+            ItemStack maybeStick = player.getMainHandItem();
+            // TODO: create a method which throws exception on trying to get stick
+            if (StickUtil.isVanillaStick(maybeStick)) {
+                StickType stickType = StickUtil.getStickType(maybeStick);
+                if (stickType == StickType.MARKER) {
+                    CompoundNBT stickNBT = StickUtil.getStickNBT(maybeStick);
+                    if (stickNBT != null) {
+                        AbstractMarkableRegion region = LocalRegions.regionFrom(src.getPlayerOrException(), new MarkerStick(stickNBT), regionName);
+                        RegionDataManager.addFlags(RegionConfig.getDefaultFlags(), region);
+                        dimCache.addRegion(region);
+                        LocalRegions.ensureHigherRegionPriorityFor((CuboidRegion) region, RegionConfig.DEFAULT_REGION_PRIORITY.get());
+                        RegionDataManager.save();
+                        sendCmdFeedback(src, new TranslationTextComponent("cli.msg.dim.info.region.create.success", buildRegionInfoLink(region)));
+
+
+                    } else {
+                        // stick data corrupt
+                    }
+                } else {
+                    // Player not holding correct stick in hand
+                }
+            } else {
+                // Player not holding stick in hand
+                sendCmdFeedback(src, new StringTextComponent(RED + "").append(new TranslationTextComponent("cli.msg.dim.info.region.create.stick.missing")));
+                return -2;
+            }
+        } catch (CommandSyntaxException e) {
+            YetAnotherWorldProtector.LOGGER.error(e);
+        }
+        return 0;
+    }
+
 
     private static int resetStick(CommandSource src) {
         try {
@@ -74,14 +125,14 @@ public final class MarkerCommands {
                 if (Objects.requireNonNull(stickType) == StickType.MARKER) {
                     mainHandItem = StickUtil.initMarkerNbt(mainHandItem, StickType.MARKER, player.getCommandSenderWorld().dimension());
                     // FIXME: When different area types are available: Get stick, reset it, and save it back.
-                    sendCmdFeedback(src, new TranslationTextComponent("Reset marker successfully.'"));
+                    sendCmdFeedback(src, new TranslationTextComponent("RegionMarker reset!"));
                     return 0;
                 } else {
-                    sendCmdFeedback(src, new TranslationTextComponent(RED + "Hold stick type is not a marker!" + RESET));
+                    sendCmdFeedback(src, new TranslationTextComponent(RED + "The item in the main hand is not a RegionMarker!"));
                     return 1;
                 }
             } else {
-                sendCmdFeedback(src, new TranslationTextComponent(RED + "Hold stick type is not a marker!" + RESET));
+                sendCmdFeedback(src, new TranslationTextComponent(RED + "The item in the main hand is not a RegionMarker!"));
                 return 1;
             }
         } catch (CommandSyntaxException e) {
@@ -94,7 +145,7 @@ public final class MarkerCommands {
         return -1;
     }
 
-    public static int giveMarkerStick(CommandSource src, PlayerEntity player) {
+    public static int giveMarkerStick(CommandSource src, ServerPlayerEntity player) {
         try {
             PlayerEntity targetPlayer;
             if (player != null) {
@@ -102,9 +153,10 @@ public final class MarkerCommands {
             } else {
                 targetPlayer = src.getPlayerOrException();
             }
-            ItemStack markerStick = StickUtil.initMarkerNbt(Items.STICK.getDefaultInstance(), StickType.MARKER, targetPlayer.getCommandSenderWorld().dimension());
+            ItemStack markerStick = StickUtil.initMarkerNbt(Items.STICK.getDefaultInstance(), StickType.MARKER, targetPlayer.level.dimension());
             targetPlayer.addItem(markerStick);
-            sendCmdFeedback(src, new TranslationTextComponent("Added marker stick to inventory of player '" + player.getScoreboardName() + "'"));
+            sendCmdFeedback(src, new TranslationTextComponent("Added RegionMarker to inventory of player '" + targetPlayer.getScoreboardName() + "'"));
+            sendMessage(targetPlayer, new TranslationTextComponent("RegionMarker added to your inventory."));
         } catch (CommandSyntaxException e) {
             sendCmdFeedback(src, new TranslationTextComponent(RED + "Command needs a player as command source" + RESET));
             return 1;
