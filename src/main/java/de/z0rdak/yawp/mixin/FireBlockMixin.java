cc00abeb4ec3a711a083d801a684032cace0122b
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-01-27 21:57:50 +0100

    remove logic for mixin testing purposes

--- src/main/java/de/z0rdak/yawp/mixin/FireBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/FireBlockMixin.java
@@ -1,9 +1,5 @@
 package de.z0rdak.yawp.mixin;
 
-import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
-import de.z0rdak.yawp.handler.flags.HandlerUtil;
-import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
-import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
@@ -15,20 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-import static de.z0rdak.yawp.core.flag.RegionFlag.FIRE_SPREAD;
-
 
 @Mixin(FireBlock.class)
 public abstract class FireBlockMixin {
 
     @Inject(method = "tryCatchFire", at = @At(value = "HEAD"), cancellable = true, remap = false)
     private void spread(Level world, BlockPos pos, int spreadFactor, Random rand, int currentAge, Direction dir, CallbackInfo info) {
-        if (!world.isClientSide) {
-            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(world.dimension());
-            FlagCheckEvent flagCheckEvent = HandlerUtil.checkTargetEvent(pos, FIRE_SPREAD, dimCache.getDimensionalRegion());
-            if (flagCheckEvent.isDenied()) {
-                info.cancel();
-            }
-        }
+        info.cancel();
     }
 }
