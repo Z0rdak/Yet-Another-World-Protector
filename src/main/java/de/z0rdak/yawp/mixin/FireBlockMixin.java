f6938eff26f2d036cd48983e3e3813c41c6df3ed
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-01-30 08:08:05 +0100

    fix mixin for spreading fire flag

--- src/main/java/de/z0rdak/yawp/mixin/FireBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/FireBlockMixin.java
@@ -4,30 +4,24 @@ import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.block.FireBlock;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Random;
-
-
 @Mixin(FireBlock.class)
 public abstract class FireBlockMixin {
 
-    @Inject(method = "tryCatchFire", at = @At(value = "HEAD"), cancellable = true, remap = false)
-    private void spread(World world, BlockPos pos, int spreadFactor, Random rand, int currentAge, Direction dir, CallbackInfo info) {
+    @Inject(method = "trySpreadingFire", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    private void spread(World world, BlockPos pos, int spreadFactor, Random rand, int currentAge, CallbackInfo info) {
         if (!world.isClient) {
             DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(world.getRegistryKey());
-            /*
-            FlagCheckEvent flagCheckEvent = HandlerUtil.checkTargetEvent(pos, FIRE_DESTROY, dimCache.getDimensionalRegion());
-            if (flagCheckEvent.isDenied()) {
-               info.cancel();
-            }
-
-             */
+            //FlagCheckEvent flagCheckEvent = HandlerUtil.checkTargetEvent(pos, RegionFlag.DRAGON_BLOCK_PROT, dimCache.getDimensionalRegion());
+            // if (flagCheckEvent.isDenied()) {
+            info.cancel();
+            //}
         }
     }
 }
