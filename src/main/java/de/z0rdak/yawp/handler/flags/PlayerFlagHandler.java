b50c4603c06c42af7b4675793b939b7794094dc3
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-02-03 23:50:39 +0100

    add more specific use flags

--- src/main/java/de/z0rdak/yawp/handler/flags/PlayerFlagHandler.java
+++ src/main/java/de/z0rdak/yawp/handler/flags/PlayerFlagHandler.java
@@ -428,49 +428,38 @@ public final class PlayerFlagHandler {
                 boolean isContainer = targetEntity instanceof LecternBlockEntity || isLockableTileEntity;
 
                 // used to allow player to place blocks when shift clicking container or usable bock
-                boolean playerHasNoBlocksToPlaceInHands = player.getItemInHand(InteractionHand.MAIN_HAND).getItem().equals(Items.AIR)
-                        && player.getItemInHand(InteractionHand.OFF_HAND).getItem().equals(Items.AIR);
+                boolean hasEmptyHands = hasEmptyHands(player);
 
                 BlockHitResult pos = event.getHitVec();
                 if (pos != null && pos.getType() == HitResult.Type.BLOCK) {
-                    boolean isInteractableBlock = isInteractableBlock(event.getLevel().getBlockState(event.getPos()).getBlock());
-                    if (isInteractableBlock) {
-                        if (player.isShiftKeyDown() && playerHasNoBlocksToPlaceInHands || !player.isShiftKeyDown()) {
-                            FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getPos(), USE, dimCache.getDimensionalRegion());
-                            handleAndSendMsg(event, flagCheckEvent);
-                        }
+                    if (player.isShiftKeyDown() && hasEmptyHands || !player.isShiftKeyDown()) {
+                        FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getPos(), USE_BLOCKS, dimCache.getDimensionalRegion());
+                        handleAndSendMsg(event, flagCheckEvent);
                     }
                 }
+
+                if (!hasEmptyHands) {
+                    FlagCheckEvent.PlayerFlagEvent useItemCheck = checkPlayerEvent(player, event.getPos(), USE_ITEMS, dimCache.getDimensionalRegion());
+                    handleAndSendMsg(event, useItemCheck);
+                }
+
+                // Note: following flags are already covered with use_blocks
                 // check for ender chest access
                 if (isEnderChest) {
-                    if (player.isShiftKeyDown() && playerHasNoBlocksToPlaceInHands || !player.isShiftKeyDown()) {
+                    if (player.isShiftKeyDown() && hasEmptyHands || !player.isShiftKeyDown()) {
                         FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, targetEntity.getBlockPos(), ENDER_CHEST_ACCESS, dimCache.getDimensionalRegion());
                         handleAndSendMsg(event, flagCheckEvent);
                     }
                 }
                 // check for container access
                 if (isContainer) {
-                    if (player.isShiftKeyDown() && playerHasNoBlocksToPlaceInHands || !player.isShiftKeyDown()) {
+                    if (player.isShiftKeyDown() && hasEmptyHands || !player.isShiftKeyDown()) {
                         FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, targetEntity.getBlockPos(), CONTAINER_ACCESS, dimCache.getDimensionalRegion());
                         handleAndSendMsg(event, flagCheckEvent);
                     }
                 }
             }
         }
-    }
-
-    private static boolean isInteractableBlock(Block target) {
-        return target instanceof ButtonBlock ||
-                target instanceof DoorBlock ||
-                target instanceof TrapDoorBlock ||
-                target instanceof LeverBlock ||
-                target instanceof NoteBlock ||
-                target instanceof FenceGateBlock ||
-                target instanceof DaylightDetectorBlock ||
-                target instanceof DiodeBlock ||
-                target instanceof LecternBlock ||
-                target instanceof BeaconBlock ||
-                target instanceof BrewingStandBlock;
     }
 
     @SubscribeEvent
@@ -484,6 +473,63 @@ public final class PlayerFlagHandler {
                     FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getTarget().blockPosition(), CONTAINER_ACCESS, dimCache.getDimensionalRegion());
                     handleAndSendMsg(event, flagCheckEvent);
                 }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntityInteraction(PlayerInteractEvent.EntityInteractSpecific event) {
+        if (isServerSide(event)) {
+            Player player = event.getEntity();
+            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(event.getEntity()));
+            if (dimCache != null) {
+                FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getTarget().blockPosition(), USE_ENTITIES, dimCache.getDimensionalRegion());
+                handleAndSendMsg(event, flagCheckEvent);
+
+                if (!hasEmptyHands(player)) {
+                    FlagCheckEvent.PlayerFlagEvent useItemCheck = checkPlayerEvent(player, event.getPos(), USE_ITEMS, dimCache.getDimensionalRegion());
+                    handleAndSendMsg(event, useItemCheck);
+                }
+            }
+        }
+    }
+
+    private static boolean hasEmptyHands(Player player) {
+        return player.getItemInHand(InteractionHand.MAIN_HAND).getItem().equals(Items.AIR)
+                && player.getItemInHand(InteractionHand.OFF_HAND).getItem().equals(Items.AIR);
+    }
+
+    @SubscribeEvent
+    public static void onEntityInteraction(PlayerInteractEvent.EntityInteract event) {
+        if (isServerSide(event)) {
+            Player player = event.getEntity();
+            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(event.getEntity()));
+            if (dimCache != null) {
+                FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getTarget().blockPosition(), USE_ENTITIES, dimCache.getDimensionalRegion());
+                handleAndSendMsg(event, flagCheckEvent);
+
+                if (!hasEmptyHands(player)) {
+                    FlagCheckEvent.PlayerFlagEvent useItemCheck = checkPlayerEvent(player, event.getPos(), USE_ENTITIES, dimCache.getDimensionalRegion());
+                    handleAndSendMsg(event, useItemCheck);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntityInteraction(PlayerInteractEvent.RightClickItem event) {
+        if (isServerSide(event)) {
+            Player player = event.getEntity();
+            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(event.getEntity()));
+            if (dimCache != null) {
+
+                if (!hasEmptyHands(player)) {
+                    FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getPos(), USE_ENTITIES, dimCache.getDimensionalRegion());
+                    handleAndSendMsg(event, flagCheckEvent);
+                }
+
+                FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getPos(), USE_ITEMS, dimCache.getDimensionalRegion());
+                handleAndSendMsg(event, flagCheckEvent);
             }
         }
     }
@@ -506,7 +552,7 @@ public final class PlayerFlagHandler {
                     List<Player> players = event.getLevel().getEntities(EntityType.PLAYER, areaAbovePressurePlate, (player) -> true);
                     boolean isCanceledForOne = false;
                     for (Player player : players) {
-                        FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getPos(), USE, dimCache.getDimensionalRegion());
+                        FlagCheckEvent.PlayerFlagEvent flagCheckEvent = checkPlayerEvent(player, event.getPos(), USE_BLOCKS, dimCache.getDimensionalRegion());
                         isCanceledForOne = isCanceledForOne || handleAndSendMsg(event, flagCheckEvent);
                         event.setCanceled(isCanceledForOne);
                     }
