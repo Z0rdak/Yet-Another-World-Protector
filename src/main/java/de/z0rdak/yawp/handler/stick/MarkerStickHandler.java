6388b350f42d038326c713185bebb196980aa4e8
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-02-21 12:05:49 +0100

    implement marker interaction handler

--- src/main/java/de/z0rdak/yawp/handler/stick/MarkerStickHandler.java
+++ src/main/java/de/z0rdak/yawp/handler/stick/MarkerStickHandler.java
@@ -1,76 +1,94 @@
 package de.z0rdak.yawp.handler.stick;
 
+import de.z0rdak.yawp.YetAnotherWorldProtector;
+import de.z0rdak.yawp.core.area.AreaType;
+import de.z0rdak.yawp.core.stick.MarkerStick;
+import de.z0rdak.yawp.util.StickType;
+import de.z0rdak.yawp.util.StickUtil;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Objects;
+
+import static de.z0rdak.yawp.util.StickUtil.*;
+
 public class MarkerStickHandler {
 
-    /*
-    public static void onMarkBlock(ItemStack involvedItem, PlayerInteractEvent.RightClickBlock event) {
-        MarkerStick marker = new MarkerStick(involvedItem.getNbt().getCompound(STICK));
-        AreaType areaType = marker.getAreaType();
-        if (areaType == null) {
-            YetAnotherWorldProtector.LOGGER.warn("Unknown area type on marking - should really not happening");
-            return;
+    public static void onCreateStick(PlayerEntity player, ItemStack input, ItemStack output) {
+        StickType type = StickType.of(output.getName().getString());
+        if (type != StickType.UNKNOWN) {
+            // split stack and only create one stick, also refund xp
+            input.setCount(output.getCount() - 1);
+            player.giveItemStack(input);
+            player.getInventory().markDirty();
+            player.addExperienceLevels(1);
+            StickUtil.initMarkerNbt(output, type, player.getWorld().getRegistryKey());
         }
-        if (event.getPlayer().isShiftKeyDown()) {
-            marker.setTeleportPos(event.getPos());
-            involvedItem.getNbt().put(STICK, marker.serializeNBT());
-            return;
-        }
-        // add block to NBT list
-        marker.addMarkedBlock(event.getPos());
-        // check whether marked blocks form a valid marked area
-        marker.checkValidArea();
-        involvedItem.getNbt().put(STICK, marker.serializeNBT());
-        setStickName(involvedItem, StickType.MARKER);
     }
 
-
-    /**
-     * Create a region from the NBT data of the renamed region marker.
-     * TODO: Make parent selectable for creating region, this is needed to check if a player is allowed to create a region with the stick
-
-    public static void onCreateRegion(AnvilRepairEvent event) {
-        ItemStack outputItem = event.getItemResult();
-        PlayerEntity player = event.getPlayer();
-        NbtCompound stickNBT = outputItem.getNbt().getCompound(STICK);
-        StickType type = StickType.of(stickNBT.getString(STICK_TYPE));
-        if (type == StickType.MARKER) {
-            String regionName = outputItem.getName().getString();
-            MarkerStick marker = new MarkerStick(stickNBT);
-            if (marker.isValidArea()) {
-                AbstractMarkableRegion region = LocalRegions.regionFrom(player, marker, regionName);
-                if (region != null) {
-                    DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(player.getWorld().getRegistryKey());
-                    if (dimCache != null){
-                        dimCache.addRegion(region);
-                        LocalRegions.ensureHigherRegionPriorityFor((CuboidRegion) region, RegionConfig.DEFAULT_REGION_PRIORITY.get());
-                        marker.reset();
-                        outputItem.getNbt().put(STICK, marker.serializeNBT());
-                        setStickName(outputItem, type);
-                        // TODO: Reset marker on dimChange?
-                        RegionDataManager.save();
-                    } else {
-                        sendMessage(player, MutableText.of(new TranslatableTextContent("PlayerEntity dimension not matching marker data")));
+    public static void onMarkBlock(PlayerEntity player, ItemStack involvedItem, BlockPos target) {
+        if (!player.getWorld().isClient) {
+            // TODO: Maybe check if player is allowed to mark block
+            if (!involvedItem.equals(ItemStack.EMPTY) && isVanillaStick(involvedItem)) {
+                StickType stickType = getStickType(involvedItem);
+                if (Objects.requireNonNull(stickType) == StickType.MARKER) {
+                    MarkerStick marker = new MarkerStick(involvedItem.getNbt().getCompound(STICK));
+                    AreaType areaType = marker.getAreaType();
+                    if (areaType == null) {
+                        YetAnotherWorldProtector.LOGGER.warn("Unknown area type on marking - should really not happening");
+                        return;
                     }
-                } else {
-                    player.sendMessage(MutableText.of(new TranslatableTextContent("Invalid region type")));
+                    if (player.isCrawling()) {
+                        marker.setTeleportPos(target);
+                        involvedItem.getNbt().put(STICK, marker.serializeNBT());
+                        return;
+                    }
+                    // add block to NBT list
+                    marker.addMarkedBlock(target);
+                    // check whether marked blocks form a valid marked area
+                    marker.checkValidArea();
+                    involvedItem.getNbt().put(STICK, marker.serializeNBT());
+                    setStickName(involvedItem, StickType.MARKER);
                 }
-            } else {
-                player.sendMessage(MutableText.of(new TranslatableTextContent("Could not create region")));
             }
-        } else {
-            player.sendMessage(MutableText.of(new TranslatableTextContent("Invalid stick type / NBT data")));
         }
     }
 
-    public static void onCycleRegionMarker(ItemStack markerStickItem){
-        NbtCompound nbt = markerStickItem.getNbt();
-        MarkerStick marker = new MarkerStick(nbt.getCompound(STICK));
-        // change area nbt, reset marked blocks, set valid to false
-        marker.cycleMode();
-        // update stick name
-        markerStickItem.getNbt().put(STICK, marker.serializeNBT());
-        StickUtil.setStickName(markerStickItem, StickType.MARKER);
-    }
+    public static void onCycleMode(PlayerEntity player, ItemStack involvedItem) {
+        if (!player.getWorld().isClient) {
+            // is some valid mod stick
+            if (!involvedItem.equals(ItemStack.EMPTY)
+                    && hasNonNullTag(involvedItem)
+                    && involvedItem.getNbt().contains(STICK)) {
+                HitResult blockLookingAt = player.raycast(20.0d, 0.0f, false);
+                boolean targetIsAir;
+                if (blockLookingAt.getType() == HitResult.Type.BLOCK) {
+                    BlockPos blockpos = ((BlockHitResult) blockLookingAt).getBlockPos();
+                    BlockState blockstate = player.getWorld().getBlockState(blockpos);
+                    targetIsAir = blockstate.getBlock().equals(Blocks.AIR);
+                } else {
+                    targetIsAir = blockLookingAt.getType() == HitResult.Type.MISS;
+                }
 
-     */
+                if (player.isCrawling() && targetIsAir) {
+                    StickType stickType = getStickType(involvedItem);
+                    if (Objects.requireNonNull(stickType) == StickType.MARKER) {
+                        NbtCompound nbt = involvedItem.getNbt();
+                        MarkerStick marker = new MarkerStick(nbt.getCompound(STICK));
+                        // change area nbt, reset marked blocks, set valid to false
+                        marker.cycleMode();
+                        // update stick name
+                        involvedItem.getNbt().put(STICK, marker.serializeNBT());
+                        StickUtil.setStickName(involvedItem, StickType.MARKER);
+                    }
+                }
+            }
+        }
+    }
 }
