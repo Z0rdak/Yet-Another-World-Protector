031d1e42118844431301333edb01e704fd71988f
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-02-22 22:36:31 +0100

    add mixin for exec-command flag

--- src/main/java/de/z0rdak/yawp/mixin/CommandManagerMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/CommandManagerMixin.java
@@ -1,22 +1,44 @@
 package de.z0rdak.yawp.mixin;
 
 import com.mojang.brigadier.ParseResults;
+import de.z0rdak.yawp.core.flag.RegionFlag;
 import de.z0rdak.yawp.handler.CommandInterceptor;
+import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
+import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
+import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
+
 @Mixin(CommandManager.class)
 public abstract class CommandManagerMixin {
 
-    @Inject(method = "execute", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/CommandDispatcher;execute(Lcom/mojang/brigadier/ParseResults;)I"), cancellable = true, remap = false)
     public void execute(ParseResults<ServerCommandSource> parseResults, String command, CallbackInfoReturnable<Integer> cir) {
+        // check mod command permissions
         int result = CommandInterceptor.handleModCommands(parseResults, command);
         if (result != 0) {
             cir.setReturnValue(1);
+        }
+
+        // check exec-command flag
+        ServerCommandSource cmdSource = parseResults.getContext().getSource();
+        if (cmdSource.isExecutedByPlayer()) {
+            ServerPlayerEntity player = cmdSource.getPlayer();
+            if (player != null) {
+                DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(player));
+                FlagCheckEvent.PlayerFlagEvent flagCheck = checkPlayerEvent(player, player.getBlockPos(), RegionFlag.EXECUTE_COMMAND, dimCache.getDimensionalRegion());
+                if (flagCheck.isDenied()) {
+                    sendFlagDeniedMsg(flagCheck);
+                    cir.setReturnValue(1);
+                }
+            }
         }
     }
 }
