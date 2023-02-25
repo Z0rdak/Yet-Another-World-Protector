d43e961a6164851fe1b1f2253eaaf2f6e84735d4
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-02-25 18:02:47 +0100

    add mixing for xp drop flags

--- src/main/java/de/z0rdak/yawp/mixin/LivingEntityMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/LivingEntityMixin.java
@@ -1,14 +1,21 @@
 package de.z0rdak.yawp.mixin;
 
+import de.z0rdak.yawp.core.flag.RegionFlag;
+import de.z0rdak.yawp.core.region.DimensionalRegion;
 import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
+import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static de.z0rdak.yawp.core.flag.RegionFlag.*;
@@ -17,8 +24,12 @@ import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
+    @Shadow
+    @Nullable
+    protected PlayerEntity attackingPlayer;
+
     @Inject(method = "handleFallDamage", at = @At(value = "HEAD"), cancellable = true, allow = 1)
-    public void spawnChildFromBreeding(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+    public void onFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity self = (LivingEntity) (Object) this;
         if (!self.world.isClient) {
             DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(self));
@@ -53,5 +64,38 @@ public abstract class LivingEntityMixin {
                 }
             }
         }
+    }
+
+    @Inject(method = "dropXp", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ExperienceOrbEntity;spawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/Vec3d;I)V"), cancellable = true, allow = 1)
+    public void onXpDrop(CallbackInfo ci) {
+        LivingEntity self = (LivingEntity) (Object) this;
+        DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(self));
+        DimensionalRegion dimRegion = dimCache.getDimensionalRegion();
+        FlagCheckEvent flagCheckEvent = checkTargetEvent(self.getBlockPos(), RegionFlag.XP_DROP_ALL, dimRegion);
+        if (flagCheckEvent.isDenied()) {
+            ExperienceOrbEntity.spawn((ServerWorld) self.world, self.getPos(), 0);
+            ci.cancel();
+            return;
+        }
+        if (this.attackingPlayer != null) {
+            FlagCheckEvent.PlayerFlagEvent playerFlagCheckEvent = checkPlayerEvent(this.attackingPlayer, self.getBlockPos(), RegionFlag.XP_DROP_PLAYER, dimCache.getDimensionalRegion());
+            if (playerFlagCheckEvent.isDenied()) {
+                sendFlagDeniedMsg(playerFlagCheckEvent);
+                ExperienceOrbEntity.spawn((ServerWorld) self.world, self.getPos(), 0);
+                ci.cancel();
+
+            }
+        }
+        if (isMonster(self)) {
+            flagCheckEvent = checkTargetEvent(self.getBlockPos(), RegionFlag.XP_DROP_MONSTER, dimRegion);
+        } else {
+            flagCheckEvent = checkTargetEvent(self.getBlockPos(), RegionFlag.XP_DROP_OTHER, dimRegion);
+        }
+        if (flagCheckEvent.isDenied()) {
+            ExperienceOrbEntity.spawn((ServerWorld) self.world, self.getPos(), 0);
+            ci.cancel();
+        }
+
+
     }
 }
