4a718273c1f1bccc623dfe7a08cff4257dab2072
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-03-06 21:24:06 +0100

    add mixin for flag WITHER_BLOCK_PROT

---
+++ src/main/java/de/z0rdak/yawp/mixin/WitherEntityMixin.java
@@ -0,0 +1,29 @@
+package de.z0rdak.yawp.mixin;
+
+import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
+import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
+import de.z0rdak.yawp.managers.data.region.RegionDataManager;
+import net.minecraft.entity.boss.WitherEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static de.z0rdak.yawp.core.flag.RegionFlag.WITHER_BLOCK_PROT;
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.checkTargetEvent;
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.getEntityDim;
+
+@Mixin(WitherEntity.class)
+public abstract class WitherEntityMixin {
+    @Inject(method = "mobTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/boss/WitherEntity;canDestroy(Lnet/minecraft/block/BlockState;)Z"), cancellable = true, allow = 1)
+    public void onWitherDestroyBlocks(CallbackInfo ci) {
+        WitherEntity self = (WitherEntity) (Object) this;
+        if (!self.world.isClient) {
+            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(self));
+            FlagCheckEvent flagCheck = checkTargetEvent(self.getBlockPos(), WITHER_BLOCK_PROT, dimCache.getDimensionalRegion());
+            if (flagCheck.isDenied()) {
+                ci.cancel();
+            }
+        }
+    }
+}
