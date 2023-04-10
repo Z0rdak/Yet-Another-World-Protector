d210351fdabda128a7485ec37816205926a3f09b
Marcel Bartholet <Marcel.Bartholet@web.de>
2023-04-10 18:28:41 +0200

    fix mixin warning

--- src/main/java/de/z0rdak/yawp/mixin/FireBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/FireBlockMixin.java
@@ -3,9 +3,9 @@ package de.z0rdak.yawp.mixin;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.FireBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,12 +17,12 @@ import java.util.Random;
 @Mixin(FireBlock.class)
 public abstract class FireBlockMixin {
 
-    @Inject(method = "tryCatchFire", at = @At(value = "HEAD"), cancellable = true)
-    private void spread(Level world, BlockPos pos, int spreadFactor, Random rand, int currentAge, Direction dir, CallbackInfo info) {
+    @Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    private void onFireTick(BlockState state, ServerLevel world, BlockPos pos, Random rand, CallbackInfo info) {
         if (!world.isClientSide) {
             DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(world.dimension());
             /*
-            FlagCheckEvent flagCheckEvent = HandlerUtil.checkTargetEvent(pos, FIRE_DESTROY, dimCache.getDimensionalRegion());
+            FlagCheckEvent flagCheckEvent = HandlerUtil.checkTargetEvent(pos, NO_FIRE_TICK, dimCache.getDimensionalRegion());
             if (flagCheckEvent.isDenied()) {
                info.cancel();
             }
