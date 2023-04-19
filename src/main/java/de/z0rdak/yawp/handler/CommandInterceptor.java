a714c215fc304f6180a3d463d7c8408294d69392
Marcel Bartholet <Marcel.Bartholet@web.de>
2023-04-19 22:43:51 +0200

    fix command interceptor not considering player permission for owned regions; fix event handler priority; update changelog and bump version

--- src/main/java/de/z0rdak/yawp/handler/CommandInterceptor.java
+++ src/main/java/de/z0rdak/yawp/handler/CommandInterceptor.java
@@ -76,29 +76,29 @@ public class CommandInterceptor {
                 if (src.getEntity() instanceof Player) {
                     ServerPlayer player = src.getPlayerOrException();
                     boolean hasConfigPermission = hasPlayerPermission(player);
+                    boolean isOwner = region.hasOwner(player.getUUID()) || (player.getTeam() != null && region.hasOwner(player.getTeam().getName()));
+                    boolean isOwnerOfParent = region.getParent() != null && region.getParent().hasOwner(player.getUUID())
+                            || (player.getTeam() != null && region.getParent().hasOwner(player.getTeam().getName()));
                     boolean containsInfoCmd = nodeNames.contains(INFO.toString()) || nodeNames.contains(LIST.toString()) || nodeNames.contains(SPATIAL.toString());
+
                     // /wp region <dim> <region> info|list|spatial|state
                     if (cmdNodes.size() == 4 || (cmdNodes.size() > 4 && containsInfoCmd) || cmdNodes.size() == 5 && nodeNames.get(4).equals(STATE.toString())) {
-                        event.setCanceled(!(AllowInfoCmds() || hasConfigPermission));
+                        event.setCanceled(!(isOwner || AllowInfoCmds() || hasConfigPermission));
                         if (event.isCanceled()) {
-                            sendCmdFeedback(src, Component.translatable(  "cli.msg.dim.info.region.info.deny", buildRegionInfoLink(region, LOCAL)));
+                            sendCmdFeedback(src, Component.translatable("cli.msg.dim.info.region.info.deny", buildRegionInfoLink(region, LOCAL)));
                         }
                         return;
                     }
                     // check if player is owner of parent region or has permission to update region area
                     if (cmdNodes.size() > 4 && nodeNames.contains(AREA.toString())) {
-                        if (!region.getParent().hasOwner(player.getUUID())
-                                && (player.getTeam() == null || !region.getParent().hasOwner(player.getTeam().getName()))
-                                && !hasConfigPermission) {
+                        if (!isOwnerOfParent && !isOwner && !hasConfigPermission) {
                             YetAnotherWorldProtector.LOGGER.info("Player not allowed to manage region '" + region.getName() + "'");
                             sendCmdFeedback(src, Component.translatable("cli.msg.dim.info.region.modify.local.deny", buildRegionInfoLink(region, LOCAL)));
                             event.setCanceled(true);
                         }
                     }
                     // check permission for other commands
-                    if (!region.hasOwner(player.getUUID())
-                            && (player.getTeam() == null || !region.hasOwner(player.getTeam().getName()))
-                            && !hasConfigPermission) {
+                    if (!isOwner && !hasConfigPermission) {
                         YetAnotherWorldProtector.LOGGER.info("Player not allowed to manage region '" + region.getName() + "'");
                         sendCmdFeedback(src, Component.translatable("cli.msg.dim.info.region.modify.local.deny", buildRegionInfoLink(region, LOCAL)));
                         event.setCanceled(true);
@@ -131,20 +131,22 @@ public class CommandInterceptor {
             DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(dim);
             try {
                 if (src.getEntity() instanceof Player) {
-                    ServerPlayer player = src.getPlayerOrException();
-                    boolean hasConfigPermission = hasPlayerPermission(player);
-                    // check for info cmd permission
-                    boolean isInfoCmd = (nodeNames.size() > 3 && nodeNames.contains(INFO.toString()) || nodeNames.contains(LIST.toString()));
-                    if (nodeNames.size() == 3 || isInfoCmd) {
-                        event.setCanceled(!(AllowInfoCmds() || hasConfigPermission));
-                        if (event.isCanceled()) {
-                            sendCmdFeedback(src, Component.translatable(  "cli.msg.dim.info.region.info.deny", buildRegionInfoLink(dimCache.getDimensionalRegion(), DIMENSION)));
-                        }
-                        return;
-                    }
-                    // check permission for other commands
                     if (dimCache != null) {
-                        if (!dimCache.hasOwner(player) && !hasConfigPermission) {
+                        ServerPlayer player = src.getPlayerOrException();
+                        boolean hasConfigPermission = hasPlayerPermission(player);
+                        boolean isOwner = dimCache.hasOwner(player);
+
+                        // check for info cmd permission
+                        boolean isInfoCmd = (nodeNames.size() > 3 && nodeNames.contains(INFO.toString()) || nodeNames.contains(LIST.toString()));
+                        if (nodeNames.size() == 3 || isInfoCmd) {
+                            event.setCanceled(!(isOwner || AllowInfoCmds() || hasConfigPermission));
+                            if (event.isCanceled()) {
+                                sendCmdFeedback(src, Component.translatable("cli.msg.dim.info.region.info.deny", buildRegionInfoLink(dimCache.getDimensionalRegion(), DIMENSION)));
+                            }
+                            return;
+                        }
+                        // check permission for other commands
+                        if (!isOwner && !hasConfigPermission) {
                             YetAnotherWorldProtector.LOGGER.info("Player not allowed to manage dim");
                             sendCmdFeedback(src, Component.translatable("cli.msg.dim.info.region.modify.dim.deny", buildRegionInfoLink(dimCache.getDimensionalRegion(), DIMENSION)));
                             event.setCanceled(true);
