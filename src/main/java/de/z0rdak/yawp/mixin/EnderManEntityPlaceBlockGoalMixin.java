7bd70da3339e7074ef073d8f240f82446fcd9463
Peter Svensson <petersv@psv.nu>
2023-05-27 08:52:45 +0200

    implement the enderman griefing flag

---
+++ src/main/java/de/z0rdak/yawp/mixin/EnderManEntityPlaceBlockGoalMixin.java
@@ -0,0 +1,36 @@
+package de.z0rdak.yawp.mixin;
+
+import de.z0rdak.yawp.core.region.DimensionalRegion;
+import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
+import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
+import de.z0rdak.yawp.managers.data.region.RegionDataManager;
+import net.minecraft.entity.mob.EndermanEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static de.z0rdak.yawp.core.flag.RegionFlag.ENDERMAN_GRIEFING;
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.checkTargetEvent;
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.getEntityDim;
+
+@Mixin(targets = "net.minecraft.entity.mob.EndermanEntity$PlaceBlockGoal")
+public abstract class EnderManEntityPlaceBlockGoalMixin {
+    @Shadow private EndermanEntity enderman;
+
+    @Inject(method = "canStart()Z", at = @At(value = "HEAD"), cancellable = true, allow = 1)
+    public void onCanStart(CallbackInfoReturnable<Boolean> cir) {
+        EndermanEntity self = enderman;
+        // Early return for the common case
+        if (this.enderman.getCarriedBlock() == null) {
+            cir.setReturnValue(false);
+        }
+        DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(self));
+        DimensionalRegion dimRegion = dimCache.getDimensionalRegion();
+        FlagCheckEvent flagCheck = checkTargetEvent(self.getBlockPos(), ENDERMAN_GRIEFING, dimRegion);
+        if (flagCheck.isDenied()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
