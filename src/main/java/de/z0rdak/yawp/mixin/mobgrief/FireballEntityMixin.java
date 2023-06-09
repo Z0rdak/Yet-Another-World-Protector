e3b6517ff50a89ba38a43b0f1f011887479c460e
Marcel Bartholet <Marcel.Bartholet@web.de>
2023-06-09 10:56:35 +0200

    fix wrong mixin ref

--- src/main/java/de/z0rdak/yawp/mixin/mobgrief/FireballEntityMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/mobgrief/FireballEntityMixin.java
@@ -11,7 +11,7 @@ import net.minecraft.entity.projectile.FireballEntity;
 @Mixin(FireballEntity.class)
 public class FireballEntityMixin {
 
-    @Inject(method = "onCollision(Lnet/minecraft/util/hit/HitResult;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion()Lnet/minecraft/world/explosion/Explosion;"), cancellable = true, allow = 1)
+    @Inject(method = "onCollision(Lnet/minecraft/util/hit/HitResult;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"), cancellable = true, allow = 1)
     public void onBlockHit(CallbackInfo ci) {
         FireballEntity self = (FireballEntity) (Object) this;
         if (MobGriefingHelper.preventGrief(self)) {
