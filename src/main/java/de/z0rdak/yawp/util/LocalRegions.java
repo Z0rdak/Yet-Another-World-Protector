dc0107f65b1f3dfd9379c8e8f741377fc01f9e55
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-09-01 14:35:31 +0200

    remove unused methods

--- src/main/java/de/z0rdak/yawp/util/LocalRegions.java
+++ src/main/java/de/z0rdak/yawp/util/LocalRegions.java
@@ -123,7 +123,6 @@ public final class LocalRegions {
                 .collect(Collectors.toList());
     }
 
-    @Nullable
     public static IMarkableRegion getInvolvedRegionFor(BlockPos position, RegistryKey<World> dim) {
         List<IMarkableRegion> regionsForPos = getInvolvedRegionsFor(position, dim);
         if (regionsForPos.isEmpty()) {
@@ -142,43 +141,6 @@ public final class LocalRegions {
                 // position check should always be the last check to do, because it is the most computation expensive
                 .filter(region -> region.contains(position))
                 .collect(Collectors.toList());
-    }
-
-    public static List<IMarkableRegion> getInvolvedRegionsFor(BlockPos position, PlayerEntity player, RegistryKey<World> dim) {
-        return RegionDataManager.get().getRegionsFor(dim).stream()
-                .filter(IMarkableRegion::isActive)
-                //.filter(region -> !region.permits(player))
-                // position check should always be the last check to do, because it is the most computation expensive
-                .filter(region -> region.contains(position))
-                .collect(Collectors.toList());
-    }
-
-    @Nullable
-    public static IMarkableRegion getInvolvedRegionFor(RegionFlag flag, BlockPos position, RegistryKey<World> dim) {
-        List<IMarkableRegion> regionsForPos = getInvolvedRegionsFor(flag, position, dim);
-        if (regionsForPos.isEmpty()) {
-            return null;
-        } else {
-            return Collections.max(regionsForPos, Comparator.comparing(IMarkableRegion::getPriority));
-        }
-    }
-
-    public static IMarkableRegion getInvolvedRegionFor(RegionFlag flag, BlockPos position, PlayerEntity player, RegistryKey<World> dim) {
-        List<IMarkableRegion> regionsForPos = getInvolvedRegionsFor(flag, position, player, dim);
-        if (regionsForPos.isEmpty()) {
-            return null;
-        } else {
-            return Collections.max(regionsForPos, Comparator.comparing(IMarkableRegion::getPriority));
-        }
-    }
-
-    public static IMarkableRegion getInvolvedRegionFor(BlockPos position, PlayerEntity player, RegistryKey<World> dim) {
-        List<IMarkableRegion> regionsForPos = getInvolvedRegionsFor(position, player, dim);
-        if (regionsForPos.isEmpty()) {
-            return null;
-        } else {
-            return Collections.max(regionsForPos, Comparator.comparing(IMarkableRegion::getPriority));
-        }
     }
 
     /**
