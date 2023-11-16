c0f4d0febbfdcfd79c57caf9c6e23b63289172e0
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-11-16 20:15:39 +0100

    implement getHull for SphereArea

--- src/main/java/de/z0rdak/yawp/core/area/SphereArea.java
+++ src/main/java/de/z0rdak/yawp/core/area/SphereArea.java
@@ -5,6 +5,10 @@ import de.z0rdak.yawp.util.AreaUtil;
 import de.z0rdak.yawp.util.constants.AreaNBT;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.phys.AABB;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static de.z0rdak.yawp.util.AreaUtil.distance;
 
@@ -32,7 +36,7 @@ public class SphereArea extends CenteredArea {
     }
 
     // TODO: MAX sphere radius
-    public SphereArea(BlockPos middlePos, int radius){
+    public SphereArea(BlockPos middlePos, int radius) {
         this(middlePos, new BlockPos(middlePos)
                 .offset(0, radius, 0));
     }
@@ -40,6 +44,17 @@ public class SphereArea extends CenteredArea {
     @Override
     public boolean contains(BlockPos pos) {
         return distance(this.center, pos) < this.radius + 0.5;
+    }
+
+    @Override
+    public Set<BlockPos> getHull() {
+        AABB cube = new AABB(new BlockPos(this.center).offset(-this.radius, -this.radius, -this.radius),
+                new BlockPos(this.center).offset(this.radius, this.radius, this.radius));
+        Set<BlockPos> cubeBlocks = AreaUtil.blocksBetween(cube);
+        return cubeBlocks.stream()
+                .filter(blockPos -> distance(this.center, blockPos) > this.radius + 0.5)
+                .filter(blockPos -> distance(this.center, blockPos) < this.radius - 0.5)
+                .collect(Collectors.toSet());
     }
 
     @Override
