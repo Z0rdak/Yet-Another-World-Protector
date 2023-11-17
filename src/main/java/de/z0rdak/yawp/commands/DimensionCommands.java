b3f8028ff15b17128169060a9ef51ed821edfb7f
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2023-11-17 15:42:46 +0100

    add constant

--- src/main/java/de/z0rdak/yawp/commands/DimensionCommands.java
+++ src/main/java/de/z0rdak/yawp/commands/DimensionCommands.java
@@ -106,15 +106,15 @@ public class DimensionCommands {
                                                 //        .suggests((ctx, builder) -> AreaArgumentType.areaType().listSuggestions(ctx, builder))
                                                 //        .executes(ctx -> createRegion(ctx.getSource(), getRegionNameArgument(ctx), getDimCacheArgument(ctx), getAreaTypeArgument(ctx))))
                                                 .then(Commands.literal(AreaType.CUBOID.areaType)
-                                                        .then(Commands.argument("pos1", BlockPosArgument.blockPos())
-                                                                .then(Commands.argument("pos2", BlockPosArgument.blockPos())
+                                                        .then(Commands.argument(POS1.toString(), BlockPosArgument.blockPos())
+                                                                .then(Commands.argument(POS2.toString(), BlockPosArgument.blockPos())
                                                                         .executes(ctx -> createCuboidRegion(ctx, getRegionNameArgument(ctx), getDimCacheArgument(ctx),
-                                                                                BlockPosArgument.getSpawnablePos(ctx, "pos1"),
-                                                                                BlockPosArgument.getSpawnablePos(ctx, "pos2"), null))
+                                                                                BlockPosArgument.getSpawnablePos(ctx, POS1.toString()),
+                                                                                BlockPosArgument.getSpawnablePos(ctx, POS2.toString()), null))
                                                                         .then(Commands.argument(CommandConstants.OWNER.toString(), EntityArgument.player())
                                                                                 .executes(ctx -> createCuboidRegion(ctx, getRegionNameArgument(ctx), getDimCacheArgument(ctx),
-                                                                                        BlockPosArgument.getSpawnablePos(ctx, "pos1"),
-                                                                                        BlockPosArgument.getSpawnablePos(ctx, "pos2"), getOwnerArgument(ctx))))))
+                                                                                        BlockPosArgument.getSpawnablePos(ctx, POS1.toString()),
+                                                                                        BlockPosArgument.getSpawnablePos(ctx, POS2.toString()), getOwnerArgument(ctx))))))
                                                 )
                                         )
                                 )
