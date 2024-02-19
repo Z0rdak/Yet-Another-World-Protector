1a958cf5f06ff91cc243ea687277234bec64477d
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-02-19 11:18:02 +0100

    refactor code and add comments

--- src/main/java/de/z0rdak/yawp/util/MojangApiHelper.java
+++ src/main/java/de/z0rdak/yawp/util/MojangApiHelper.java
@@ -26,7 +26,6 @@ public class MojangApiHelper {
     private MojangApiHelper() {
     }
 
-    // TODO: Make async
     @Nullable
     public static GameProfile getGameProfileInfo(UUID uuid, Consumer<GameProfile> onResult) {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
@@ -35,24 +34,33 @@ public class MojangApiHelper {
             HttpGet httpGet = new HttpGet(uri);
             try {
                 CloseableHttpResponse response = httpclient.execute(httpGet);
-                if (response.getStatusLine().getStatusCode() == 200) {
-                    Reader reader = new InputStreamReader(response.getEntity().getContent(), UTF_8);
-                    // TODO: Test - ignore other properties of response
-                    MojangProfileResponse profileResponse = new Gson().fromJson(reader, MojangProfileResponse.class);
-                    String uuidStr = profileResponse.id.replaceAll(
-                            "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-                    assert uuidStr.equals(uuid.toString());
-                    return new GameProfile(UUID.fromString(uuidStr), profileResponse.name);
+                GameProfile gameProfile = deserializeGameProfile(response);
+                if (gameProfile != null) {
+                    return gameProfile;
                 }
+                onResult.accept(null);
                 return null;
             } catch (IOException e) {
                 YetAnotherWorldProtector.LOGGER.error("Error fetching game profile info for player '{}': {}", uuid.toString(), e);
                 throw e;
             }
         } catch (IOException e) {
+            onResult.accept(null);
             YetAnotherWorldProtector.LOGGER.error("Error fetching game profile info for player '{}': {}", uuid.toString(), e);
             return null;
         }
+    }
+
+    @Nullable
+    private static GameProfile deserializeGameProfile(CloseableHttpResponse response) throws IOException {
+        if (response.getStatusLine().getStatusCode() == 200) {
+            Reader reader = new InputStreamReader(response.getEntity().getContent(), UTF_8);
+            MojangProfileResponse profileResponse = new Gson().fromJson(reader, MojangProfileResponse.class);
+            String uuidStr = profileResponse.id.replaceAll(
+                    "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+            return new GameProfile(UUID.fromString(uuidStr), profileResponse.name);
+        }
+        return null;
     }
 
     @Nullable
@@ -67,13 +75,11 @@ public class MojangApiHelper {
                     YetAnotherWorldProtector.LOGGER.error("Could not retrieve game profile for player " + username);
                     return null;
                 }
-                if (response.getStatusLine().getStatusCode() == 200) {
-                    Reader reader = new InputStreamReader(response.getEntity().getContent(), UTF_8);
-                    MojangProfileResponse profileResponse = new Gson().fromJson(reader, MojangProfileResponse.class);
-                    String uuidStr = profileResponse.id.replaceAll(
-                            "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-                    return new GameProfile(UUID.fromString(uuidStr), profileResponse.name);
+                GameProfile gameProfile = deserializeGameProfile(response);
+                if (gameProfile != null) {
+                    return gameProfile;
                 }
+                onResult.accept(null);
                 return null;
             } catch (IOException e) {
                 YetAnotherWorldProtector.LOGGER.error("Error fetching game profile info for player '{}': {}", username, e);
@@ -81,6 +87,7 @@ public class MojangApiHelper {
             }
         } catch (IOException e) {
             YetAnotherWorldProtector.LOGGER.error("Error fetching game profile info for player '{}': {}", username, e);
+            onResult.accept(null);
             return null;
         }
     }
@@ -88,12 +95,17 @@ public class MojangApiHelper {
     @Nullable
     public static GameProfile lookupGameProfileInCache(CommandContext<CommandSource> ctx, String playerName) {
         PlayerProfileCache profileCache = ctx.getSource().getServer().getProfileCache();
+        // Uses Mojang's API to retrieve info from player repo. It invokes
+        // YggdrasilGameProfileRepository.findProfilesByNames through the PlayerProfileCache
+        // which itself makes an HTTP request to Mojang's API
         return profileCache.get(playerName);
     }
 
     @Nullable
     public static GameProfile lookupGameProfileInCache(CommandContext<CommandSource> ctx, UUID uuid) {
         PlayerProfileCache profileCache = ctx.getSource().getServer().getProfileCache();
+        // This in contrast to the name search does not make an HTTP request
+        // It just looks up the profile in the cache
         return profileCache.get(uuid);
     }
 
