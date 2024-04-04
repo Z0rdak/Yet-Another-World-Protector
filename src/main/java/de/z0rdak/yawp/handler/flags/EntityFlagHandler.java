e62853b510f30b3e2a4b8f9f82fbc98789d0b0b1
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-04-04 22:15:33 +0200

    rework flag checks for EntityFlagHandler

--- src/main/java/de/z0rdak/yawp/handler/flags/EntityFlagHandler.java
+++ src/main/java/de/z0rdak/yawp/handler/flags/EntityFlagHandler.java
@@ -1,11 +1,12 @@
 package de.z0rdak.yawp.handler.flags;
 
 import de.z0rdak.yawp.YetAnotherWorldProtector;
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.api.events.region.FlagCheckResult;
+import de.z0rdak.yawp.core.flag.FlagState;
 import de.z0rdak.yawp.core.flag.RegionFlag;
-import de.z0rdak.yawp.core.region.DimensionalRegion;
-import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
-import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.LivingEntity;
@@ -17,17 +18,23 @@ import net.minecraft.world.entity.monster.Shulker;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.WanderingTrader;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.EntityTeleportEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.function.Consumer;
+
 import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.FORGE;
 
-@Mod.EventBusSubscriber(modid = YetAnotherWorldProtector.MODID, value = Dist.DEDICATED_SERVER, bus = FORGE)
+@Mod.EventBusSubscriber(modid = YetAnotherWorldProtector.MODID, bus = FORGE)
 public class EntityFlagHandler {
 
     private EntityFlagHandler() {
@@ -36,38 +43,23 @@ public class EntityFlagHandler {
     @SubscribeEvent
     public static void onEnderTeleportTo(EntityTeleportEvent.EnderEntity event) {
         if (isServerSide(event)) {
-            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(event.getEntity()));
-            if (dimCache != null) {
-                DimensionalRegion dimRegion = dimCache.getDimensionalRegion();
-                // handle enderman teleportation
-                if (event.getEntityLiving() instanceof EnderMan) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(new BlockPos(event.getPrev()), RegionFlag.ENDERMAN_TELEPORT_FROM_REGION, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
-                    /*
-                    flagCheckEvent = checkTargetEvent(new BlockPos(event.getTarget()), RegionFlag.ENDERMAN_TELEPORT_TO_REGION, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
-                     */
+            ResourceKey<Level> dim = getEntityDim(event.getEntity());
+            // handle enderman teleportation
+            BlockPos target = new BlockPos(event.getPrev());
+            if (event.getEntityLiving() instanceof EnderMan) {
+                FlagCheckEvent checkEvent = new FlagCheckEvent(target, RegionFlag.ENDERMAN_TELEPORT_FROM_REGION, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                // handle shulker teleportation
-                if (event.getEntityLiving() instanceof Shulker) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(new BlockPos(event.getPrev()), RegionFlag.SHULKER_TELEPORT_FROM_REGION, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                    }
-                    /*
-                    flagCheckEvent = checkTargetEvent(new BlockPos(event.getTarget()), RegionFlag.SHULKER_TELEPORT_TO_REGION, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
-                     */
+                processCheck(checkEvent, null, denyResult -> event.setCanceled(true));
+            }
+            // handle shulker teleportation
+            if (event.getEntityLiving() instanceof Shulker) {
+                FlagCheckEvent checkEvent = new FlagCheckEvent(target, RegionFlag.SHULKER_TELEPORT_FROM_REGION, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
+                processCheck(checkEvent, null, denyResult -> event.setCanceled(true));
             }
         }
     }
@@ -76,51 +68,49 @@ public class EntityFlagHandler {
     public static void onFall(LivingFallEvent event) {
         if (isServerSide(event)) {
             LivingEntity entity = event.getEntityLiving();
-            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(event.getEntity()));
-            if (dimCache != null) {
-                DimensionalRegion dimRegion = dimCache.getDimensionalRegion();
-                FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.FALL_DAMAGE, dimRegion);
-                event.setCanceled(flagCheckEvent.isDenied());
-                if (event.isCanceled()) {
-                    event.setDistance(0.0f);
-                    event.setDamageMultiplier(0.0f);
+            ResourceKey<Level> dim = getEntityDim(event.getEntity());
+            BlockPos target = entity.blockPosition();
+            Player player = entity instanceof Player ? (Player) entity : null;
+            Consumer<FlagCheckResult> onDenyHandler = denyResult -> {
+                event.setCanceled(true);
+                event.setDistance(0.0f);
+                event.setDamageMultiplier(0.0f);
+            };
+
+            FlagCheckEvent checkEvent = new FlagCheckEvent(target, RegionFlag.FALL_DAMAGE, dim, player);
+            if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                return;
+            }
+            FlagState flagState = processCheck(checkEvent, null, onDenyHandler);
+            if (flagState == FlagState.DENIED)
+                return;
+
+            if (isPlayer(entity)) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.FALL_DAMAGE_PLAYERS, dim, player);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
                     return;
                 }
-                if (isPlayer(entity)) {
-                    flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.FALL_DAMAGE_PLAYERS, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        event.setDistance(0.0f);
-                        event.setDamageMultiplier(0.0f);
-                        return;
-                    }
+            }
+            if (isVillager(entity)) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.FALL_DAMAGE_VILLAGERS, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (isVillager(entity)) {
-                    flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.FALL_DAMAGE_VILLAGERS, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        event.setDistance(0.0f);
-                        event.setDamageMultiplier(0.0f);
-                        return;
-                    }
+            }
+            if (isAnimal(entity)) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.FALL_DAMAGE_ANIMALS, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (isAnimal(entity)) {
-                    flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.FALL_DAMAGE_ANIMALS, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        event.setDistance(0.0f);
-                        event.setDamageMultiplier(0.0f);
-                        return;
-                    }
+            }
+            if (isMonster(entity)) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.FALL_DAMAGE_MONSTERS, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (isMonster(entity)) {
-                    flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.FALL_DAMAGE_MONSTERS, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        event.setDistance(0.0f);
-                        event.setDamageMultiplier(0.0f);
-                    }
-                }
+            }
+            if (checkEvent != null) {
+                processCheck(checkEvent, null, onDenyHandler);
             }
         }
     }
@@ -128,63 +118,65 @@ public class EntityFlagHandler {
     @SubscribeEvent
     public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
         if (isServerSide(event)) {
-            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(event.getEntity()));
-            if (dimCache != null) {
-                DimensionalRegion dimRegion = dimCache.getDimensionalRegion();
-                Entity entity = event.getEntity();
-                if (entity instanceof Mob) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_ALL, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+            Entity entity = event.getEntity();
+            ResourceKey<Level> dim = getEntityDim(event.getEntity());
+            BlockPos target = entity.blockPosition();
+            Consumer<FlagCheckResult> onDenyHandler = denyResult -> event.setCanceled(true);
+
+            FlagCheckEvent checkEvent = null;
+            if (entity instanceof Mob) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_ALL, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (isAnimal(entity)) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_ANIMAL, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+                FlagState flagState = processCheck(checkEvent, null, onDenyHandler);
+                if (flagState == FlagState.DENIED)
+                    return;
+            }
+            if (isAnimal(entity)) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_ANIMAL, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (isMonster(entity)) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_MONSTER, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+            }
+            if (isMonster(entity)) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_MONSTER, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (entity instanceof SnowGolem || entity instanceof IronGolem) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_GOLEM, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+            }
+            if (entity instanceof SnowGolem || entity instanceof IronGolem) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_GOLEM, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (entity instanceof Villager) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_VILLAGER, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+            }
+            if (entity instanceof Villager) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_VILLAGER, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (entity instanceof WanderingTrader) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_TRADER, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+            }
+            if (entity instanceof WanderingTrader) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_TRADER, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (entity instanceof Slime) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_SLIME, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
-                    if (event.isCanceled()) {
-                        return;
-                    }
+            }
+            if (entity instanceof Slime) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_SLIME, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
-                if (entity instanceof ExperienceOrb) {
-                    FlagCheckEvent flagCheckEvent = HandlerUtil.checkEvent(entity.blockPosition(), RegionFlag.SPAWNING_XP, dimRegion);
-                    event.setCanceled(flagCheckEvent.isDenied());
+            }
+            if (entity instanceof ExperienceOrb) {
+                checkEvent = new FlagCheckEvent(target, RegionFlag.SPAWNING_XP, dim, null);
+                if (MinecraftForge.EVENT_BUS.post(checkEvent)) {
+                    return;
                 }
+            }
+            if (checkEvent != null) {
+                processCheck(checkEvent, null, onDenyHandler);
             }
         }
     }
