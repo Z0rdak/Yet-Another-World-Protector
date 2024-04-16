6170814b873ec5fc6d8d798b2605dbb93c819574
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-04-16 12:26:41 +0200

    change comment to be more concise

--- src/main/java/de/z0rdak/yawp/core/flag/IFlag.java
+++ src/main/java/de/z0rdak/yawp/core/flag/IFlag.java
@@ -63,7 +63,8 @@ public interface IFlag extends INBTSerializable<CompoundTag>, Comparable<IFlag> 
     void setOverride(boolean doesOverride);
 
     /**
-     * Returns whether the flag is active in the region. <br>
+     * Returns whether the flag is active in the region. <br></br>
+     * This means the flag state is either ALLOWED or DENIED. <br></br>
      * Disabled flags are not considered for flag checks.
      *
      * @return true if flag is active, false otherwise.
