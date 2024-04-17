42c1ae5d5a276e64c05e0028a022d344716bb44a
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-04-17 11:22:55 +0200

    fix caluclation in sphereArea by using manhattan distance calculation; implement interface for sphereArea

--- src/main/java/de/z0rdak/yawp/core/area/SphereArea.java
+++ src/main/java/de/z0rdak/yawp/core/area/SphereArea.java
@@ -5,12 +5,12 @@ import de.z0rdak.yawp.util.constants.AreaNBT;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3d;
+import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static de.z0rdak.yawp.util.AreaUtil.distance;
+import static de.z0rdak.yawp.util.AreaUtil.distanceManhattan;
 
 public class SphereArea extends CenteredArea {
 
@@ -21,39 +21,87 @@ public class SphereArea extends CenteredArea {
         this.deserializeNBT(nbt);
     }
 
-    public SphereArea(BlockPos centerPos, BlockPos scopePos){
+    public SphereArea(BlockPos centerPos, BlockPos scopePos) {
         super(centerPos, AreaType.SPHERE);
-        this.radius = (int) (distance(centerPos, scopePos) + 0.5);
+        this.radius = (distanceManhattan(centerPos, scopePos));
     }
 
-    @Override
-    public Vector3d getCenter() {
-        return new Vector3d(this.center.getX(), this.center.getY(), this.center.getZ());
+    public SphereArea(BlockPos middlePos, int radius) {
+        this(middlePos, new BlockPos(middlePos).offset(0, radius, 0));
     }
 
     public int getRadius() {
         return this.radius;
     }
 
-    public SphereArea(BlockPos middlePos, int radius) {
-        this(middlePos, new BlockPos(middlePos)
-                .offset(0, radius, 0));
-    }
-
     @Override
     public boolean contains(BlockPos pos) {
-        return distance(this.center, pos) < this.radius + 0.5;
+        return distanceManhattan(this.center, pos) <= this.radius;
     }
 
     @Override
     public Set<BlockPos> getHull() {
-        AxisAlignedBB cube = new AxisAlignedBB(new BlockPos(this.center).offset(-this.radius, -this.radius, -this.radius),
-                new BlockPos(this.center).offset(this.radius, this.radius, this.radius));
+        AxisAlignedBB cube = new AxisAlignedBB(new BlockPos(this.center).offset(-this.radius, -this.radius, -this.radius), new BlockPos(this.center).offset(this.radius, this.radius, this.radius));
         Set<BlockPos> cubeBlocks = AreaUtil.blocksBetween(cube);
-        return cubeBlocks.stream()
-                .filter(blockPos -> distance(this.center, blockPos) > this.radius + 0.5)
-                .filter(blockPos -> distance(this.center, blockPos) < this.radius - 0.5)
-                .collect(Collectors.toSet());
+        return cubeBlocks.stream().filter(pos -> distanceManhattan(this.center, pos) == this.radius).collect(Collectors.toSet());
+    }
+
+    public boolean contains(CuboidArea inner) {
+        double maxDistance = Double.NEGATIVE_INFINITY;
+        // Calculate the maximum distance from the sphere center to cuboid vertices
+        for (BlockPos vertex : inner.getVertices()) {
+            double distance = distanceManhattan(this.center, vertex);
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+        // Check if the maximum distance is less than or equal to the sphere's radius
+        return maxDistance <= this.getRadius();
+    }
+
+    public boolean contains(SphereArea inner) {
+        return distanceManhattan(this.center, inner.center) + inner.radius <= this.radius;
+    }
+
+    public boolean intersects(CuboidArea other) {
+        // Check if sphere's center is inside the cuboid
+        if (other.contains(this.center)) {
+            return true;
+        }
+        // Compute the closest point on the cuboid to the sphere
+        double closestX = Math.max(other.getArea().minX, Math.min(this.center.getX(), other.getArea().maxX));
+        double closestY = Math.max(other.getArea().minY, Math.min(this.center.getY(), other.getArea().maxY));
+        double closestZ = Math.max(other.getArea().minZ, Math.min(this.center.getZ(), other.getArea().maxZ));
+        // Calculate the distance between the sphere center and the closest point
+        return distanceManhattan(this.center, new BlockPos(closestX, closestY, closestZ)) <= this.radius;
+    }
+
+    public boolean intersects(SphereArea other) {
+        return distanceManhattan(this.center, other.center) <= this.radius + other.radius;
+    }
+
+    @Override
+    public boolean containsOther(IMarkableArea inner) {
+        switch (inner.getAreaType()) {
+            case CUBOID:
+                return this.contains((CuboidArea) inner);
+            case SPHERE:
+                return this.contains((SphereArea) inner);
+            default:
+                throw new NotImplementedException("Area type not implemented yet");
+        }
+    }
+
+    @Override
+    public boolean intersects(IMarkableArea other) {
+        switch (other.getAreaType()) {
+            case CUBOID:
+                return this.intersects((CuboidArea) other);
+            case SPHERE:
+                return this.intersects((SphereArea) other);
+            default:
+                throw new NotImplementedException("Area type not implemented yet");
+        }
     }
 
     @Override
