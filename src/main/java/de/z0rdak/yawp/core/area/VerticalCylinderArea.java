e0cdfc3764dcf0f0cace0e11dcd004cef3dbc743
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-04-17 11:23:40 +0200

    fix calculation by using manhattan distance

--- src/main/java/de/z0rdak/yawp/core/area/VerticalCylinderArea.java
+++ src/main/java/de/z0rdak/yawp/core/area/VerticalCylinderArea.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.Set;
 
-import static de.z0rdak.yawp.util.AreaUtil.distance;
+import static de.z0rdak.yawp.util.AreaUtil.distanceManhattan;
 import static de.z0rdak.yawp.util.AreaUtil.length;
 
 /**
@@ -25,8 +25,8 @@ public class VerticalCylinderArea extends CenteredArea {
     public VerticalCylinderArea(BlockPos centerBottomPos, BlockPos scopePos){
         super(centerBottomPos, AreaType.CYLINDER);
         this.centerTopPos = new BlockPos(centerBottomPos.getX(), scopePos.getY(), centerBottomPos.getZ());
-        this.radius = (int) (distance(centerBottomPos, new BlockPos(scopePos.getX(), centerBottomPos.getY(), scopePos.getZ())));
-        this.distance = (int) (distance(centerBottomPos, this.centerTopPos) + 0.5);
+        this.radius = distanceManhattan(centerBottomPos, new BlockPos(scopePos.getX(), centerBottomPos.getY(), scopePos.getZ()));
+        this.distance = distanceManhattan(centerBottomPos, this.centerTopPos);
     }
 
     public VerticalCylinderArea(BlockPos centerBottomPos, int radius, int distance){
@@ -57,8 +57,8 @@ public class VerticalCylinderArea extends CenteredArea {
         boolean b2 = multiply(pos.subtract(centerTopPos), dist).compareTo(BlockPos.ZERO) <= 0;
         boolean isBetweenPlanes = b1 && b2;
         BlockPos crossProduct = pos.subtract(center).cross(dist);
-        double distance = length(crossProduct) / distance(centerTopPos, center);
-        boolean isInsideSurface = distance <= (radius - 0.5);
+        double distance = length(crossProduct) / distanceManhattan(centerTopPos, center);
+        boolean isInsideSurface = distance <= radius;
         return isBetweenPlanes && isInsideSurface;
     }
 
@@ -92,6 +92,17 @@ public class VerticalCylinderArea extends CenteredArea {
         super.deserializeNBT(nbt);
         this.distance = nbt.getInt(AreaNBT.RADIUS);
         this.radius = nbt.getInt(AreaNBT.HEIGHT);
+    }
+
+
+    @Override
+    public boolean containsOther(IMarkableArea other) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+
+    @Override
+    public boolean intersects(IMarkableArea other) {
+        throw new NotImplementedException("Not yet implemented");
     }
 
     // Cylinder [x,y,z] with radius r and height h
