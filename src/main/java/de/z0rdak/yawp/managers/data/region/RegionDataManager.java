c95d7e35a51a4194ef79cb45efe574abbeb91346
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-04-19 11:26:08 +0200

    make sure region hierarchy is intact after resetting global region

--- src/main/java/de/z0rdak/yawp/managers/data/region/RegionDataManager.java
+++ src/main/java/de/z0rdak/yawp/managers/data/region/RegionDataManager.java
@@ -293,7 +293,12 @@ public class RegionDataManager extends SavedData {
     }
 
     public void resetGlobalRegion() {
+        List<IProtectedRegion> collect = new ArrayList<>(globalRegion.getChildren().values());
         globalRegion = new GlobalRegion();
+        collect.forEach(dr -> {
+            globalRegion.addChild(dr);
+            dr.setParent(globalRegion);
+        });
         save();
     }
 
