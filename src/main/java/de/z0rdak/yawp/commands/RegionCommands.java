48e614ae2d65aac408b5de0c4a4ecd4033f13317
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-04-26 22:43:49 +0200

    fix rename command not being on the correct hierarchy

--- src/main/java/de/z0rdak/yawp/commands/RegionCommands.java
+++ src/main/java/de/z0rdak/yawp/commands/RegionCommands.java
@@ -131,14 +131,14 @@ public class RegionCommands {
                                         .then(literal(TELEPORT)
                                                 .executes(ctx -> teleport(ctx, getRegionArgument(ctx)))
                                                 .then(Commands.argument(PLAYER.toString(), EntityArgument.player())
-                                                        .executes(ctx -> teleport(ctx, getRegionArgument(ctx), getPlayerArgument(ctx)))))
-                                        .then(literal(RENAME)
-                                                .then(Commands.argument(NAME.toString(), StringArgumentType.word())
-                                                        .suggests((ctx, builder) -> ISuggestionProvider.suggest(Collections.singletonList(getRegionArgument(ctx).getName()), builder))
-                                                        .executes(ctx -> renameRegion(ctx, getRegionArgument(ctx), getRegionNameArgument(ctx), getDimCacheArgument(ctx)))))
+                                                        .executes(ctx -> teleport(ctx, getRegionArgument(ctx), getPlayerArgument(ctx))))))
+                                .then(literal(RENAME)
+                                        .then(Commands.argument(NAME.toString(), StringArgumentType.word())
+                                                .suggests((ctx, builder) -> ISuggestionProvider.suggest(Collections.singletonList(getRegionArgument(ctx).getName()), builder))
+                                                .executes(ctx -> renameRegion(ctx, getRegionArgument(ctx), getRegionNameArgument(ctx), getDimCacheArgument(ctx)))))
                                 // Idea: reset player, team, etc. with complete hierarchy
                                 // Scenario: Keep region and children with flags but reset it for new player base
-                        )
+
                 );
     }
 
