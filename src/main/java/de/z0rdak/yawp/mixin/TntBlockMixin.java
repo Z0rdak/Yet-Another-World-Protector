d8e996ddc78d204b1158d53f03f70b429ed5e75f
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2024-05-13 22:02:44 +0200

    fix ignite-explosives flag mixin

--- src/main/java/de/z0rdak/yawp/mixin/TntBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/TntBlockMixin.java
@@ -1,49 +1,36 @@
 package de.z0rdak.yawp.mixin;
 
-import static de.z0rdak.yawp.core.flag.RegionFlag.IGNITE_EXPLOSIVES;
-import static de.z0rdak.yawp.handler.flags.HandlerUtil.checkPlayerEvent;
-import static de.z0rdak.yawp.handler.flags.HandlerUtil.sendFlagDeniedMsg;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TntBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static de.z0rdak.yawp.core.flag.RegionFlag.IGNITE_EXPLOSIVES;
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 
 @Mixin(TntBlock.class)
 public class TntBlockMixin {
 
-    @Inject(method = "onUse", at = @At("HEAD"), cancellable = true, allow = 1)
-    public void onUseFlintAndSteel(BlockState state, World world, BlockPos pos, PlayerEntity player2, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        ItemStack itemStack = player2.getStackInHand(hand);
-        if (sameItemStackIgnoreDurability(itemStack, Items.FLINT_AND_STEEL.getDefaultStack()) || itemStack.isOf(Items.FIRE_CHARGE)) {
+    @Inject(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/TntBlock;primeTnt(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/LivingEntity;)V"), cancellable = true, allow = 1)
+    public void onPlayerIgniteExplosive(BlockState state, World world, BlockPos pos, PlayerEntity player2, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if (isServerSide(world)) {
             DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(world.getRegistryKey());
             FlagCheckEvent flagCheck = checkPlayerEvent(player2, pos, IGNITE_EXPLOSIVES, dimCache.getDimensionalRegion());
             if (flagCheck.isDenied()) {
                 sendFlagDeniedMsg(flagCheck, player2);
                 cir.setReturnValue(ActionResult.CONSUME);
             }
-        }
-    }
-
-    private static boolean sameItemStackIgnoreDurability(ItemStack left, ItemStack right){
-        if (!left.isDamageable()) {
-            return ItemStack.canCombine(left, right);
-        } else {
-            return false;
         }
     }
 }
