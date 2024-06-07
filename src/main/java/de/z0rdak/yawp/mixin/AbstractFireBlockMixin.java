61024fa00313a8b30ee17da69e2f2f550fe4c872
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-06-07 19:17:51 +0200

    update flagcheck handling for mixins 1

--- src/main/java/de/z0rdak/yawp/mixin/AbstractFireBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/AbstractFireBlockMixin.java
@@ -1,38 +1,30 @@
 package de.z0rdak.yawp.mixin;
 
-import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
-import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
-import de.z0rdak.yawp.managers.data.region.RegionDataManager;
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.api.events.region.RegionEvents;
+import de.z0rdak.yawp.handler.flags.HandlerUtil;
 import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.NetherPortal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Optional;
-
 import static de.z0rdak.yawp.core.flag.RegionFlag.SPAWN_PORTAL;
-import static de.z0rdak.yawp.handler.flags.HandlerUtil.checkTargetEvent;
 
 @Mixin(AbstractFireBlock.class)
 public abstract class AbstractFireBlockMixin {
 
-    @Inject(method = "onBlockAdded", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/AreaHelper;getNewPortal(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction$Axis;)Ljava/util/Optional;"), cancellable = true)
+    @Inject(method = "onBlockAdded", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/NetherPortal;createPortal()V"), cancellable = true)
     private void onSpawnPortal(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify, CallbackInfo info) {
         if (!world.isClient) {
-            Optional<NetherPortal> optional = NetherPortal.getNewPortal(world, pos, Direction.Axis.X);
-            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(world.getRegistryKey());
-            FlagCheckEvent flagCheckEvent = checkTargetEvent(pos, SPAWN_PORTAL, dimCache.getDimensionalRegion());
-            if (flagCheckEvent.isDenied()) {
-                optional = Optional.empty();
+            FlagCheckEvent checkEvent = new FlagCheckEvent(pos, SPAWN_PORTAL, world.getRegistryKey(), null);
+            if (RegionEvents.CHECK_FLAG.invoker().checkFlag(checkEvent)) {
+                return;
             }
-            optional.ifPresent(NetherPortal::createPortal);
-            info.cancel();
+            HandlerUtil.processCheck(checkEvent, null, deny -> info.cancel());
         }
     }
 }
--- src/main/java/de/z0rdak/yawp/mixin/TntBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/TntBlockMixin.java
@@ -1,14 +1,18 @@
 package de.z0rdak.yawp.mixin;
 
+import static de.z0rdak.yawp.core.flag.RegionFlag.BREAK_BLOCKS;
 import static de.z0rdak.yawp.core.flag.RegionFlag.IGNITE_EXPLOSIVES;
 import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.api.events.region.RegionEvents;
+import de.z0rdak.yawp.core.flag.FlagState;
+import de.z0rdak.yawp.util.MessageSender;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
 import net.minecraft.block.BlockState;
@@ -26,12 +30,14 @@ public class TntBlockMixin {
     @Inject(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/TntBlock;primeTnt(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/LivingEntity;)V"), cancellable = true, allow = 1)
     public void onPlayerIgniteExplosive(BlockState state, World world, BlockPos pos, PlayerEntity player2, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         if (isServerSide(world)) {
-            DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(world.getRegistryKey());
-            FlagCheckEvent flagCheck = checkPlayerEvent(player2, pos, IGNITE_EXPLOSIVES, dimCache.getDimensionalRegion());
-            if (flagCheck.isDenied()) {
-                sendFlagDeniedMsg(flagCheck, player2);
-                cir.setReturnValue(ActionResult.CONSUME);
+            FlagCheckEvent checkEvent = new FlagCheckEvent(pos, IGNITE_EXPLOSIVES, world.getRegistryKey(), player2);
+            if (RegionEvents.CHECK_FLAG.invoker().checkFlag(checkEvent)) {
+                return;
             }
+            processCheck(checkEvent, null, result -> {
+                MessageSender.sendFlagMsg(result);
+                cir.setReturnValue(ActionResult.CONSUME);
+            });
         }
     }
 }
