b13dc8a8ce55c68f434b617c4b5c1c6607fb1dda
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-06-08 22:07:54 +0200

    update flagcheck handling for mixins 3

--- src/main/java/de/z0rdak/yawp/mixin/AxeItemMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/AxeItemMixin.java
@@ -1,9 +1,12 @@
 package de.z0rdak.yawp.mixin;
 
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.api.events.region.RegionEvents;
 import de.z0rdak.yawp.core.flag.RegionFlag;
-import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
+import de.z0rdak.yawp.handler.flags.HandlerUtil;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
+import de.z0rdak.yawp.util.MessageSender;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemUsageContext;
@@ -15,6 +18,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static de.z0rdak.yawp.core.flag.RegionFlag.*;
 import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 
 @Mixin(AxeItem.class)
@@ -23,26 +27,26 @@ public abstract class AxeItemMixin {
     @Inject(method = "useOnBlock", at = @At(value = "HEAD"), cancellable = true, allow = 1)
     public void onUseAxeOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         World world = context.getWorld();
-        BlockPos blockPos = context.getBlockPos();
+        BlockPos pos = context.getBlockPos();
         PlayerEntity player = context.getPlayer();
-        if (!world.isClient) {
-            if (player != null) {
-                DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(player));
-                FlagCheckEvent.PlayerFlagEvent flagCheck = checkPlayerEvent(player, blockPos, RegionFlag.TOOL_SECONDARY_USE, dimCache.getDimensionalRegion());
-                if (flagCheck.isDenied()) {
-                    sendFlagDeniedMsg(flagCheck);
-                    cir.setReturnValue(ActionResult.PASS);
-                }
-
-                // FIXME: AXE_STRIP does prevent scrape and wax_off as well - either rename it or create separate flags
-                flagCheck = checkPlayerEvent(player, blockPos, RegionFlag.AXE_STRIP, dimCache.getDimensionalRegion());
-                if (flagCheck.isDenied()) {
-                    sendFlagDeniedMsg(flagCheck);
-                    cir.setReturnValue(ActionResult.PASS);
-                }
-
+        if (!world.isClient && player != null) { 
+            FlagCheckEvent checkEvent = new FlagCheckEvent(pos, TOOL_SECONDARY_USE, world.getRegistryKey(), player);
+            if (RegionEvents.CHECK_FLAG.invoker().checkFlag(checkEvent)) {
+                return;
             }
-
+            HandlerUtil.processCheck(checkEvent, null, deny -> {
+                MessageSender.sendFlagMsg(deny);
+                cir.setReturnValue(ActionResult.PASS);
+            });
+            
+            checkEvent = new FlagCheckEvent(pos, AXE_STRIP, world.getRegistryKey(), player);
+            if (RegionEvents.CHECK_FLAG.invoker().checkFlag(checkEvent)) {
+                return;
+            }
+            HandlerUtil.processCheck(checkEvent, null, deny -> {
+                MessageSender.sendFlagMsg(deny);
+                cir.setReturnValue(ActionResult.PASS);
+            });
         }
     }
 }
--- src/main/java/de/z0rdak/yawp/mixin/CommandManagerMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/CommandManagerMixin.java
@@ -1,19 +1,24 @@
 package de.z0rdak.yawp.mixin;
 
 import com.mojang.brigadier.ParseResults;
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.api.events.region.RegionEvents;
 import de.z0rdak.yawp.core.flag.RegionFlag;
 import de.z0rdak.yawp.handler.CommandInterceptor;
-import de.z0rdak.yawp.handler.flags.FlagCheckEvent;
+import de.z0rdak.yawp.handler.flags.HandlerUtil;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
+import de.z0rdak.yawp.util.MessageSender;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static de.z0rdak.yawp.core.flag.RegionFlag.USE_BONEMEAL;
 import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 
 @Mixin(CommandManager.class)
@@ -32,12 +37,14 @@ public abstract class CommandManagerMixin {
         if (cmdSource.isExecutedByPlayer()) {
             ServerPlayerEntity player = cmdSource.getPlayer();
             if (player != null) {
-                DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(getEntityDim(player));
-                FlagCheckEvent.PlayerFlagEvent flagCheck = checkPlayerEvent(player, player.getBlockPos(), RegionFlag.EXECUTE_COMMAND, dimCache.getDimensionalRegion());
-                if (flagCheck.isDenied()) {
-                    sendFlagDeniedMsg(flagCheck);
-                    cir.setReturnValue(1);
+                FlagCheckEvent checkEvent = new FlagCheckEvent(player.getBlockPos(), USE_BONEMEAL, getEntityDim(player), player);
+                if (RegionEvents.CHECK_FLAG.invoker().checkFlag(checkEvent)) {
+                    return;
                 }
+                HandlerUtil.processCheck(checkEvent, null, deny -> {
+                    MessageSender.sendFlagMsg(deny);
+                    cir.setReturnValue(1);
+                });
             }
         }
     }
