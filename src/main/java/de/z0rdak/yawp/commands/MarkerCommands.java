32970677a3f7b7e320897cfaf509e414c52c1420
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-08-17 21:52:16 +0200

    fix region marker

--- src/main/java/de/z0rdak/yawp/commands/MarkerCommands.java
+++ src/main/java/de/z0rdak/yawp/commands/MarkerCommands.java
@@ -11,6 +11,7 @@ import de.z0rdak.yawp.config.server.CommandPermissionConfig;
 import de.z0rdak.yawp.config.server.RegionConfig;
 import de.z0rdak.yawp.core.region.IMarkableRegion;
 import de.z0rdak.yawp.core.region.IProtectedRegion;
+import de.z0rdak.yawp.core.region.RegionType;
 import de.z0rdak.yawp.core.stick.MarkerStick;
 import de.z0rdak.yawp.managers.data.region.DimensionRegionCache;
 import de.z0rdak.yawp.managers.data.region.RegionDataManager;
@@ -57,12 +58,10 @@ public final class MarkerCommands {
                                 .executes(ctx -> createMarkedRegion(ctx, getRegionNameArgument(ctx), null))
                                 .then(Commands.argument(PARENT.toString(), StringArgumentType.word())
                                         .suggests((ctx, builder) -> ContainingOwnedRegionArgumentType.owningRegions().listSuggestionsWithMarker(ctx, builder))
-                                        .executes(ctx -> createMarkedRegion(ctx, getRegionNameArgument(ctx), getContainingOwnedRegionArgument(ctx))))))
-                ;
+                                        .executes(ctx -> createMarkedRegion(ctx, getRegionNameArgument(ctx), getContainingOwnedRegionArgumentWithMarker(ctx))))));
     }
 
-    public static IMarkableRegion fromMarkedBlocks(CommandContext<CommandSourceStack> ctx, String regionName) throws CommandSyntaxException {
-        Player player = ctx.getSource().getPlayerOrException();
+    public static IMarkableRegion fromMarkedBlocks(CommandContext<CommandSourceStack> ctx, Player player, String regionName) throws CommandSyntaxException {
         ItemStack maybeStick = player.getMainHandItem();
         if (StickUtil.isVanillaStick(maybeStick)) {
             StickType stickType = StickUtil.getStickType(maybeStick);
@@ -89,22 +88,7 @@ public final class MarkerCommands {
         }
     }
 
-    private static int createMarkedRegion(CommandContext<CommandSourceStack> ctx, String regionName, IMarkableRegion parentRegion) {
-        try {
-            Player player = ctx.getSource().getPlayerOrException();
-            if (parentRegion == null) {
-                DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(player.level.dimension());
-                return createRegion(ctx, regionName, dimCache.getDimensionalRegion());
-            } else {
-                return createRegion(ctx, regionName, parentRegion);
-            }
-        } catch (CommandSyntaxException e) {
-            YetAnotherWorldProtector.LOGGER.error(e);
-            return -3;
-        }
-    }
-
-    private static int createRegion(CommandContext<CommandSourceStack> ctx, String regionName, IProtectedRegion parentRegion) {
+    private static int createMarkedRegion(CommandContext<CommandSourceStack> ctx, String regionName, IProtectedRegion parentRegion) {
         try {
             DimensionRegionCache dimCache = RegionDataManager.get().cacheFor(ctx.getSource().getLevel().dimension());
             int res = RegionDataManager.get().isValidRegionName(dimCache.getDimensionalRegion().getDim(), regionName);
@@ -117,39 +101,38 @@ public final class MarkerCommands {
                 return res;
             }
             Player player = ctx.getSource().getPlayerOrException();
-            IMarkableRegion region = fromMarkedBlocks(ctx, regionName);
-            if (region == null) {
+            IMarkableRegion newRegion = fromMarkedBlocks(ctx, player, regionName);
+            if (newRegion == null) {
                 sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.msg.dim.info.region.create.stick.area.invalid").withStyle(RED));
                 return -1;
             }
-            LocalRegions.RegionOverlappingInfo overlapping = LocalRegions.getOverlappingWithPermission(region, player);
-            if (!overlapping.hasContaining()) {
-                sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.arg.region.owned.invalid", parentRegion.getName()));
-                return 1;
-            }
-            RegionDataManager.addFlags(RegionConfig.getDefaultFlags(), region);
-            if (MinecraftForge.EVENT_BUS.post(new RegionEvent.CreateRegionEvent(region, player))) {
-                return 1;
-            }
-            boolean hasConfigPermission = CommandPermissionConfig.hasConfigPermission(player);
-            boolean hasRegionPermission = CommandPermissionConfig.hasRegionPermission(parentRegion, player, CommandUtil.OWNER);
-            if (hasConfigPermission || hasRegionPermission) {
-                dimCache.addRegion(dimCache.getDimensionalRegion(), region);
-                parentRegion.addChild(region);
-                LocalRegions.ensureHigherRegionPriorityFor(region, RegionConfig.getDefaultPriority());
-                RegionDataManager.save();
-                sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.msg.dim.info.region.create.success", buildRegionInfoLink(region), buildRegionInfoLink(parentRegion)));
-                return 0;
-            } else {
-                sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.msg.dim.info.region.create.local.deny", buildRegionInfoLink(parentRegion)));
-                return 1;
-            }
+            IProtectedRegion parent = parentRegion == null ? dimCache.getDimensionalRegion() : parentRegion;
+            return createRegion(ctx, player, dimCache, newRegion, parent);            
         } catch (CommandSyntaxException e) {
             YetAnotherWorldProtector.LOGGER.error(e);
+            sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.msg.dim.info.region.create.stick.no-player", "This command can only be executed as a player!").withStyle(RED));
             return -1;
         }
     }
 
+    private static int createRegion(CommandContext<CommandSourceStack> ctx, Player player, DimensionRegionCache dimCache, IMarkableRegion region, IProtectedRegion parentRegion) {
+        if (MinecraftForge.EVENT_BUS.post(new RegionEvent.CreateRegionEvent(region, player))) {
+            return 1;
+        }
+        boolean hasConfigPermission = CommandPermissionConfig.hasConfigPermission(player);
+        boolean hasRegionPermission = CommandPermissionConfig.hasRegionPermission(parentRegion, player, CommandUtil.OWNER);
+        if (hasConfigPermission || hasRegionPermission) {
+            RegionDataManager.addFlags(RegionConfig.getDefaultFlags(), region);
+            dimCache.addRegion(parentRegion, region);
+            LocalRegions.ensureHigherRegionPriorityFor(region, RegionConfig.getDefaultPriority());
+            RegionDataManager.save();
+            sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.msg.dim.info.region.create.success", buildRegionInfoLink(region), buildRegionInfoLink(parentRegion)));
+            return 0;
+        } else {
+            sendCmdFeedback(ctx.getSource(), new TranslatableComponent("cli.msg.dim.info.region.create.local.deny", buildRegionInfoLink(parentRegion)));
+            return 1;
+        }
+    }
 
     private static int resetStick(CommandContext<CommandSourceStack> ctx) {
         try {
