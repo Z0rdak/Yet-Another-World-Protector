dc372961638f3b432762e8b045fd5a2ee343eb7f
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-09-06 21:58:45 +0200

    disable mixin for powdersnow (also not covered by forge, this will maybe re-enabled later)

--- src/main/java/de/z0rdak/yawp/mixin/flag/mobgrief/PowderSnowBlockMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/flag/mobgrief/PowderSnowBlockMixin.java
@@ -4,9 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import de.z0rdak.yawp.util.MobGriefingHelper;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PowderSnowBlock;
 import net.minecraft.entity.Entity;
@@ -16,12 +14,10 @@ import net.minecraft.world.World;
 @Mixin(PowderSnowBlock.class)
 public class PowderSnowBlockMixin {
 
+    // FIXME: This is not included in the mob-grief event in forge
+    // FIXME: So for now it is disabled
     @Inject(method = "onEntityCollision(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;breakBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"), cancellable = true, allow = 1)
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
-        if (MobGriefingHelper.preventGrief(world, pos)) {
-            ci.cancel();
-        }
+        // HandlerUtil.checkMobGrief(world, pos, ci);
     }
-
-
 }
