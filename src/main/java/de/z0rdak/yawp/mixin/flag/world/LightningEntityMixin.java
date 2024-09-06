530d90d84b4f746847802aca058d4005528a2a10
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-09-06 22:02:20 +0200

    lightning flag now prevents fire spawning and oxidation removal

--- src/main/java/de/z0rdak/yawp/mixin/flag/world/LightningEntityMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/flag/world/LightningEntityMixin.java
@@ -1,45 +1,56 @@
 package de.z0rdak.yawp.mixin.flag.world;
 
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
 import net.minecraft.entity.LightningEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import static de.z0rdak.yawp.api.events.region.RegionEvents.post;
+import static de.z0rdak.yawp.core.flag.RegionFlag.FALL_DAMAGE;
+import static de.z0rdak.yawp.core.flag.RegionFlag.LIGHTNING_PROT;
+import static de.z0rdak.yawp.handler.flags.HandlerUtil.*;
 
 @Mixin(LightningEntity.class)
 public abstract class LightningEntityMixin {
 
     /**
      * lightning-oxidation-removal
-     * Prevent oxidation removal from blocks when lightning hits
-     *
-     * @param ci
+     * Prevent oxidation removal from the block when lightning hits
+     * It also prevents oxidation of nearby blocks, even when they are not protected
      */
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LightningEntity;cleanOxidation(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"), cancellable = true, allow = 1)
-    public void cleanOxidationOnHitBlock(CallbackInfo ci) {
-        LightningEntity pearl = (LightningEntity) (Object) this;
+    @Inject(method = "cleanOxidation", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"), cancellable = true, allow = 1)
+    private static void cleanOxidationOnHitBlock(World world, BlockPos pos, CallbackInfo ci) {
+        if (isServerSide(world)) {
+            FlagCheckEvent checkEvent = new FlagCheckEvent(pos, LIGHTNING_PROT, getDimKey(world), null);
+            if (post(checkEvent)) {
+                return;
+            }
+            processCheck(checkEvent, null, deny -> {
+                ci.cancel();
+            });
+        }
     }
 
     /**
      * lighting-fire-spread
      * Prevent fire spreading when lightning hits blocks
-     *
-     * @param spreadAttempts
-     * @param ci
      */
-    @Inject(method = "spawnFire", at = @At(value = "HEAD"), cancellable = true, allow = 1)
-    public void onSpawnFireFromLightning(int spreadAttempts, CallbackInfo ci) {
-        LightningEntity pearl = (LightningEntity) (Object) this;
-    }
-
-    /**
-     * lightning-power-rods
-     * Still hit lightning rod but prevent powering it
-     *
-     * @param ci
-     */
-    @Inject(method = "powerLightningRod", at = @At(value = "HEAD"), cancellable = true, allow = 1)
-    public void onPowerLightningRodFromLightning(CallbackInfo ci) {
-        LightningEntity pearl = (LightningEntity) (Object) this;
+    @Inject(method = "spawnFire", locals = LocalCapture.CAPTURE_FAILSOFT, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractFireBlock;getState(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 0), cancellable = true, allow = 1)
+    public void onSpawnFireFromLightning(int spreadAttempts, CallbackInfo ci, BlockPos blockPos) {
+        LightningEntity lightningEntity = (LightningEntity) (Object) this;
+        if (isServerSide(lightningEntity)) {
+            FlagCheckEvent checkEvent = new FlagCheckEvent(lightningEntity.getBlockPos(), LIGHTNING_PROT, getDimKey(lightningEntity), null);
+            if (post(checkEvent)) {
+                return;
+            }
+            processCheck(checkEvent, null, deny -> {
+                ci.cancel();
+            });
+        }
     }
 }
