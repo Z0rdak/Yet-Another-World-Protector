0662cd715a3cded20f570c7a7e164ba92d67b06c
Marcel Bartholet <marcel.bartholet@mni.thm.de>
2024-09-06 21:57:58 +0200

    more mobgrief flag rework

--- src/main/java/de/z0rdak/yawp/mixin/flag/mobgrief/PiglinEntityMixin.java
+++ src/main/java/de/z0rdak/yawp/mixin/flag/mobgrief/PiglinEntityMixin.java
@@ -1,22 +1,25 @@
 package de.z0rdak.yawp.mixin.flag.mobgrief;
 
+import de.z0rdak.yawp.handler.flags.HandlerUtil;
+import net.minecraft.entity.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import de.z0rdak.yawp.util.MobGriefingHelper;
 import net.minecraft.entity.mob.PiglinEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 
 @Mixin(PiglinEntity.class)
 public abstract class PiglinEntityMixin {
+    @Unique
     Entity self = (PiglinEntity) (Object) this;
     @Inject(method = "canGather", at = @At(value = "HEAD"), cancellable = true)
-    public void onCanGather(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (MobGriefingHelper.preventGrief(self)) {
-            cir.setReturnValue(false);
-        }
+    public void onCanGather(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {        
+        HandlerUtil.checkMobGrief(self.getWorld(), self.getBlockPos(), cir);
     }
+
 }
