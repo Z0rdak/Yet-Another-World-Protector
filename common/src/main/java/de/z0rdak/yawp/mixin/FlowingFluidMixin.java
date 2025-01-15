ccca4ef940cd014932011b3180cb977d8b4a6184
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-01-15 10:31:02 +0100

    fix fluid flag npe

--- common/src/main/java/de/z0rdak/yawp/mixin/FlowingFluidMixin.java
+++ common/src/main/java/de/z0rdak/yawp/mixin/FlowingFluidMixin.java
@@ -46,7 +46,7 @@ public class FlowingFluidMixin {
             specificFluidCheckEvent = new FlagCheckEvent(blockPos, LAVA_FLOW, level.dimension());
         }
         
-        if (specificFluidCheckEvent != null && Services.EVENT.post(specificFluidCheckEvent)) {
+        if (specificFluidCheckEvent == null || Services.EVENT.post(specificFluidCheckEvent)) {
             return;
         }
         HandlerUtil.processCheck(specificFluidCheckEvent, deny -> {
