5147dd2ca71a3ad1419efc158802c3b3ce3dc961
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-01-16 11:50:57 +0100

    add neoforge mixins for flags

---
+++ neoforge/src/main/java/de/z0rdak/yawp/mixin/PlayerEntityMixin.java
@@ -0,0 +1,29 @@
+package de.z0rdak.yawp.mixin;
+
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.platform.Services;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static de.z0rdak.yawp.core.flag.RegionFlag.USE_ELYTRA;
+import static de.z0rdak.yawp.handler.HandlerUtil.isServerSide;
+import static de.z0rdak.yawp.handler.HandlerUtil.processCheck;
+
+@Mixin({Player.class})
+public abstract class PlayerEntityMixin {
+    // TODO: Could be moved to common, fabric has its own dedicated event for this. Maintainability > platform dependent code?
+    @Inject(method = "tryToStartFallFlying()Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;startFallFlying()V"), allow = 1, cancellable = true)
+    void injectElytraCheck(CallbackInfoReturnable<Boolean> cir) {
+        Player player = (Player) (Object) this;
+        if (isServerSide(player.level())) {
+            FlagCheckEvent checkEvent = new FlagCheckEvent(player.blockPosition(), USE_ELYTRA, player.level().dimension());
+            if (Services.EVENT.post(checkEvent)) {
+                return;
+            }
+            processCheck(checkEvent, denyResult -> cir.setReturnValue(false));
+        }
+    }
+}
