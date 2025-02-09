5170f2470d1c28d55bdbf2a7c0366e3c0d2f75da
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-09 22:32:32 +0100

    add keep-inv flag handler

--- common/src/main/java/de/z0rdak/yawp/mixin/flag/player/PlayerEntityMixin.java
+++ common/src/main/java/de/z0rdak/yawp/mixin/flag/player/PlayerEntityMixin.java
@@ -1,14 +1,16 @@
 package de.z0rdak.yawp.mixin.flag.player;
 
 import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.core.flag.RegionFlag;
 import de.z0rdak.yawp.platform.Services;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static de.z0rdak.yawp.core.flag.RegionFlag.USE_ELYTRA;
 import static de.z0rdak.yawp.handler.HandlerUtil.isServerSide;
 import static de.z0rdak.yawp.handler.HandlerUtil.processCheck;
 
@@ -19,11 +21,25 @@ public abstract class PlayerEntityMixin {
     void injectElytraCheck(CallbackInfoReturnable<Boolean> cir) {
         Player player = (Player) (Object) this;
         if (isServerSide(player.level())) {
-            FlagCheckEvent checkEvent = new FlagCheckEvent(player.blockPosition(), USE_ELYTRA, player.level().dimension());
+            FlagCheckEvent checkEvent = new FlagCheckEvent(player.blockPosition(), RegionFlag.USE_ELYTRA, player.level().dimension());
             if (Services.EVENT.post(checkEvent)) {
                 return;
             }
             processCheck(checkEvent, denyResult -> cir.setReturnValue(false));
         }
     }
+
+    @Inject(method = "dropEquipment", at = @At(value = "HEAD"), allow = 1, cancellable = true)
+    void onDropEquipment(ServerLevel level, CallbackInfo ci) {
+        Player self = (Player) (Object) this;
+        if (isServerSide(self.level())) {
+            FlagCheckEvent checkEvent = new FlagCheckEvent(self.blockPosition(), RegionFlag.KEEP_INV, self.level().dimension());
+            if (Services.EVENT.post(checkEvent)) {
+                return;
+            }
+            processCheck(checkEvent, denyResult -> ci.cancel());
+        }
+    }
+
+
 }
