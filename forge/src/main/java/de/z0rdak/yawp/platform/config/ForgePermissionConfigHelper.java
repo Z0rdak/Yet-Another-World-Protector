97a914b4c6e8216c9af6c3a3c2aa39fe9e33561b
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-15 19:42:36 +0100

    update config helper interface and impl

--- forge/src/main/java/de/z0rdak/yawp/platform/config/ForgePermissionConfigHelper.java
+++ forge/src/main/java/de/z0rdak/yawp/platform/config/ForgePermissionConfigHelper.java
@@ -60,4 +60,9 @@ public class ForgePermissionConfigHelper implements IPermissionConfigHelper {
     public boolean allowRegionTp() {
         return PermissionConfig.allowRegionTp();
     }
+    
+    @Override
+    public boolean isMarkerCreationEnabled() {
+        return PermissionConfig.isMarkerCreationEnabled();
+    }
 }
