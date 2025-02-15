aa6aaf2152db95176d44bb6a816a569f64330936
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-15 20:05:46 +0100

    add config value check to anvil injection

--- common/src/main/java/de/z0rdak/yawp/mixin/stick/AnvilScreenHandlerMixin.java
+++ common/src/main/java/de/z0rdak/yawp/mixin/stick/AnvilScreenHandlerMixin.java
@@ -1,12 +1,15 @@
 package de.z0rdak.yawp.mixin.stick;
 
 import de.z0rdak.yawp.handler.MarkerStickHandler;
+import de.z0rdak.yawp.platform.Services;
 import de.z0rdak.yawp.util.StickType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,17 +20,23 @@ import static de.z0rdak.yawp.handler.HandlerUtil.isServerSide;
 @Mixin(AnvilMenu.class)
 public abstract class AnvilScreenHandlerMixin {
 
+    @Shadow @Nullable private String itemName;
+
     @Inject(method = "onTake", at = @At("HEAD"), cancellable = true, allow = 1)
     private void onTakeOutput(Player player, ItemStack outputItem, CallbackInfo ci) {
         if (isServerSide(player.level())) {
             try {
+                if (!Services.PERMISSION_CONFIG.isMarkerCreationEnabled()) {
+                    return;
+                }
                 AnvilMenu anvilMenu = (AnvilMenu) (Object) this;
                 ItemStack inputItem = anvilMenu.getSlot(0).getItem();
                 ItemStack ingredientInput = anvilMenu.getSlot(1).getItem();
                 ItemStack stick = Items.STICK.getDefaultInstance();
                 boolean isInputAndOutputStick = ItemStack.isSameItem(outputItem, stick)
                         && ItemStack.isSameItem(inputItem, stick);
-                if (isInputAndOutputStick && ingredientInput.isEmpty()) {
+                var outputIsNamedAsMarker = this.itemName != null && this.itemName.equals(StickType.MARKER.stickName);
+                if (isInputAndOutputStick && ingredientInput.isEmpty() && outputIsNamedAsMarker) {
                     MarkerStickHandler.onCreateStick(player, inputItem, outputItem);
                     player.getInventory().setChanged();
                     ci.cancel();
