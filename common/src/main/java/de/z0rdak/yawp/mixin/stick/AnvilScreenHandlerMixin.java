e2df409e7a9f60ee322ea1b703cb7d3f60d87f01
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-15 20:05:46 +0100

    add config value check to anvil injection

--- common/src/main/java/de/z0rdak/yawp/mixin/stick/AnvilScreenHandlerMixin.java
+++ common/src/main/java/de/z0rdak/yawp/mixin/stick/AnvilScreenHandlerMixin.java
@@ -1,11 +1,13 @@
 package de.z0rdak.yawp.mixin.stick;
 
 import de.z0rdak.yawp.handler.MarkerStickHandler;
+import de.z0rdak.yawp.platform.Services;
 import de.z0rdak.yawp.util.StickType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,6 +28,9 @@ public abstract class AnvilScreenHandlerMixin {
     private void onTakeOutput(Player player, ItemStack outputItem, CallbackInfo ci) {
         if (isServerSide(player.level())) {
             try {
+                if (!Services.PERMISSION_CONFIG.isMarkerCreationEnabled()) {
+                    return;
+                }
                 AnvilMenu anvilMenu = (AnvilMenu) (Object) this;
                 ItemStack inputItem = anvilMenu.getSlot(0).getItem();
                 ItemStack ingredientInput = anvilMenu.getSlot(1).getItem();
