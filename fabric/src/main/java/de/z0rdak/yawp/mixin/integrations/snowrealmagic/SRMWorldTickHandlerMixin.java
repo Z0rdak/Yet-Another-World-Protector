fa03055d599045d64f7e63457d38e65ff0b6ebdf
Magnus Jensen <magnushjensen.mail@gmail.com>
2025-02-19 00:01:54 +0100

    feat: add snow real magic compat (#152)

---
+++ fabric/src/main/java/de/z0rdak/yawp/mixin/integrations/snowrealmagic/SRMWorldTickHandlerMixin.java
@@ -0,0 +1,29 @@
+package de.z0rdak.yawp.mixin.integrations.snowrealmagic;
+
+
+import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
+import de.z0rdak.yawp.core.flag.RegionFlag;
+import de.z0rdak.yawp.handler.HandlerUtil;
+import de.z0rdak.yawp.platform.Services;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import snownee.snow.WorldTickHandler;
+
+@Mixin(WorldTickHandler.class)
+public class SRMWorldTickHandlerMixin {
+
+    @Inject(method = "doSnow", at = @At(value = "INVOKE", target = "Lsnownee/snow/Hooks;convert(Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;IIZ)Z", ordinal = 0), cancellable = true, remap = false)
+    private static void doSnow(ServerLevel level, BlockPos.MutableBlockPos pos, CallbackInfo ci) {
+        FlagCheckEvent checkEvent = new FlagCheckEvent(pos, RegionFlag.SNOW_FALL, level.dimension());
+        if (Services.EVENT.post(checkEvent)){
+            return;
+        }
+        HandlerUtil.processCheck(checkEvent, deny -> {
+            ci.cancel();
+        });
+    }
+}
