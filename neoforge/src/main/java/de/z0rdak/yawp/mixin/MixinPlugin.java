3df43e409b984ef744be3c6b59c20f9af40b6cbb
Magnus Jensen <magnushjensen.mail@gmail.com>
2025-02-19 00:32:18 +0100

    feat: add snow real magic compat

---
+++ neoforge/src/main/java/de/z0rdak/yawp/mixin/MixinPlugin.java
@@ -0,0 +1,49 @@
+package de.z0rdak.yawp.mixin;
+
+import net.neoforged.fml.loading.LoadingModList;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String s) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return "";
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String s, String mixinClassName) {
+        if (mixinClassName.startsWith("snownee.snow")) {
+            return LoadingModList.get().getModFileById("snowrealmagic") != null;
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> set, Set<String> set1) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return List.of();
+    }
+
+    @Override
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {
+
+    }
+}
