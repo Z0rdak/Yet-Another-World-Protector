bb5523dabc0f6f03534efb24a53edce6916b2be9
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-20 22:08:57 +0100

    fix merge error

--- common/src/main/java/de/z0rdak/yawp/core/flag/FlagCorrelation.java
+++ common/src/main/java/de/z0rdak/yawp/core/flag/FlagCorrelation.java
@@ -4,13 +4,19 @@ import de.z0rdak.yawp.core.region.IProtectedRegion;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Represents the resolved association between a region and a flag. <br>
- * This is used to determine the region responsible for a specific flag state. <br>
- * The responsible region may not always be the one directly involved in a flag check event;
- * it could be a parent region that overrides the flag state of a child region. <br>
- * <br>
- * This class is primarily used for passing information between recursive calls when evaluating flag checks
- * and for retaining flag resolution details for use in the CLI.
+ * Represents the resolved association between a region and a flag.
+ * <p>
+ * This class determines which region is responsible for a specific flag state.  
+ * The responsible region may not always be the one directly involved in a flag check event;  
+ * it could be an ancestor region that overrides the flag state of a child region.
+ * </p>
+ * <p>
+ * {@code FlagCorrelation} is primarily used for passing information in recursive flag evaluations  
+ * and retaining flag resolution details for use in the CLI.
+ * </p>
+ *
+ * @param region the region responsible for the resolved flag state, must not be {@code null}
+ * @param flag   the flag associated with the resolved state, may be {@code null} if undefined
  */
 public record FlagCorrelation(
         IProtectedRegion region,
--- fabric/src/main/java/de/z0rdak/yawp/mixin/flag/player/ServerPlayerMixin.java
+++ fabric/src/main/java/de/z0rdak/yawp/mixin/flag/player/ServerPlayerMixin.java
@@ -4,20 +4,16 @@ import de.z0rdak.yawp.api.FlagEvaluator;
 import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
 import de.z0rdak.yawp.data.region.RegionDataManager;
 import de.z0rdak.yawp.platform.Services;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.Relative;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.portal.TeleportTransition;
+import net.minecraft.world.level.portal.DimensionTransition;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Set;
 
 import static de.z0rdak.yawp.core.flag.RegionFlag.*;
 import static de.z0rdak.yawp.handler.HandlerUtil.*;
@@ -43,11 +39,11 @@ public abstract class ServerPlayerMixin {
         }
     }
 
-    @Inject(method = "teleport(Lnet/minecraft/world/level/portal/TeleportTransition;)Lnet/minecraft/server/level/ServerPlayer;", at = @At(value = "HEAD"), allow = 1, cancellable = true)
-    private void onChangeDimension(TeleportTransition teleportTransition, CallbackInfoReturnable<Entity> cir) {
+    @Inject(method = "changeDimension", at = @At(value = "HEAD"), allow = 1, cancellable = true)
+    private void onchangeDimension(DimensionTransition transition, CallbackInfoReturnable<Entity> cir) {
         Player player = (Player) (Object) this;
         if (isServerSide(player)) {
-            RegionDataManager.addDimKeyOnDimensionChange(player, player.level(), teleportTransition.newLevel());
+            RegionDataManager.addDimKeyOnDimensionChange(player, player.level(), transition.newLevel());
 
             FlagCheckEvent checkEvent = new FlagCheckEvent(player.blockPosition(), USE_PORTAL_PLAYERS, getDimKey(player), player);
             if (Services.EVENT.post(checkEvent))
@@ -57,27 +53,12 @@ public abstract class ServerPlayerMixin {
                 cir.setReturnValue(null);
             });
 
-            checkEvent = new FlagCheckEvent(player.blockPosition(), ENTER_DIM, getDimKey(teleportTransition.newLevel()), player);
+            checkEvent = new FlagCheckEvent(player.blockPosition(), ENTER_DIM, getDimKey(transition.newLevel()), player);
             if (Services.EVENT.post(checkEvent))
                 return;
             FlagEvaluator.processCheck(checkEvent, deny -> {
                 sendFlagMsg(deny);
                 cir.setReturnValue(null);
-            });
-        }
-    }
-
-    @Inject(method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDLjava/util/Set;FFZ)Z", at = @At(value = "HEAD"), allow = 1, cancellable = true)
-    private void onTeleportToDimension(ServerLevel level, double x, double y, double z, Set<Relative> relativeMovements, float yaw, float pitch, boolean setCamera, CallbackInfoReturnable<Boolean> cir) {
-        Player player = (Player) (Object) this;
-        if (isServerSide(player)) {
-            FlagCheckEvent checkEvent = new FlagCheckEvent(player.blockPosition(), USE_PORTAL_PLAYERS, player.level().dimension(), player);
-            if (Services.EVENT.post(checkEvent)) {
-                return;
-            }
-            FlagEvaluator.processCheck(checkEvent, deny -> {
-                sendFlagMsg(deny);
-                cir.setReturnValue(false);
             });
         }
     }
