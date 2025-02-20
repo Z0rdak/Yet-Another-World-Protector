60d74bd5b1870c85963f4e606c4b08a33749faad
Marcel Bartholet <18328939+Z0rdak@users.noreply.github.com>
2025-02-20 21:48:02 +0100

    attempt to fixing issue where owners/members of a region would not be able to bypass flags when parent region has the same flag set

--- common/src/main/java/de/z0rdak/yawp/api/FlagEvaluator.java
+++ common/src/main/java/de/z0rdak/yawp/api/FlagEvaluator.java
@@ -4,24 +4,18 @@ import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
 import de.z0rdak.yawp.api.events.region.FlagCheckResult;
 import de.z0rdak.yawp.api.permission.Permissions;
 import de.z0rdak.yawp.constants.Constants;
-import de.z0rdak.yawp.core.flag.FlagCorrelation;
-import de.z0rdak.yawp.core.flag.FlagState;
-import de.z0rdak.yawp.core.flag.IFlag;
-import de.z0rdak.yawp.core.flag.RegionFlag;
+import de.z0rdak.yawp.core.flag.*;
 import de.z0rdak.yawp.core.region.IMarkableRegion;
 import de.z0rdak.yawp.core.region.IProtectedRegion;
 import de.z0rdak.yawp.core.region.RegionType;
 import de.z0rdak.yawp.data.region.RegionDataManager;
-import de.z0rdak.yawp.core.flag.FlagContext;
 import de.z0rdak.yawp.handler.HandlerUtil;
-import de.z0rdak.yawp.core.flag.RegionFlagResolution;
 import de.z0rdak.yawp.platform.Services;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import org.apache.commons.lang3.NotImplementedException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -33,25 +27,17 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+import static de.z0rdak.yawp.core.flag.RegionFlag.BREAK_BLOCKS;
 import static de.z0rdak.yawp.core.flag.RegionFlag.MOB_GRIEFING;
 
 public class FlagEvaluator {
-
-    public static boolean playerHasBypassPermission(@NotNull IProtectedRegion region, @Nullable Player player) {
-        return player != null && hasPermissionOrIsOpWithBypass(region, player);
-    }
-
-    private static boolean hasPermissionOrIsOpWithBypass(@NotNull IProtectedRegion region, @NotNull Player player) {
-        boolean hasPermission = Permissions.get().hasAnyPermission(region, player, Permissions.getGroups(region, player));
-        return hasPermission || Permissions.get().hasConfigPermAndOpBypassFlags(player);
-    }
 
     /**
      * Processes a flag check event and executes the corresponding consumer based on the result.  
      * <p>
      * The given flag check event is evaluated to determine the flag state and then  
      * posted to the event bus for further processing. If the flag state is  
-     * {@code ALLOWED}, {@code UNDEFINED}, or {@code DISABLED}, the {@code onAllow} consumer  
+     * {@code ALLOWED}, the {@code onAllow} consumer  
      * is executed. If the flag state is {@code DENIED}, the {@code onDeny} consumer is executed.  
      * </p>
      *
@@ -63,13 +49,14 @@ public class FlagEvaluator {
     public static FlagState processCheck(@NotNull FlagCheckEvent checkEvent, @Nullable Consumer<FlagCheckResult> onAllow, @Nullable Consumer<FlagCheckResult> onDeny) {
         FlagCheckResult result = evaluate(checkEvent);
         result = Services.EVENT.post(result);
-        if (result.getFlagState() == FlagState.ALLOWED && onAllow != null) {
+        var state = result.getFlagState();
+        if (state == FlagState.ALLOWED && onAllow != null) {
             onAllow.accept(result);
         }
-        if (result.getFlagState() == FlagState.DENIED && onDeny != null) {
+        if (state == FlagState.DENIED && onDeny != null) {
             onDeny.accept(result);
         }
-        return result.getFlagState();
+        return state;
     }
 
     /**
@@ -104,77 +91,56 @@ public class FlagEvaluator {
     }
 
     /**
-     * Evaluates the given flag check event and determines the resulting flag state.  
-     * <p>
-     * This method identifies the responsible region for the given position and dimension,  
-     * retrieves the applicable flag resolution from the region hierarchy,  
-     * and returns the corresponding flag state as part of a {@link FlagCheckResult}.  
-     * </p>
-     * <p>
-     * If no relevant region is found, the resulting flagstate defaults to {@link FlagState#UNDEFINED}.  
-     * </p>
+     * Evaluates a flag check event by determining the responsible region, resolving the flag context, 
+     * and computing the resulting flag state.
      *
-     * @param checkEvent the flag check event to evaluate, must not be {@code null}
-     * @return a {@link FlagCheckResult} containing the evaluated flag state, responsible region, and flag
+     * <p>The evaluation follows these steps:
+     * <ul>
+     *     <li>Finds the responsible region for the given target and dimension.</li>
+     *     <li>If no region is found, returns an undefined {@link FlagCheckResult}.</li>
+     *     <li>Constructs a {@link FlagContext} using the region, flag, and player information.</li>
+     *     <li>Resolves the effective flag context by considering inheritance and overrides.</li>
+     *     <li>Returns a {@link FlagCheckResult} containing the determined flag state and context details.</li>
+     * </ul>
+     *
+     * @param checkEvent the flag check event containing information about the target, dimension, and flag.
+     * @return a {@link FlagCheckResult} representing the evaluated flag state and context.
      */
     public static FlagCheckResult evaluate(FlagCheckEvent checkEvent) {
-        RegionFlag regionFlag = checkEvent.getRegionFlag();
-        IProtectedRegion targetRegion = findResponsibleRegion(checkEvent.getTarget(), checkEvent.getDimension());
+        var targetRegion = findResponsibleRegion(checkEvent.getTarget(), checkEvent.getDimension());
         if (targetRegion == null) {
             return FlagCheckResult.Undefined(checkEvent);
         }
-        if (regionFlag == RegionFlag.BREAK_BLOCKS) {
-            Constants.LOGGER.info("Breaking blocks");
+        var regionFlag = checkEvent.getRegionFlag();
+        var flagContext = new FlagContext(targetRegion, regionFlag, targetRegion.getFlag(regionFlag.name), checkEvent.getPlayer());
+        var resultingContext = resolveFlag(targetRegion, flagContext);
+        return new FlagCheckResult(checkEvent, resultingContext.resultingState(), resultingContext.region(), resultingContext.flag());
+    }
+
+    /**
+     * Resolves the final {@link FlagContext} for a given region by considering flag inheritance 
+     * from parent regions. The initial flag context needs to be the flag context corresponding to the given region.
+     *
+     * <p>This method follows a recursive approach to determine the effective flag context:</p>
+     * <ul>
+     *     <li>If the region is a root region, it returns the given {@code flagContext}.</li>
+     *     <li>Otherwise, it retrieves the {@link FlagContext} of the parent region.</li>
+     *     <li>The current flag context is merged with the parent's context using {@link FlagContext#inheritContext}.</li>
+     *     <li>The process continues recursively up the region hierarchy until the root is reached.</li>
+     * </ul>
+     *
+     * @param region      the region for which the flag context is being resolved.
+     * @param flagContext the initial flag context for the region.
+     * @return the resolved {@link FlagContext}, considering inheritance from parent regions.
+     */
+    public static FlagContext resolveFlag(IProtectedRegion region, FlagContext flagContext) {
+        if (isRootRegion(region)) {
+            return flagContext;
         }
-        RegionFlagResolution responsibleFlag = resolveFlag(targetRegion, regionFlag, checkEvent.getPlayer());
-        // return new FlagCheckResult(checkEvent, responsibleFlag);
-        throw new NotImplementedException();
+        FlagContext parentFlagContext = FlagContext.parentOf(region, flagContext.regionFlag(), flagContext.player());
+        FlagContext merged = flagContext.inheritContext(parentFlagContext);
+        return resolveFlag(region.getParent(), merged);
     }
-
-    public static RegionFlagResolution resolveFlag(IProtectedRegion region, RegionFlag regionFlag, @Nullable Player player) {
-        //return getResponsibleFlag(new FlagContext(region, regionFlag, region.getFlag(regionFlag.name), player));
-        throw new NotImplementedException();
-    }
-
-    //public static RegionFlagResolution getResponsibleFlag(FlagContext flagContext) {
-    //    if (isRootRegion(flagContext.region())) {
-    //        return flagContext.resultingFlag();
-    //    }
-    //    flagContext.checkAgainst(flagContext.region().getParent());
-    //    return getResponsibleFlag(flagContext);
-//
-    //    // Check if the player has bypass permission on the current region
-    //    if (playerHasBypassPermission(region, player)) {
-    //        carry = new RegionFlagResolution(region, null);  // Player bypasses the region's flag
-    //    } else {
-    //        // Otherwise, check the flag at the current region
-    //        IFlag flag = region.getFlag(regionFlag.name);
-    //        boolean allowedOrDefined = region.getRegionFlags().isAllowedOrDefined(regionFlag.name);
-    //        if (allowedOrDefined) {  // If the flag is allowed or defined, update the carry
-    //            carry = new RegionFlagResolution(region, flag);
-    //        } else {
-    //            if (!allowedOrDefined) {
-    //                carry = new RegionFlagResolution(region, null);
-    //            }
-    //        }
-    //    }
-//
-    //    // Check the flag in the parent region
-    //    IProtectedRegion parent = region.getParent();
-    //    IFlag parentFlag = parent.getFlag(regionFlag.name);
-    //    boolean parentFlagAllowedOrDefined = parent.getRegionFlags().isAllowedOrDefined(regionFlag.name);
-    //    if (parentFlagAllowedOrDefined) {  // not null and not undefined 
-    //        if (parentFlag.doesOverride()) {
-    //            carry = new RegionFlagResolution(parent, parentFlag);
-    //        } else // check if the players has bypass permission in the region, if so, the parent flag is not considered
-    //            if (playerHasBypassPermission(region, player))
-    //                carry = new RegionFlagResolution(region, null);
-    //            else
-    //                carry = new RegionFlagResolution(parent, parentFlag);
-    //    }
-    //    // recursive call to check the next parent
-    //    return getResponsibleFlag(parent, regionFlag, player, carry);
-    //}
 
     /**
      * Checks whether the given region is its own parent. 
@@ -201,7 +167,7 @@ public class FlagEvaluator {
      *         or {@code null} if no active region is found
      */
     @Nullable
-    private static IProtectedRegion findResponsibleRegion(@NotNull BlockPos pos, @NotNull ResourceKey<Level> dim) {
+    public static IProtectedRegion findResponsibleRegion(@NotNull BlockPos pos, @NotNull ResourceKey<Level> dim) {
         var localRegion = getInvolvedRegionFor(pos, dim);
         if (localRegion == null) {
             var dimRegion = RegionDataManager.get().cacheFor(dim).getDimensionalRegion();
@@ -271,85 +237,5 @@ public class FlagEvaluator {
                 return;
             processCheck(checkEvent, deny -> cir.setReturnValue(false));
         }
-    }
-
-
-    /**
-     * Gets the flag state for the given region and flag. <br>
-     * If the player is null, the flag state is returned as is. <br>
-     * If the player is not null, the flag state is checked against the player's permissions. <br>
-     * If the player is permitted, the flag state is returned as allowed. <br>
-     *
-     * @param region the region to get the flag state for
-     * @param flag   the flag to get the state for
-     * @param player the player to check the flag state against
-     * @return the flag state for the given region and flag
-     */
-    private static FlagState getFlagState(IProtectedRegion region, RegionFlag flag, @Nullable Player player) {
-        if (player == null) {
-            return region.getFlags().flagState(flag.name);
-        } else {
-            boolean hasPermission = Permissions.get().hasAnyPermission(region, player, Permissions.getGroups(region, player));
-            boolean isPermitted = hasPermission || Permissions.get().hasConfigPermAndOpBypassFlags(player);
-            if (isPermitted) {
-                return FlagState.ALLOWED;
-            } else {
-                return region.getFlags().flagState(flag.name);
-            }
-        }
-    }
-    
-    /**
-     * Gets the responsible region for the given position and dimension. <br>
-     * The responsible region is the region with the highest priority among all involved regions at the given location and dimension. <br>
-     *
-     * @param pos the position to get the responsible region for
-     * @param dim the dimension to get the responsible region for
-     * @return the responsible region for the given position and dimension
-     */
-    @Nullable
-    private static IProtectedRegion getResponsible(BlockPos pos, ResourceKey<Level> dim) {
-        IMarkableRegion region = getInvolvedRegionFor(pos, dim);
-        if (region == null) {
-            IProtectedRegion dimRegion = RegionDataManager.get().cacheFor(dim).getDimensionalRegion();
-            if (dimRegion.isActive()) {
-                return dimRegion;
-            } else {
-                return RegionDataManager.get().getGlobalRegion().isActive()
-                        ? RegionDataManager.get().getGlobalRegion()
-                        : null;
-            }
-        }
-        return region;
-    }
-
-    public static FlagCorrelation getResponsibleFlag(IProtectedRegion region, RegionFlag regionFlag, @Nullable FlagCorrelation carry) {
-        if (carry == null) {
-            FlagState flagState = region.getFlags().flagState(regionFlag.name);
-            if (flagState == FlagState.ALLOWED || flagState == FlagState.DENIED) {
-                IFlag flag = region.getFlag(regionFlag.name);
-                carry = new FlagCorrelation(region, flag);
-            } else
-                carry = new FlagCorrelation(region, null);
-        }
-        if (region.equals(region.getParent())) {
-            if (region.getFlags().flagState(regionFlag.name) != FlagState.UNDEFINED) {
-                if (carry.getFlag() == null) {
-                    carry = new FlagCorrelation(region, region.getFlag(regionFlag.name));
-                }
-            }
-            return carry;
-        }
-        FlagState flagState = region.getParent().getFlags().flagState(regionFlag.name);
-        if (flagState == FlagState.ALLOWED || flagState == FlagState.DENIED) {
-            IFlag flag = region.getParent().getFlag(regionFlag.name);
-            if (carry.getFlag() == null) {
-                carry = new FlagCorrelation(region.getParent(), flag);
-            }
-            if (flag.doesOverride()) {
-                carry = new FlagCorrelation(region.getParent(), flag);
-            }
-        }
-        return getResponsibleFlag(region.getParent(), regionFlag, carry);
     }
 }
