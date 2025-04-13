2f040383dd5ffbd784c6cbb99f25a782346c6fbe
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-04-13 17:49:32 +0200

    move util methods from old DimensionRegionCache to LevelRegionData

--- common/src/main/java/de/z0rdak/yawp/data/region/LevelRegionData.java
+++ common/src/main/java/de/z0rdak/yawp/data/region/LevelRegionData.java
@@ -2,10 +2,10 @@ package de.z0rdak.yawp.data.region;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import de.z0rdak.yawp.api.commands.CommandConstants;
+import de.z0rdak.yawp.commands.arguments.region.RegionArgumentType;
 import de.z0rdak.yawp.constants.Constants;
-import de.z0rdak.yawp.core.region.DimensionalRegion;
-import de.z0rdak.yawp.core.region.IMarkableRegion;
-import de.z0rdak.yawp.core.region.MarkedRegion;
+import de.z0rdak.yawp.core.region.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -13,8 +13,13 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.saveddata.SavedDataType;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static de.z0rdak.yawp.api.commands.CommandConstants.values;
 
 public class LevelRegionData extends SavedData {
 
@@ -50,9 +55,86 @@ public class LevelRegionData extends SavedData {
     public LevelRegionData(ResourceLocation id) {
         this.id = id;
         this.locals = new HashMap<>();
-        var global = RegionDataManager.get().getGlobalRegion();
+        var global = RegionDataManager.getGlobalRegion();
         ResourceKey<Level> levelRk = ResourceKey.create(Registries.DIMENSION, id);
         this.dim = new DimensionalRegion(levelRk, global);
     }
+
+    public void renameLocal(IMarkableRegion region, String regionName) {
+        if (this.locals.containsKey(regionName)) {
+            throw new IllegalArgumentException("Region with name '" + regionName + "' already exists in dimension '" + this.dim.getName() + "'!");
+        }
+        IMarkableRegion currentRegion = this.locals.get(region.getName());
+        IProtectedRegion parent = currentRegion.getParent();
+        this.removeLocal(currentRegion);
+        currentRegion.rename(regionName);
+        this.addLocal(parent, currentRegion);
+    }
+
+    /**
+     * Method to check if a region name is valid for a given dimension. <br>
+     * A region name is valid if it matches the pattern and is not already used in the dimension.
+     *
+     * @param dim        the dimension to be checked.
+     * @param regionName the name of the region to be checked.
+     * @return -1 if the region name is invalid, 0 if the region name is valid, 1 if the region name is already used in the dimension.
+     */
+    public int isValidRegionName(ResourceKey<Level> dim, String regionName) {
+        List<String> commandStrings = Arrays.stream(values()).map(CommandConstants::toString).collect(Collectors.toList());
+        if (!regionName.matches(RegionArgumentType.VALID_NAME_PATTERN.pattern())
+                || commandStrings.contains(regionName.toLowerCase())) {
+            return -1;
+        }
+        if (this.locals.containsKey(regionName)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Method to check if a region name is valid for a given dimension. <br>
+     * A region name is valid if it matches the pattern and is not already used in the dimension.
+     *
+     * @param dim        the dimension to be checked.
+     * @param regionName the name of the region to be checked.
+     * @return -1 if the region name is invalid, 0 if the region name is valid, 1 if the region name is already used in the dimension.
+     */
+    public boolean isAvailableForLocal(ResourceKey<Level> dim, String regionName) {
+        if (this.locals.containsKey(regionName)) {
+            return false;
+        }
+        return isValidRegionName(dim, regionName) == 0;
+    }
+
+
+    public void addLocal(IProtectedRegion parent, IMarkableRegion child) {
+        parent.addChild(child);
+        this.locals.put(child.getName(), child);
+    }
+
+    public void addLocal(IMarkableRegion child) {
+        this.dim.addChild(child);
+        this.locals.put(child.getName(), child);
+    }
+
+    public void removeLocal(IMarkableRegion region) {
+        if (this.hasLocal(region.getName())) {
+            this.locals.remove(region.getName());
+            if (region.getParent().getRegionType() == RegionType.DIMENSION) {
+                region.getParent().removeChild(region);
+            }
+        }
+    }
+
+    public boolean hasLocal(String regionName) {
+        return locals.containsKey(regionName);
+    }
+
+    public void clearLocals() {
+        this.locals.clear();
+        this.dim.clearChildren();
+    }
+
+
 }
 
