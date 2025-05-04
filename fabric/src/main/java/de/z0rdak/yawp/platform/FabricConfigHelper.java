b71e5d55af376072b9d0e91f7e638dccd9807f73
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-05-04 21:33:22 +0200

    update config

--- fabric/src/main/java/de/z0rdak/yawp/platform/FabricConfigHelper.java
+++ fabric/src/main/java/de/z0rdak/yawp/platform/FabricConfigHelper.java
@@ -8,8 +8,7 @@ import de.z0rdak.yawp.config.server.LoggingConfig;
 import de.z0rdak.yawp.config.server.PermissionConfig;
 import de.z0rdak.yawp.config.server.RegionConfig;
 import de.z0rdak.yawp.platform.services.IConfigHelper;
-import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
-import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeModConfigEvents;
+import fuzs.forgeconfigapiport.fabric.api.v5.ModConfigEvents;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
@@ -33,12 +32,12 @@ public class FabricConfigHelper implements IConfigHelper {
     }
     
     private void registerModConfig(ModConfigSpec spec, ModConfig.Type type, String configName) {
-        NeoForgeConfigRegistry.INSTANCE.register(MOD_ID, type, spec, configName);
+        fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry.INSTANCE.register(MOD_ID, type, spec, configName);
     }
 
     public void register() {
-        NeoForgeModConfigEvents.loading(MOD_ID).register(FabricConfigHelper::onModLoading);
-        NeoForgeModConfigEvents.reloading(MOD_ID).register(FabricConfigHelper::onModReloading);
+        ModConfigEvents.loading(MOD_ID).register(FabricConfigHelper::onModLoading);
+        ModConfigEvents.reloading(MOD_ID).register(FabricConfigHelper::onModReloading);
         // registering configuration
         Services.CONFIG_REGISTRY.registerServerConfig(PermissionConfig.CONFIG_SPEC, PermissionConfig.CONFIG_NAME);
         Services.CONFIG_REGISTRY.registerServerConfig(FlagConfig.CONFIG_SPEC, FlagConfig.CONFIG_NAME);
