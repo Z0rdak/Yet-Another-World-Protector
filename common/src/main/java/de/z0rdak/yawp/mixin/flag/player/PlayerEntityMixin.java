0a76fe0efda6d9c9fc0b4317eb551fa9db5f4e58
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-05-09 22:43:10 +0200

    fix no elytra flag not considering player

--- common/src/main/java/de/z0rdak/yawp/mixin/flag/player/PlayerEntityMixin.java
+++ common/src/main/java/de/z0rdak/yawp/mixin/flag/player/PlayerEntityMixin.java
@@ -22,8 +22,7 @@ public abstract class PlayerEntityMixin {
     void injectElytraCheck(CallbackInfoReturnable<Boolean> cir) {
         Player self = (Player) (Object) this;
         if (isServerSide(self.level())) {
-            FlagCheckEvent checkEvent = new FlagCheckEvent(self.blockPosition(), RegionFlag.USE_ELYTRA, getDimKey(self));
-            if (Services.EVENT.post(checkEvent)) {
+            FlagCheckEvent checkEvent = new FlagCheckEvent(self.blockPosition(), RegionFlag.USE_ELYTRA, getDimKey(self), self);            if (Services.EVENT.post(checkEvent)) {
                 return;
             }
             FlagEvaluator.processCheck(checkEvent, denyResult -> cir.setReturnValue(false));
