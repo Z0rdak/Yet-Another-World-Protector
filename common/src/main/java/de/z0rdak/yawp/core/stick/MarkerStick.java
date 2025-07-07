93ec3dbef5e0b947d9d27189c277b5f1bbb58847
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-07 19:20:40 +0200

    remove tp pos property

--- common/src/main/java/de/z0rdak/yawp/core/stick/MarkerStick.java
+++ common/src/main/java/de/z0rdak/yawp/core/stick/MarkerStick.java
@@ -18,7 +18,6 @@ import java.util.UUID;
 
 public class MarkerStick extends AbstractStick implements INbtSerializable<CompoundTag> {
 
-    private BlockPos teleportPos;
     private ResourceKey<Level> dimension;
     private AreaType areaType;
     private boolean isValidArea;
@@ -30,7 +29,6 @@ public class MarkerStick extends AbstractStick implements INbtSerializable<Compo
         this.isValidArea = false;
         this.markedBlocks = new ArrayList<>(this.areaType.maxBlocks);
         this.dimension = dim;
-        this.teleportPos = null;
     }
 
     public MarkerStick(CompoundTag nbt) {
@@ -46,15 +44,6 @@ public class MarkerStick extends AbstractStick implements INbtSerializable<Compo
     public void reset() {
         this.markedBlocks = new ArrayList<>(this.areaType.maxBlocks);
         this.isValidArea = false;
-        this.teleportPos = null;
-    }
-
-    public BlockPos getTeleportPos() {
-        return teleportPos;
-    }
-
-    public void setTeleportPos(BlockPos teleportPos) {
-        this.teleportPos = teleportPos;
     }
 
     public ResourceKey<Level> getDimension() {
@@ -105,10 +94,6 @@ public class MarkerStick extends AbstractStick implements INbtSerializable<Compo
         nbt.putBoolean(ItemNbtKeys.VALID_AREA, this.isValidArea);
         nbt.putString(ItemNbtKeys.AREA_TYPE, this.areaType.areaType);
         nbt.putString(ItemNbtKeys.DIM, this.dimension.location().toString());
-        nbt.putBoolean(ItemNbtKeys.IS_TP_SET, this.teleportPos != null);
-        if (this.teleportPos != null) {
-            nbt.put(ItemNbtKeys.TP_POS, NbtUtils.writeBlockPos(this.teleportPos));
-        }
         ListTag blocks = new ListTag();
         this.markedBlocks.forEach(block -> blocks.add(NbtUtils.writeBlockPos(block)));
         nbt.put(ItemNbtKeys.MARKED_BLOCKS, blocks);
@@ -120,10 +105,6 @@ public class MarkerStick extends AbstractStick implements INbtSerializable<Compo
         super.deserializeNBT(nbt);
         this.isValidArea = nbt.getBoolean(ItemNbtKeys.VALID_AREA);
         this.areaType = AreaType.of(nbt.getString(ItemNbtKeys.AREA_TYPE));
-        boolean isTpSet = nbt.getBoolean(ItemNbtKeys.IS_TP_SET);
-        if (isTpSet) {
-            this.teleportPos = NbtCompatHelper.toBlockPos(nbt, ItemNbtKeys.TP_POS).orElse(null);
-        }
         this.dimension = ResourceKey.create(Registries.DIMENSION, ResourceLocation.parse(nbt.getString(ItemNbtKeys.DIM)));
         ListTag markedBlocksNBT = (ListTag) nbt.get(ItemNbtKeys.MARKED_BLOCKS);
         if (markedBlocksNBT != null) {
