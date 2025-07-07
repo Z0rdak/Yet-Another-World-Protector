8fc05461cbf59f7c151e50689a288374ea68e029
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-07 19:19:00 +0200

    remove teleport pos from marker

--- common/src/main/java/de/z0rdak/yawp/constants/serialization/ItemNbtKeys.java
+++ common/src/main/java/de/z0rdak/yawp/constants/serialization/ItemNbtKeys.java
@@ -6,8 +6,6 @@ public class ItemNbtKeys {
     public static final String VALID_AREA = "valid";
     public static final String AREA_TYPE = "type";
     public static final String DIM = "dim";
-    public static final String TP_POS = "tp_pos";
-    public static final String IS_TP_SET = "is_tp_set";
     public static final String STICK_TYPE = "stick_type";
     public static final String STICK_ID = "stick-id";
     public static final String STICK = "stick";
