0a4e101a2d37d1dd48250cf31e8dc79327f95aa2
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-07 20:45:58 +0200

    add player commands which will from now on hold commands which can only be executed by players

--- common/src/main/java/de/z0rdak/yawp/commands/CommandRegistry.java
+++ common/src/main/java/de/z0rdak/yawp/commands/CommandRegistry.java
@@ -46,7 +46,8 @@ public final class CommandRegistry {
                 .then(MarkerCommands.build())
                 .then(GlobalCommands.build())
                 .then(DimensionCommands.build())
-                .then(RegionCommands.build());
+                .then(RegionCommands.build())
+                .then(PlayerCommands.build());
     }
 
     private static int promptHelp(CommandSourceStack src) {
