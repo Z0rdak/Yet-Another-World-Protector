5669339f00c6baad6d4f6f54ca3fef5b2a7c0b1c
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-07 11:49:54 +0200

    disable keep_inv and no_hunger flag for now

--- common/src/main/java/de/z0rdak/yawp/mixin/flag/player/PlayerEntityMixin.java
+++ common/src/main/java/de/z0rdak/yawp/mixin/flag/player/PlayerEntityMixin.java
@@ -31,6 +31,7 @@ public abstract class PlayerEntityMixin {
         }
     }
 
+    /*
     @Inject(method = "dropEquipment", at = @At(value = "HEAD"), allow = 1, cancellable = true)
     void onDropEquipment(CallbackInfo ci) {
         Player self = (Player) (Object) this;
@@ -42,7 +43,8 @@ public abstract class PlayerEntityMixin {
             FlagEvaluator.processCheck(checkEvent, denyResult -> ci.cancel());
         }
     }
-
+    */
+    /*
     @Inject(method = "causeFoodExhaustion", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/food/FoodData;addExhaustion(F)V"), cancellable = true, allow = 1)
     public void onGainHunger(float exhaustion, CallbackInfo ci) {
         Player self = (Player) (Object) this;
@@ -51,12 +53,13 @@ public abstract class PlayerEntityMixin {
             if (Services.EVENT.post(checkEvent))
                 return;
             FlagState flagState = FlagEvaluator.processCheck(checkEvent,
-                    deny -> { /* player has no permission -> do nothing to apply hunger */ }
+                    deny -> { /* player has no permission -> do nothing to apply hunger  }
             );
             if (flagState == FlagState.ALLOWED) {
                 ci.cancel();
             }            
         }
     }
+    */
 
 }
