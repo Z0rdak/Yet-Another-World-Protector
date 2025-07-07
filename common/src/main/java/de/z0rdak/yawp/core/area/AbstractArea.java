fc7740765b1be64e8acb34cb65ce603b9b509bc8
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-07 19:25:34 +0200

    add display properties to abstract area

--- common/src/main/java/de/z0rdak/yawp/core/area/AbstractArea.java
+++ common/src/main/java/de/z0rdak/yawp/core/area/AbstractArea.java
@@ -6,9 +6,15 @@ import net.minecraft.nbt.CompoundTag;
 public abstract class AbstractArea implements IMarkableArea {
 
     private AreaType areaType;
+    private BlockDisplayProperties displayProperties;
 
     protected AbstractArea(AreaType areaType) {
         this.areaType = areaType;
+        this.displayProperties = new BlockDisplayProperties(
+                BlockDisplayProperties.DEFAULT_BLOCK,
+                BlockDisplayProperties.DEFAULT_GLOW,
+                BlockDisplayProperties.DEFAULT_LIGHT_LEVEL
+        );
     }
 
     protected AbstractArea(CompoundTag nbt) {
@@ -20,14 +26,26 @@ public abstract class AbstractArea implements IMarkableArea {
     }
 
     @Override
+    public BlockDisplayProperties getDisplay() {
+        return displayProperties;
+    }
+
+    @Override
+    public void updateDisplay(BlockDisplayProperties properties) {
+        this.displayProperties = properties;
+    }
+
+    @Override
     public CompoundTag serializeNBT() {
         CompoundTag nbt = new CompoundTag();
         nbt.putString(RegionNbtKeys.AREA_TYPE, this.areaType.areaType);
+        nbt.put("display", this.displayProperties.serializeNBT());
         return nbt;
     }
 
     @Override
     public void deserializeNBT(CompoundTag nbt) {
+        this.displayProperties = new BlockDisplayProperties(nbt);
         this.areaType = AreaType.of(nbt.getString(RegionNbtKeys.AREA_TYPE));
     }
 }
