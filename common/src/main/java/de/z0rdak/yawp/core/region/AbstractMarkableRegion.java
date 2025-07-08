6f35e0a38eec411acdcaf68c2a0ca871c0e804d2
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-08 19:06:42 +0200

    implement method

--- common/src/main/java/de/z0rdak/yawp/core/region/AbstractMarkableRegion.java
+++ common/src/main/java/de/z0rdak/yawp/core/region/AbstractMarkableRegion.java
@@ -145,6 +145,6 @@ public abstract class AbstractMarkableRegion extends AbstractRegion implements I
 
     @Override
     public TeleportAnchors getTpAnchors() {
-        return null;
+        return this.tpAnchors;
     }
 }
