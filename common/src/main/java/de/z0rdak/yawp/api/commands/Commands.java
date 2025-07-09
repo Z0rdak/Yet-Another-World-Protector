56f604bc08155481c85c09f0e640d1720c586638
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-09 16:32:03 +0200

    add links to CLI to show and hide telepot anchors; use teleport command instead of direct tp command

--- common/src/main/java/de/z0rdak/yawp/api/commands/Commands.java
+++ common/src/main/java/de/z0rdak/yawp/api/commands/Commands.java
@@ -284,6 +284,21 @@ public final class Commands {
         return appendSubCommand(baseCmd, subCmd);
     }
 
+    public static String buildTeleportTpAnchorCommand(IMarkableRegion region, String name) {
+        String subCmd = buildSubCmdStr(TELEPORT.toString(), name);
+        return buildTeleportAnchorSubCommand(region, subCmd);
+    }
+
+    public static String buildShowTpAnchorCommand(IMarkableRegion region, String name) {
+        String subCmd = buildSubCmdStr(HIDE.toString(), name);
+        return buildTeleportAnchorSubCommand(region, subCmd);
+    }
+
+    public static String buildHideTpAnchorCommand(IMarkableRegion region, String name) {
+        String subCmd = buildSubCmdStr(SHOW.toString(), name);
+        return buildTeleportAnchorSubCommand(region, subCmd);
+    }
+
     public static String buildSuggestRenameTpAnchorCommand(IMarkableRegion region, String name) {
         String subCmd = buildSubCmdStr(RENAME.toString(), name, "");
         return buildTeleportAnchorSubCommand(region, subCmd);
