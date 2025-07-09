21d15a118713161dcd8eca824c8120aa8298bab1
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-09 08:17:33 +0200

    rename for clarification

--- common/src/main/java/de/z0rdak/yawp/util/ChatComponentBuilder.java
+++ common/src/main/java/de/z0rdak/yawp/util/ChatComponentBuilder.java
@@ -47,7 +47,10 @@ public class ChatComponentBuilder {
     private ChatComponentBuilder() {
     }
 
-    public static String buildBlockCoordinateStr(BlockPos target) {
+    /**
+     * Builds a string from the given block pos which can be used in commands
+     */
+    public static String commandBlockPosStr(BlockPos target) {
         return target.getX() + " " + target.getY() + " " + target.getZ();
     }
 
@@ -60,7 +63,7 @@ public class ChatComponentBuilder {
     }
 
     public static String tinyBlockPos(BlockPos target) {
-        return "[" + buildBlockCoordinateStr(target) + "]";
+        return "[" + commandBlockPosStr(target) + "]";
     }
 
     public static String buildBlockPosLinkText(BlockPos target) {
@@ -296,7 +299,7 @@ public class ChatComponentBuilder {
     }
 
     public static String buildTeleportCmd(ResourceKey<Level> dim, String tpSource, BlockPos target) {
-        return buildExecuteCommandString(dim, "tp " + tpSource + " " + buildBlockCoordinateStr(target));
+        return buildExecuteCommandString(dim, "tp " + tpSource + " " + commandBlockPosStr(target));
     }
 
     /**
