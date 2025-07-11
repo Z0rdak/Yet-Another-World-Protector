af8c6f1327488a8f529015885266ca55519039bd
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-11 10:27:07 +0200

    add command shortcut alternatives for region info, create and delete in the current dimension

--- common/src/main/java/de/z0rdak/yawp/commands/PlayerCommands.java
+++ common/src/main/java/de/z0rdak/yawp/commands/PlayerCommands.java
@@ -6,13 +6,15 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import de.z0rdak.yawp.api.commands.CommandConstants;
 import de.z0rdak.yawp.api.core.IDimensionRegionApi;
 import de.z0rdak.yawp.api.core.RegionManager;
 import de.z0rdak.yawp.api.permission.Permissions;
 import de.z0rdak.yawp.api.visualization.VisualizationManager;
+import de.z0rdak.yawp.commands.arguments.region.ContainingOwnedRegionArgumentType;
 import de.z0rdak.yawp.commands.arguments.region.RegionArgumentType;
+import de.z0rdak.yawp.core.area.AreaType;
 import de.z0rdak.yawp.core.area.DisplayType;
-import de.z0rdak.yawp.core.region.IMarkableRegion;
 import de.z0rdak.yawp.core.region.IProtectedRegion;
 import de.z0rdak.yawp.platform.Services;
 import de.z0rdak.yawp.util.text.messages.multiline.MultiLineMessage;
@@ -21,15 +23,18 @@ import de.z0rdak.yawp.util.text.messages.pagination.RegionsInDimensionPagination
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static de.z0rdak.yawp.api.MessageSender.sendError;
 import static de.z0rdak.yawp.api.commands.CommandConstants.*;
+import static de.z0rdak.yawp.commands.DimensionCommands.*;
 import static de.z0rdak.yawp.commands.RegionCommands.*;
 import static de.z0rdak.yawp.commands.arguments.ArgumentUtil.*;
 
@@ -37,6 +42,53 @@ import static de.z0rdak.yawp.commands.arguments.ArgumentUtil.*;
 class PlayerCommands {
 
     private PlayerCommands() {
+    }
+
+    static LiteralArgumentBuilder<CommandSourceStack> buildInfoLocal() {
+        return literal(INFO)
+                    .then(Commands.argument(LOCAL.toString(), StringArgumentType.word())
+                            .suggests((ctx, builder) -> RegionArgumentType.region().listSuggestionsIn(ctx, builder, ctx.getSource().getLevel()))
+                            .executes(ctx -> CommandUtil.promptRegionInfo(ctx, getRegionIn(ctx, ctx.getSource().getLevel())))
+                    );
+    }
+
+    static LiteralArgumentBuilder<CommandSourceStack> buildDeleteLocal() {
+        return literal(DELETE)
+                    .then(Commands.argument(LOCAL.toString(), StringArgumentType.word())
+                            .suggests((ctx, builder) -> RegionArgumentType.region().listSuggestionsIn(ctx, builder, ctx.getSource().getLevel()))
+                            .executes(ctx -> DimensionCommands.deleteRegion(ctx, getRegionIn(ctx, ctx.getSource().getLevel())))
+                    );
+    }
+
+    static LiteralArgumentBuilder<CommandSourceStack> buildCreateLocal() {
+        return literal(CREATE)
+                .then(Commands.argument(CommandConstants.NAME.toString(), StringArgumentType.word())
+                                .suggests((ctx, builder) -> SharedSuggestionProvider.suggest(Collections.singletonList(getRandomExample()), builder))
+                                .then(Commands.literal(AreaType.CUBOID.areaType)
+                                        .then(Commands.argument(POS1.toString(), BlockPosArgument.blockPos())
+                                                .then(Commands.argument(POS2.toString(), BlockPosArgument.blockPos())
+                                                        .executes(ctx -> createCuboidRegion(ctx, getRegionNameArgument(ctx),
+                                                                BlockPosArgument.getSpawnablePos(ctx, POS1.toString()),
+                                                                BlockPosArgument.getSpawnablePos(ctx, POS2.toString()), null))
+                                                        .then(Commands.argument(CommandConstants.PARENT.toString(), StringArgumentType.word())
+                                                                .suggests((ctx, builder) -> ContainingOwnedRegionArgumentType.owningRegions().listSuggestions(ctx, builder))
+                                                                .executes(ctx -> createCuboidRegion(ctx, getRegionNameArgument(ctx),
+                                                                        BlockPosArgument.getSpawnablePos(ctx, POS1.toString()),
+                                                                        BlockPosArgument.getSpawnablePos(ctx, POS2.toString()), getContainingOwnedRegionArgument(ctx))))))
+                                )
+                                .then(Commands.literal(AreaType.SPHERE.areaType)
+                                        .then(Commands.argument(CENTER_POS.toString(), BlockPosArgument.blockPos())
+                                                .then(Commands.argument(RADIUS.toString(), IntegerArgumentType.integer(0))
+                                                        .executes(ctx -> createSphereRegion(ctx, getRegionNameArgument(ctx),
+                                                                BlockPosArgument.getSpawnablePos(ctx, CENTER_POS.toString()),
+                                                                IntegerArgumentType.getInteger(ctx, RADIUS.toString()), null))
+                                                        .then(Commands.argument(CommandConstants.PARENT.toString(), StringArgumentType.word())
+                                                                .suggests((ctx, builder) -> ContainingOwnedRegionArgumentType.owningRegions().listSuggestions(ctx, builder))
+                                                                .executes(ctx -> createSphereRegion(ctx, getRegionNameArgument(ctx),
+                                                                        BlockPosArgument.getSpawnablePos(ctx, CENTER_POS.toString()),
+                                                                        IntegerArgumentType.getInteger(ctx, RADIUS.toString()), getContainingOwnedRegionArgument(ctx))))))
+                                )
+                        );
     }
 
     // TODO: HIDE LOCAL HIERARCHY/INTERSECTING
