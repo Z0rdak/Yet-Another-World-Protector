f44d3030739d6fa9e87912e28be813b723b40933
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-15 11:53:01 +0200

    change config helper to accept string for now until flag registry is in place

--- fabric/src/main/java/de/z0rdak/yawp/platform/config/FabricFlagConfigHelper.java
+++ fabric/src/main/java/de/z0rdak/yawp/platform/config/FabricFlagConfigHelper.java
@@ -24,7 +24,7 @@ public class FabricFlagConfigHelper implements IFlagConfigHelper {
     }
 
     @Override
-    public boolean isDisabledByConfig(Flag flag) {
+    public boolean isDisabledByConfig(String flag) {
         return FlagConfig.isDisabledByConfig(flag);
     }
 }
--- forge/src/main/java/de/z0rdak/yawp/platform/config/ForgeFlagConfigHelper.java
+++ forge/src/main/java/de/z0rdak/yawp/platform/config/ForgeFlagConfigHelper.java
@@ -24,7 +24,7 @@ public class ForgeFlagConfigHelper implements IFlagConfigHelper {
     }
 
     @Override
-    public boolean isDisabledByConfig(Flag flag) {
+    public boolean isDisabledByConfig(String flag) {
         return FlagConfig.isDisabledByConfig(flag);
     }
 }
