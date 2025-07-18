58adc62dd4aee00e9d8fcb954a0041bd600fd036
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-07-18 07:26:09 +0200

    fix: display properties being initialized with wrong default values

--- common/src/main/java/de/z0rdak/yawp/core/area/BlockDisplayProperties.java
+++ common/src/main/java/de/z0rdak/yawp/core/area/BlockDisplayProperties.java
@@ -64,14 +64,14 @@ public final class BlockDisplayProperties implements INbtSerializable<CompoundTa
     @Override
     public void deserializeNBT(CompoundTag nbt) {
         String string = nbt.getString("block");
-        this.blockRl = ResourceLocation.tryParse(string);
-        if  (this.blockRl == null) {
+        var res = ResourceLocation.tryParse(string);
+        if (res == null) {
             Random rand = new Random();
             int randomNum = rand.nextInt(0, DEFAULT_BLOCKS.size());
             this.blockRl = DEFAULT_BLOCKS.get(randomNum);
         }
-        this.hasGlow = nbt.getBoolean("hasGlow");
-        this.lightLevel = nbt.getInt("lightLevel");
+        this.hasGlow = nbt.contains("hasGlow") ?  nbt.getBoolean("hasGlow") : DEFAULT_GLOW;
+        this.lightLevel = nbt.contains("lightLevel") ? nbt.getInt("lightLevel") : DEFAULT_LIGHT_LEVEL;
     }
 
     public ResourceLocation blockRl() {
