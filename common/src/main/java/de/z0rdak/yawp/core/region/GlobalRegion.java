36f3f26820e46edac50f7c670e66eb5d490e991d
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-10-29 21:22:57 +0100

    rename Flag to FlagValue, to make distinction easier

--- common/src/main/java/de/z0rdak/yawp/core/region/GlobalRegion.java
+++ common/src/main/java/de/z0rdak/yawp/core/region/GlobalRegion.java
@@ -2,11 +2,10 @@ package de.z0rdak.yawp.core.region;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import de.z0rdak.yawp.core.flag.Flag;
+import de.z0rdak.yawp.core.flag.FlagValue;
 import de.z0rdak.yawp.core.flag.RegionFlags;
 import de.z0rdak.yawp.core.group.PlayerContainer;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
@@ -26,7 +25,7 @@ public class GlobalRegion extends ProtectedRegion {
                                     .forGetter(ProtectedRegion::getParentName),
                             Codec.STRING.fieldOf("type")
                                     .forGetter(r -> r.getRegionType().type),
-                            Codec.unboundedMap(Codec.STRING, Flag.CODEC)
+                            Codec.unboundedMap(Codec.STRING, FlagValue.CODEC)
                                     .fieldOf("flags")
                                     .forGetter(r -> r.getFlags().getFlagMap()),
                             Codec.BOOL.fieldOf("isActive")
