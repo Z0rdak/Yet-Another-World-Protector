4f06f80f059fa2054ed32534e5d1ef78ca02ba0f
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2025-11-03 14:13:37 +0100

    add event to notify listeners when yawp finished loading data for levels

--- common/src/main/java/de/z0rdak/yawp/data/region/RegionDataManager.java
+++ common/src/main/java/de/z0rdak/yawp/data/region/RegionDataManager.java
@@ -37,7 +37,7 @@ import static de.z0rdak.yawp.handler.HandlerUtil.isServerSide;
 
 public class RegionDataManager {
 
-    public static final Logger LOGGER = LogManager.getLogger(Constants.MOD_ID.toUpperCase(Locale.ROOT) + "-DataManager");
+    public static final Logger LOGGER = LogManager.getLogger(Constants.MOD_ID.toUpperCase() + "-RegionDataManager");
     private static MinecraftServer serverInstance;
     private static LevelListData savedLevelData = new LevelListData();
     private static GlobalRegionData globalRegionData = new GlobalRegionData();
@@ -202,6 +202,7 @@ public class RegionDataManager {
                     restoreHierarchy(dimRegionStorage.get(levelRl), region);
                 });
             }
+            Services.YAWP_EVENT_DISPATCHER.post(level);
         } catch (NullPointerException npe) {
             LOGGER.error(Component.translatableWithFallback("data.region.level.local.load.failed", "Loading regions failed!").getString(), npe);
         }
