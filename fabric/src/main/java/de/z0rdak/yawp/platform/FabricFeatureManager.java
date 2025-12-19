a45fb0373d331aa01431308276d3ec0f0183c038
Marcel Bartholet <Marcel.Bartholet@web.de>
2025-12-19 09:31:26 +0100

    implement fabric feature manager to set up hooks for player tracking

---
+++ fabric/src/main/java/de/z0rdak/yawp/platform/FabricFeatureManager.java
@@ -0,0 +1,15 @@
+package de.z0rdak.yawp.platform;
+
+import de.z0rdak.yawp.handler.PlayerPosTracker;
+import de.z0rdak.yawp.platform.services.FeatureManager;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+
+public class FabricFeatureManager implements FeatureManager {
+    @Override
+    public void enablePlayerTracker() {
+        ServerTickEvents.START_WORLD_TICK.register(PlayerPosTracker::tickLevel);
+        ServerPlayConnectionEvents.DISCONNECT.register(
+                (e, s) -> PlayerPosTracker.onPlayerDisc(e.getPlayer()));
+    }
+}
