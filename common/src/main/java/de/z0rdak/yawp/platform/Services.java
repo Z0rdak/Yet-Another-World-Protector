f14dc6d4aa6ab6f21765405c91acc26d9c51672b
Marcel Bartholet <Marcel.Bartholet@web.de>
2025-12-19 09:30:29 +0100

    add service to enable player tracking in config

--- common/src/main/java/de/z0rdak/yawp/platform/Services.java
+++ common/src/main/java/de/z0rdak/yawp/platform/Services.java
@@ -2,14 +2,15 @@ package de.z0rdak.yawp.platform;
 
 import de.z0rdak.yawp.constants.Constants;
 import de.z0rdak.yawp.platform.services.IConfigHelper;
-import de.z0rdak.yawp.platform.services.event.FlagEventDispatcher;
+import de.z0rdak.yawp.platform.services.FeatureManager;
 import de.z0rdak.yawp.platform.services.IPermissionHelper;
-import de.z0rdak.yawp.platform.services.event.RegionEventDispatcher;
+import de.z0rdak.yawp.platform.services.IPlatformHelper;
 import de.z0rdak.yawp.platform.services.config.IFlagConfigHelper;
 import de.z0rdak.yawp.platform.services.config.ILoggingConfigHelper;
 import de.z0rdak.yawp.platform.services.config.IPermissionConfigHelper;
-import de.z0rdak.yawp.platform.services.IPlatformHelper;
 import de.z0rdak.yawp.platform.services.config.IRegionConfigHelper;
+import de.z0rdak.yawp.platform.services.event.FlagEventDispatcher;
+import de.z0rdak.yawp.platform.services.event.RegionEventDispatcher;
 import de.z0rdak.yawp.platform.services.event.YawpEventDispatcher;
 
 import java.util.ServiceLoader;
@@ -26,6 +27,7 @@ public class Services {
     public static final FlagEventDispatcher FLAG_EVENT_DISPATCHER = Services.load(FlagEventDispatcher.class);
     public static final RegionEventDispatcher REGION_EVENT_DISPATCHER = Services.load(RegionEventDispatcher.class);
     public static final YawpEventDispatcher YAWP_EVENT_DISPATCHER = Services.load(YawpEventDispatcher.class);
+    public static final FeatureManager FEATURE_MANAGER = Services.load(FeatureManager.class);
 
     public static <T> T load(Class<T> clazz) {
 
