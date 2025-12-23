16d194666f1d555f72335c466a0b749421fbb939
Marcel Bartholet <marcel.bartholet@web.de>
2025-12-23 16:11:34 +0100

    resolve cherry-pick config mess from 1.20.1 to 1.21.1

--- neoforge/src/main/java/de/z0rdak/yawp/platform/NeoForgeConfigHelper.java
+++ neoforge/src/main/java/de/z0rdak/yawp/platform/NeoForgeConfigHelper.java
@@ -1,16 +1,8 @@
 package de.z0rdak.yawp.platform;
 
-import de.z0rdak.yawp.api.events.region.FlagCheckEvent;
-import de.z0rdak.yawp.api.events.region.NeoForgeFlagCheckEvent;
-import de.z0rdak.yawp.api.events.region.NeoForgeFlagCheckResult;
-import de.z0rdak.yawp.commands.CommandRegistry;
 import de.z0rdak.yawp.config.ConfigRegistry;
-import de.z0rdak.yawp.config.server.FlagConfig;
-import de.z0rdak.yawp.config.server.LoggingConfig;
-import de.z0rdak.yawp.config.server.PermissionConfig;
-import de.z0rdak.yawp.config.server.RegionConfig;
+import de.z0rdak.yawp.config.server.*;
 import de.z0rdak.yawp.constants.Constants;
-import de.z0rdak.yawp.platform.config.NeoForgeLoggingConfigHelper;
 import de.z0rdak.yawp.platform.services.IConfigHelper;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -19,7 +11,6 @@ import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
-import net.neoforged.neoforge.common.NeoForge;
 
 public class NeoForgeConfigHelper implements IConfigHelper {
 
@@ -65,22 +56,13 @@ public class NeoForgeConfigHelper implements IConfigHelper {
         Services.CONFIG_REGISTRY.registerServerConfig(FlagConfig.CONFIG_SPEC, FlagConfig.CONFIG_NAME);
         Services.CONFIG_REGISTRY.registerServerConfig(RegionConfig.CONFIG_SPEC, RegionConfig.CONFIG_NAME);
         Services.CONFIG_REGISTRY.registerServerConfig(LoggingConfig.CONFIG_SPEC, LoggingConfig.CONFIG_NAME);
+        Services.CONFIG_REGISTRY.registerServerConfig(FeatureConfig.CONFIG_SPEC, FeatureConfig.CONFIG_NAME);
     }
 
     @SubscribeEvent
     public static void onConfigLoading(ModConfigEvent.Loading event) {
         if (event.getConfig().getModId().equals(Constants.MOD_ID)) {
-            Runnable registerHandler = () -> {
-                if (LoggingConfig.shouldLogFlagChecks()) {
-                    NeoForge.EVENT_BUS.addListener(
-                            (NeoForgeFlagCheckEvent e) -> LoggingConfig.logCheck(NeoForgeFlagCheckEvent.asNonEvent(e)));
-                }
-                if (LoggingConfig.shouldLogFlagCheckResults()) {
-                    NeoForge.EVENT_BUS.addListener(
-                            (NeoForgeFlagCheckResult e) -> LoggingConfig.logResult(NeoForgeFlagCheckResult.asNonEvent(e)));
-                }
-            };            
-            ConfigRegistry.onModLoaded(event.getConfig().getFileName(), registerHandler);
+            ConfigRegistry.onModLoaded(event.getConfig().getFileName(), () -> {});
         }
     }
 
