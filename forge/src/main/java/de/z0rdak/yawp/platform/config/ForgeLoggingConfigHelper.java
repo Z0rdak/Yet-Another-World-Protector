1970488c948d30263170df12f792f352232684af
Marcel Bartholet <Marcel.Bartholet@web.de>
2025-12-29 21:28:02 +0100

    fix import

--- forge/src/main/java/de/z0rdak/yawp/platform/config/ForgeLoggingConfigHelper.java
+++ forge/src/main/java/de/z0rdak/yawp/platform/config/ForgeLoggingConfigHelper.java
@@ -2,8 +2,6 @@ package de.z0rdak.yawp.platform.config;
 
 import de.z0rdak.yawp.api.events.flag.FlagCheckRequest;
 import de.z0rdak.yawp.api.events.flag.FlagCheckResult;
-import de.z0rdak.yawp.platform.event.ForgeFlagCheckRequest;
-import de.z0rdak.yawp.platform.event.ForgeFlagCheckResult;
 import de.z0rdak.yawp.config.server.LoggingConfig;
 import de.z0rdak.yawp.platform.services.config.ILoggingConfigHelper;
 
@@ -28,8 +26,7 @@ public class ForgeLoggingConfigHelper implements ILoggingConfigHelper {
 
     @Override
     public boolean logCheck(FlagCheckRequest check) {
-        ForgeFlagCheckRequest event = new ForgeFlagCheckRequest(check.getTarget(), check.getRegionFlag(), check.getDimension(), check.getPlayer(), check.getId());
-        return LoggingConfig.logCheck(event);
+        return LoggingConfig.logCheck(check);
     }
 
     @Override
