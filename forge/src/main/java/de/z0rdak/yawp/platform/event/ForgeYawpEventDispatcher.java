f7e2b2950220e17dbb307ce1ef108edefe8a9563
Z0rdak <macel.bartholet@web.de>
2026-04-10 23:58:11 +0200

    remove forge leftover from porting

--- forge/src/main/java/de/z0rdak/yawp/platform/event/ForgeYawpEventDispatcher.java
+++
@@ -1,7 +0,0 @@
-package de.z0rdak.yawp.platform.event;
-
-import de.z0rdak.yawp.platform.services.event.YawpEventDispatcher;
-
-public final class ForgeYawpEventDispatcher implements YawpEventDispatcher {
-
-}
