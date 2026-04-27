39ee63fc0e5dc17b718f668cfe44cf85ba0a0530
Z0rdak <macel.bartholet@web.de>
2026-04-27 10:32:27 +0200

    global track command now only suggest untracked levels;

--- common/src/main/java/de/z0rdak/yawp/commands/GlobalCommands.java
+++ common/src/main/java/de/z0rdak/yawp/commands/GlobalCommands.java
@@ -6,9 +6,15 @@ import com.mojang.brigadier.context.CommandContext;
 import de.z0rdak.yawp.api.core.RegionManager;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.commands.arguments.DimensionArgument;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import static de.z0rdak.yawp.api.MessageSender.sendCmdFeedback;
 import static de.z0rdak.yawp.api.commands.CommandConstants.*;
@@ -43,9 +49,17 @@ public class GlobalCommands {
                         )
                 )
                 .then(literal(RESET).executes(GlobalCommands::resetGlobalRegion))
-                // TODO: Only suggest levels which are not tracked yet
                 .then(literal(TRACK)
                         .then(Commands.argument(DIM.toString(), DimensionArgument.dimension())
+                                .suggests((ctx, builder) -> {
+                                    var allLevels = ctx.getSource().levels().stream().
+                                            map(ResourceKey::identifier)
+                                            .collect(Collectors.toSet());
+                                    var tracked = RegionManager.get().getLevels();
+                                    allLevels.removeAll(tracked);
+                                    var untracked = allLevels.stream().map(Identifier::toString).toList();
+                                    return SharedSuggestionProvider.suggest(untracked, builder);
+                                })
                         .executes(ctx -> trackLevel(ctx, DimensionArgument.getDimension(ctx, DIM.toString()))))
                 )
                 // TODO: Only suggest levels which are already tracked
